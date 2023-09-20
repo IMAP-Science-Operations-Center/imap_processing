f1fbf5ab40ab425fa19501f69139539b9a588590
Tenzin Choedon <tenzin.choedon@lasp.colorado.edu>
2023-09-20 16:16:13 -0600

    added MET/SHCOARSE time as first dimension of dataset

---
+++ imap_processing/swe/l1a/swe_l1a.py
@@ -0,0 +1,23 @@
+import logging
+
+from imap_processing.swe.l0 import decom_swe
+from imap_processing.swe.l1a.swe_science import swe_science
+
+
+def swe_l1a(packet_file: str):
+    """This function receives all L0 data file. Based on appId, it
+    call its function to process. If appId is science, it requires more work
+    than other appId such as appId of housekeeping.
+
+    Parameters
+    ----------
+    packet_file : str
+        The path and filename to the L0 file to read
+    """
+    decom_data = decom_swe.decom_packets(packet_file)
+    logging.info(f"Unpacking data from {packet_file}")
+
+    # If appId is science, then the file should contain all data of science appId
+    if decom_data[0].header["PKT_APID"].raw_value == 1344:
+        logging.info("Processing science data")
+        return swe_science(decom_data=decom_data)
