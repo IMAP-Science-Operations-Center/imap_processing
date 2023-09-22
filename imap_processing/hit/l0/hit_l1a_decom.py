545c5b00b317679121c9632a627718170dfac131
Sean Hoyt <sean.hoyt@lasp.colorado.edu>
2023-09-22 13:53:19 -0600

    fixed hit l1a docstrings

--- imap_processing/hit/l0/hit_l1a_decom.py
+++ imap_processing/hit/l0/hit_l1a_decom.py
@@ -59,6 +59,7 @@ def decom_hit_packets(packet_file: str, xtce: str):
 def create_datasets(data):
     """
     Creates a dataset for each APID in the data
+    Parameters
     ----------
     data : dict
         A single dictionary containing data for all instances of an APID.
@@ -87,6 +88,7 @@ def group_apid_data(packets, apid):
     """
     Creates a list of dictionaries containing data for each instance
     of a specified APID
+    Parameters
     ----------
     packets: list
         List of all the unpacked data from decom.decom_packets()
