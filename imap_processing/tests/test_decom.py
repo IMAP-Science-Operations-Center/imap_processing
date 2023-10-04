37b5d81e36232df1b76726bbf3c5b10ef502a38e
Matthew Bourque <matthew.bourque@lasp.colorado.edu>
2023-10-04 10:36:15 -0600

    Avoiding glob library by using pathlib instead; added check of uint value; marking idex test as expected fail

--- imap_processing/tests/test_decom.py
+++ imap_processing/tests/test_decom.py
@@ -1,32 +1,41 @@
 """Tests aspects of the decommutation process that is generic to all instruments"""
 
-import glob
-
 import pytest
 
 from imap_processing import imap_module_directory
 
-xtce_documents = glob.glob(f"{imap_module_directory}/*/packet_definitions/*.xml")
-xtce_documents = [
-    item for item in xtce_documents if "idex_packet_definition.xml" not in item
-]  # Temporary until IDEX packet definition is fixed
+# Scrape the repo for all packet definition XML files
+xtce_documents = list(imap_module_directory.glob("*/packet_definitions/*.xml"))
+
+# (Temporary) Mark IDEX as expected fail until packet definition is fixed
+idex_document = (
+    imap_module_directory / "idex" / "packet_definitions" / "idex_packet_definition.xml"
+)
+# Need to remove IDEX from the list in order to append it again with proper marking
+xtce_documents = [item for item in xtce_documents if item != idex_document]
+xtce_documents.append(
+    pytest.param(
+        idex_document,
+        marks=pytest.mark.xfail(
+            reason="Packet Definition does not include properly formatted CCSDS Header"
+        ),
+    )
+)
 
 
 @pytest.mark.parametrize("xtce_document", xtce_documents)
 def test_ccsds_header(xtce_document):
     """Test if the XTCE document contains the proper CCSDS header information"""
 
-    print(f"\nTesting {xtce_document}")
-
     # Define what is expected in the XTCE document
     header_keys = [
-        "VERSION",
-        "TYPE",
-        "SEC_HDR_FLG",
-        "PKT_APID",
-        "SEG_FLGS",
-        "SRC_SEQ_CTR",
-        "PKT_LEN",
+        ("VERSION", "UINT3"),
+        ("TYPE", "UINT1"),
+        ("SEC_HDR_FLG", "UINT1"),
+        ("PKT_APID", "UINT11"),
+        ("SEG_FLGS", "UINT2"),
+        ("SRC_SEQ_CTR", "UINT14"),
+        ("PKT_LEN", "UINT16"),
     ]
     header_container_set = (
         "<xtce:ContainerSet>"
@@ -50,7 +59,12 @@ def test_ccsds_header(xtce_document):
 
     # Check that each header key is defined as a Parameter
     for header_key in header_keys:
-        assert f'<xtce:Parameter name="{header_key}" parameterTypeRef=' in document
+        assert (
+            f'<xtce:Parameter name="{header_key[0]}" parameterTypeRef="{header_key[1]}"'
+            in document
+            or f'<xtce:Parameter name="{header_key[0]}" parameterTypeRef="{header_key[1].lower()}"'
+            in document
+        )
 
     # Check that the header is defined as a SequenceContainer in the XTCE file
     # First remove discrepancies in whitespace, tabs, and newlines
