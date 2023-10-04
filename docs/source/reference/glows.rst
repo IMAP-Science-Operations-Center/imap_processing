075b13920027a387876c8a55d8eab589332cf5cf
Maxine Hartnett <maxine.hartnett@lasp.colorado.edu>
2023-10-04 15:55:06 -0600

    Adding L1A histogram data class

--- docs/source/reference/glows.rst
+++ docs/source/reference/glows.rst
@@ -17,3 +17,4 @@ The L0 code to decommutate the CCSDS packet data can be found below.
 
     imap_processing.glows.l0.decom_glows
     imap_processing.glows.l0.glows_l0_data
+    imap_processing.glows.l1.glows_l1a_data
--- imap_processing/glows/l1/glows_l1a_data.py
+++ imap_processing/glows/l1/glows_l1a_data.py
@@ -10,17 +10,72 @@ class HistogramL1A:
     Attributes
     ----------
     l0: HistogramL0
-        Data class containing the raw data from the histogram packet
+        HistogramL0 Data class containing the raw data from the histogram packet
     histograms: list[int]
         List of histogram data values
+    block_header: dict
+        Header for L1A
+    last_spin_id: int
+        ID of the last spin in block (computed with start spin and offset)
+    imap_start_time: tuple[int, int]
+        IMAP start time for the block, in the form (seconds, subseconds)
+    imap_end_time_offset: tuple[int, int]
+        IMAP end time offset for the block, in the form (seconds, subseconds)
+    glows_start_time: tuple[int, int]
+        GLOWS start time for the block, in the form (seconds, subseconds)
+    glows_end_time_offset: tuple[int, int]
+        GLOWS end time offset for the block, in the form (seconds, subseconds)
+    flags: dict
+        Dictionary containing "flags_set_onboard" from L0, and "is_generated_on_ground",
+        which is set to "False" for decommed packets.
     """
 
     l0: HistogramL0
     histograms: list[int]
+    block_header: dict
+    last_spin_id: int
+    imap_start_time: tuple[int, int]
+    imap_end_time_offset: tuple[int, int]
+    glows_start_time: tuple[int, int]
+    glows_end_time_offset: tuple[int, int]
+    flags: dict
+
+    def _set_l1a_data(self):
+        """Set all the Level 1a attributes which are different from level 0.
+
+        This sets all the attributes in the class except "l0", "histograms", and
+         "block_header"
+        """
+        # use start ID and offset to calculate the last spin ID in the block
+        self.last_spin_id = self.l0.STARTID + self.l0.ENDID
+
+        # Create time tuples based on second and subsecond pairs
+        self.imap_start_time = (self.l0.SEC, self.l0.SUBSEC)
+        self.imap_end_time_offset = (self.l0.OFFSETSEC, self.l0.OFFSETSUBSEC)
+        self.glows_start_time = (self.l0.GLXSEC, self.l0.GLXSUBSEC)
+        self.glows_end_time_offset = (self.l0.GLXOFFSEC, self.l0.GLXOFFSUBSEC)
+
+        # Flags
+        self.flags = {
+            "flags_set_onboard": self.l0.FLAGS,
+            "is_generated_on_ground": False,
+        }
+
+    def _set_block_header(self):
+        """Create the block header using software version info."""
+        self.block_header = {
+            "flight_software_version": self.l0.SWVER,
+            "ground_software_version": self.l0.ground_sw_version,
+            "pkts_file_name": self.l0.packet_file_name,
+            # note: packet number is seq_count (per apid!) field in CCSDS header
+            "seq_count_in_pkts_file": self.l0.ccsds_header.SRC_SEQ_CTR,
+        }
 
     def __post_init__(self):
         """Convert the level 0 histogram data into a usable L1A list."""
         self.histograms = self._convert_histogram_data(self.l0.HISTOGRAM_DATA)
+        self._set_l1a_data()
+        self._set_block_header()
 
     def _convert_histogram_data(self, binary_hist_data: str) -> list[int]:
         """Convert the raw histogram data into a list.
