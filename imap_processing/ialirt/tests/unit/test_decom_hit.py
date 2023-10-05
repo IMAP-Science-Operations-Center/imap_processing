01482f62f1b3b3016b9f51b651626cafac5443e2
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2023-10-05 16:58:26 -0600

    adding other instruments

--- imap_processing/ialirt/tests/unit/test_decom_hit.py
+++ imap_processing/ialirt/tests/unit/test_decom_hit.py
@@ -3,19 +3,21 @@ import pytest
 from imap_processing.ialirt.l0.decom_ialirt import decom_packets
 from imap_processing.ialirt.tests.unit.data_path_fixtures import \
     packet_path, xtce_ialirt_path
-from imap_processing.ialirt.l0.decom_hit import decom_hit_packets
+from imap_processing.ialirt.l0.decom_instruments import decom_instrument_packets
 
 
 @pytest.fixture()
-def decom_hit(packet_path, xtce_ialirt_path):
+def decom_data(packet_path, xtce_ialirt_path):
     """Data for decom_ultra"""
-    data_packet_list = decom_hit_packets(packet_path,  xtce_ialirt_path)
+    data_packet_list = decom_instrument_packets(packet_path,  xtce_ialirt_path)
     return data_packet_list
 
 
-def test_decom_hit(decom_hit):
+def test_decom_hit(decom_data):
     """This function reads validation data and checks that
-    decom data matches validation data for auxiliary packet"""
+    decom data matches validation data"""
+
+
 
     print('hi')
 
