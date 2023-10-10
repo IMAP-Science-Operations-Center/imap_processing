193595a89e079dd48fde8764d40459a75536d40f
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2023-10-10 12:04:15 -0600

    formatting

--- imap_processing/ialirt/l0/decom_ialirt.py
+++ imap_processing/ialirt/l0/decom_ialirt.py
@@ -1,8 +1,21 @@
-from space_packet_parser import parser, xtcedef
+import logging
+from enum import Enum
+
 import bitstring
+import xarray as xr
+from space_packet_parser import parser, xtcedef
+
+logging.basicConfig(level=logging.ERROR)
+logger = logging.getLogger(__name__)
+
+
+class PacketLength(Enum):
+    EXPECTED_LENGTH = 1464
+
 
 def decom_packets(packet_file, xtce_packet_definition):
-    """Unpack data packet. In this function, we unpack and return data
+    """
+    Unpack data packet. In this function, we unpack and return data
     as it is. Data modification will not be done at this step.
 
     Parameters
@@ -23,8 +36,8 @@ def decom_packets(packet_file, xtce_packet_definition):
 
     packets = []
 
-    with open(packet_file, "r") as file:
-        for line in file:
+    with open(packet_file) as file:
+        for line_number, line in enumerate(file, 1):
             if not line.startswith("#"):
                 # Split the line by semicolons
                 # Discard the first value since it is only a counter
@@ -45,9 +58,87 @@ def decom_packets(packet_file, xtce_packet_definition):
                     # Append the padded binary string to the final string
                     binary_values += bin_value_padded
 
-                packet_generator = packet_parser.generator(bitstring.ConstBitStream(bin=binary_values))
+                # Check the length of binary_values
+                if len(binary_values) != PacketLength.EXPECTED_LENGTH.value:
+                    error_message = f"Error on line {line_number}: " \
+                                    f"Length of binary_values (" \
+                                    f"{len(binary_values)}) does not equal " \
+                                    f"{PacketLength.EXPECTED_LENGTH.value}."
+                    logger.error(error_message)
+                    raise ValueError(error_message)
+
+                packet_generator = packet_parser.generator(
+                    bitstring.ConstBitStream(bin=binary_values))
 
                 for packet in packet_generator:
                     packets.append(packet)
 
     return packets
+
+
+def generate_xarray(packet_file: str, xtce: str):
+    """
+    Generate xarray from unpacked data.
+
+    Parameters
+    ----------
+    packet_file : str
+        Path to the CCSDS data packet file.
+    xtce : str
+        Path to the XTCE packet definition file.
+
+    Returns
+    -------
+    xr.Dataset
+        A dataset containing the decoded data fields with 'time' as the coordinating
+        dimension.
+    """
+
+    try:
+        packets = decom_packets(packet_file, xtce)
+    except Exception as e:
+        logger.error(f"Error during packet decomposition: {str(e)}")
+        return
+
+    if not packets:
+        logger.warning(f"No packets found in {packet_file}.")
+        return
+
+    logger.info(f"Decomposed {len(packets)} packets from {packet_file}.")
+
+    # List of instruments and their corresponding MET keys
+    instruments = ['SC', 'HIT', 'MAG', 'COD_LO', 'COD_HI', 'SWE', 'SWAPI']
+    instrument_coords = ['SC_SCLK_SEC', 'HIT_SC_TICK', 'MAG_ACQ', 'COD_LO_ACQ',
+                         'COD_HI_ACQ', 'SWE_ACQ_SEC', 'SWAPI_ACQ']
+
+    # Create a dictionary mapping each instrument to its time-dimension key
+    time_keys = dict(zip(instruments, instrument_coords))
+
+    # Initialize storage dictionary
+    data_storage = {inst: {} for inst in instruments}
+
+    for packet in packets:
+        for key, value in packet.data.items():
+            for inst in instruments:
+                if key.startswith(inst):
+                    if key not in data_storage[inst]:
+                        data_storage[inst][key] = []
+                    data_storage[inst][key].append(value.derived_value)
+                    break
+            else:
+                logger.warning(f"Unexpected key '{key}' found in packet data.")
+
+    logger.info("Generating datasets for each instrument.")
+
+    # Generate xarray dataset for each instrument and spacecraft
+    datasets = {}
+    for inst in instruments:
+        dataset_dict = {key: (time_keys[inst], value)
+                        for key, value in data_storage[inst].items() if
+                        key != time_keys[inst]}
+        datasets[inst] = xr.Dataset(dataset_dict, coords={
+            time_keys[inst]: data_storage[inst][time_keys[inst]]})
+
+    logger.info(f"Generated datasets for {len(datasets)} instruments.")
+
+    return datasets
--- imap_processing/ialirt/tests/unit/test_decom_ialirt.py
+++ imap_processing/ialirt/tests/unit/test_decom_ialirt.py
@@ -1,11 +1,35 @@
+import sys
+from pathlib import Path
+
 import pytest
 
-from imap_processing.ialirt.l0.decom_ialirt import decom_packets
-from imap_processing.ialirt.tests.unit.data_path_fixtures import \
-    packet_path, xtce_ialirt_path
+from imap_processing.ialirt.l0.decom_ialirt import decom_packets, generate_xarray
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
+def packet_path():
+    """Returns the raw packet directory."""
+    return Path(sys.modules[__name__.split(
+        '.')[0]].__file__).parent / 'ialirt' / 'tests' / 'test_data' / 'l0' / \
+        'IALiRT Raw Packet Telemetry.txt'
+
+
+@pytest.fixture(scope="session")
+def xtce_ialirt_path():
+    """Returns the xtce auxiliary directory."""
+    return Path(sys.modules[__name__.split(
+        '.')[0]].__file__).parent / 'ialirt' / 'packet_definitions' \
+        / "ialirt.xml"
+
+
+@pytest.fixture(scope="session")
+def decom_data(packet_path, xtce_ialirt_path):
+    """Data for decom_ultra"""
+    data_packet_list = generate_xarray(packet_path, xtce_ialirt_path)
+    return data_packet_list
+
+
+@pytest.fixture(scope="session")
 def decom_test_data(packet_path, xtce_ialirt_path):
     """Read test data from file"""
     data_packet_list = decom_packets(packet_path, xtce_ialirt_path)
@@ -23,20 +47,45 @@ def test_enumerated(decom_test_data):
 
     for packet in decom_test_data:
 
-        assert packet.data["SWAPI_STATUS"].derived_value == 'NOT_OPERATIONAL'
-        assert packet.data["MAG_STATUS"].derived_value == 'NOT_OPERATIONAL'
-        assert packet.data["HIT_STATUS"].derived_value == 'NOT_OPERATIONAL'
-        assert packet.data["CODICE_STATUS"].derived_value == 'NOT_OPERATIONAL'
-        assert packet.data["LO_STATUS"].derived_value == 'NOT_OPERATIONAL'
-        assert packet.data["HI_45_STATUS"].derived_value == 'NOT_OPERATIONAL'
-        assert packet.data["HI_90_STATUS"].derived_value == 'NOT_OPERATIONAL'
-        assert packet.data["ULTRA_45_STATUS"].derived_value == 'NOT_OPERATIONAL'
-        assert packet.data["ULTRA_90_STATUS"].derived_value == 'NOT_OPERATIONAL'
-        assert packet.data["SWE_STATUS"].derived_value == 'NOT_OPERATIONAL'
-        assert packet.data["IDEX_STATUS"].derived_value == 'NOT_OPERATIONAL'
-        assert packet.data["GLOWS_STATUS"].derived_value == 'NOT_OPERATIONAL'
-        assert packet.data["SPINPERIODVALID"].derived_value == 'INVALID'
-        assert packet.data["SPINPHASEVALID"].derived_value == 'INVALID'
-        assert packet.data["ATTITUDE"].derived_value == 'SUNSENSOR'
-        assert packet.data["CATBEDHEATERFLAG"].derived_value == 'ON'
-        assert packet.data["AUTONOMY"].derived_value == 'OPERATIONAL'
+        assert packet.data["SC_SWAPI_STATUS"].derived_value == 'NOT_OPERATIONAL'
+        assert packet.data["SC_MAG_STATUS"].derived_value == 'NOT_OPERATIONAL'
+        assert packet.data["SC_HIT_STATUS"].derived_value == 'NOT_OPERATIONAL'
+        assert packet.data["SC_CODICE_STATUS"].derived_value == 'NOT_OPERATIONAL'
+        assert packet.data["SC_LO_STATUS"].derived_value == 'NOT_OPERATIONAL'
+        assert packet.data["SC_HI_45_STATUS"].derived_value == 'NOT_OPERATIONAL'
+        assert packet.data["SC_HI_90_STATUS"].derived_value == 'NOT_OPERATIONAL'
+        assert packet.data["SC_ULTRA_45_STATUS"].derived_value == 'NOT_OPERATIONAL'
+        assert packet.data["SC_ULTRA_90_STATUS"].derived_value == 'NOT_OPERATIONAL'
+        assert packet.data["SC_SWE_STATUS"].derived_value == 'NOT_OPERATIONAL'
+        assert packet.data["SC_IDEX_STATUS"].derived_value == 'NOT_OPERATIONAL'
+        assert packet.data["SC_GLOWS_STATUS"].derived_value == 'NOT_OPERATIONAL'
+        assert packet.data["SC_SPINPERIODVALID"].derived_value == 'INVALID'
+        assert packet.data["SC_SPINPHASEVALID"].derived_value == 'INVALID'
+        assert packet.data["SC_ATTITUDE"].derived_value == 'SUNSENSOR'
+        assert packet.data["SC_CATBEDHEATERFLAG"].derived_value == 'ON'
+        assert packet.data["SC_AUTONOMY"].derived_value == 'OPERATIONAL'
+        assert packet.data["HIT_STATUS"].derived_value == 'OFF-NOMINAL'
+        assert packet.data["SWE_NOM_FLAG"].derived_value == 'OFF-NOMINAL'
+        assert packet.data["SWE_OPS_FLAG"].derived_value == 'NON-HVSCI'
+
+
+def test_decom_instruments(decom_data, decom_test_data):
+    """This function checks that all instrument parameters are accounted for."""
+
+    instrument_lengths = {}
+    instruments = ['SC', 'HIT', 'MAG', 'COD_LO', 'COD_HI', 'SWE', 'SWAPI']
+
+    # Assert that all parameters are the same
+    # between packet data and generated xarray
+    for instrument in instruments:
+        instrument_list = list(decom_data[instrument].coords)
+        instrument_list.extend(list(decom_data[instrument].data_vars))
+
+        instrument_lengths[instrument] = len(instrument_list)
+
+        assert all(param in list(decom_test_data[0].data.keys())
+                   for param in instrument_list)
+
+    # Assert that the length of parameters is the same
+    # between packet data and generated xarray
+    assert sum(instrument_lengths.values()) == len(list(decom_test_data[0].data.keys()))
