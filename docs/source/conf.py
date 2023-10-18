200ee91453d24561713caebaabd48642b037f064
Greg Lucas <greg.m.lucas@gmail.com>
2023-10-18 09:58:13 -0600

    DOC: Add intersphinx to documentation for external references

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,6 +38,7 @@ release = imap_processing.__version__
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",  # Link to other projects' documentation
     "sphinx.ext.githubpages",  # Helpful for publishing to gh-pages
     "sphinx.ext.napoleon",
     "myst_parser",
@@ -73,3 +74,11 @@ html_static_path = ["_static"]
 
 # Autosummary
 autosummary_generate = True
+
+intersphinx_mapping = {
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None),
+    "pytest": ("https://pytest.org/en/stable/", None),
+    "python": ("https://docs.python.org/3/", None),
+    "xarray": ("https://docs.xarray.dev/en/stable/", None),
+}
