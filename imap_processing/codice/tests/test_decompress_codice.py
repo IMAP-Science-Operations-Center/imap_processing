70ffbf02f5d68411dadeca2c7723989a624f9d2d
Matthew Bourque <matthew.bourque@lasp.colorado.edu>
2023-11-15 10:38:47 -0700

    Moved lookup tables to their own module; implemented some code review suggestions

--- imap_processing/codice/tests/test_decompress_codice.py
+++ imap_processing/codice/tests/test_decompress_codice.py
@@ -1,17 +1,13 @@
 """Tests the decompression algorithms for CoDICE science data"""
 
+import lzma
+
 import pytest
 
-from imap_processing.codice.l0.decompress_codice import CodiceDecompression
-
-# The value of 234 compressed using LZMA
-LZMA_EXAMPLE = (
-    b"\xfd7zXZ\x00\x00\x04\xe6\xd6\xb4F\x02\x00!\x01\x16\x00\x00\x00t/\xe5\xa3"
-    b"\x01\x00\x00\xea\x00\x00\x00\x00\x15g\x1b\xad\x02.eJ\x00\x01\x19\x01\xa5,"
-    b"\x81\xcc\x1f\xb6\xf3}\x01\x00\x00\x00\x00\x04YZ"
-)
+from imap_processing.codice.l0.decompress_codice import decompress
 
 # Test the algorithms using input value of 234 (picked randomly)
+LZMA_EXAMPLE = lzma.compress((234).to_bytes())
 TEST_DATA = [
     (234, "no compression", 234),
     (234, "lossyA", 221184),
@@ -19,7 +15,7 @@ TEST_DATA = [
     (LZMA_EXAMPLE, "lossless", 234),
     (LZMA_EXAMPLE, "lossyA+lossless", 221184),
     (LZMA_EXAMPLE, "lossyB+lossless", 1441792),
-    (None, "some_unsupported_algorithm", None),
+    pytest.param(None, "some_unsupported_algorithm", None, marks=pytest.mark.xfail()),
 ]
 
 
@@ -27,8 +23,7 @@ TEST_DATA = [
     ("compressed_value", "algorithm", "expected_result"), TEST_DATA
 )
 def test_decompress(compressed_value, algorithm, expected_result):
-    """Test the ``decompress`` method"""
+    """Tests the ``decompress`` function"""
 
-    cd = CodiceDecompression(compressed_value, algorithm)
-    cd.decompress()
-    assert cd.decompressed_value == expected_result
+    decompressed_value = decompress(compressed_value, algorithm)
+    assert decompressed_value == expected_result
