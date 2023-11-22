e802d669461c45e08954a2a7db59320d3c6dd0a5
Gabriel M <gabriel.moraga@lasp.colorado.edu>
2023-11-22 09:52:08 -0700

    updates made on eu function and added eu csv-needs work

--- imap_processing/codice/tests/test_decom.py
+++ imap_processing/codice/tests/test_decom.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import pytest
+import xarray as xr
 
 from imap_processing import imap_module_directory
 from imap_processing.codice.l0 import decom_codice
@@ -30,13 +31,13 @@ def decom_test_data():
 
 
 @pytest.fixture(scope="session")
-def validation_data():
+def raw_data():
     """Read in validation data from the CSV file
 
     Returns
     -------
-    validation_data : pandas DataFrame
-        The validation data read from the CSV, cleaned up and ready to compare
+    raw_data : pandas DataFrame
+        The raw data read from the CSV, cleaned up and ready to compare
         the decommutated packet with
     """
 
@@ -45,191 +46,78 @@ def validation_data():
         f"{imap_module_directory}/codice/tests/data/"
         f"idle_export_raw.COD_NHK_20230822_122700.csv"
     )
-    validation_data = pd.read_csv(validation_file, index_col="SHCOARSE")
+    raw_data = pd.read_csv(validation_file, index_col="SHCOARSE")
     # Remove the timestamp column and data
-    if "timestamp" in validation_data.columns:
-        validation_data.drop(columns=["timestamp"], inplace=True, errors="ignore")
+    if "timestamp" in raw_data.columns:
+        raw_data.drop(columns=["timestamp"], inplace=True, errors="ignore")
+
+    dataset = xr.Dataset()
+    dataset["First_mnemonic"] = xr.DataArray(np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
 
     # Return the data
-    return validation_data
+    return raw_data
 
 
-@pytest.fixture(scope="session")
-def eu_csv_data():
-    """Read EU data from a CSV file and filter the DataFrame to keep only columns
-    that match the mnemonics_to_process"""
-
-    # TODO: Pull mnemonics from Telemetry packet "AnalogConversions" instead of hard
-    #  coding them here
-    mnemonics_to_process = [
-        "SPIN_BIN_PERIOD",
-        "SPIN_PERIOD",
-        "SPIN_PERIOD_TIMER",
-        "OPTICS_HV_DAC_ESA_A",
-        "OPTICS_HV_DAC_ESA_B",
-        "OPTICS_HV_DAC_IONBULK",
-        "SENSOR_HV_DAC_SSDO",
-        "SENSOR_HV_DAC_SSDB",
-        "SENSOR_HV_DAC_APDB",
-        "SENSOR_HV_DAC_APDB2",
-        "SENSOR_HV_DAC_START_MCP",
-        "SENSOR_HV_DAC_STOP_MCP",
-        "SENSOR_HV_DAC_STOP_OPTICS_GRID",
-        "SBULK_VMON",
-        "SSDO_VMON",
-        "SSDB_VMON",
-        "APDB1_VMON",
-        "APDB2_VMON",
-        "IOBULK_VMON",
-        "ESAA_HI_VMON",
-        "SPARE_62",
-        "STRMCP_VMON",
-        "STPMCP_VMON",
-        "STPOG_VMON",
-        "APDB1_IMON",
-        "ESAB_HI_VMON",
-        "SPARE_68",
-        "APDB2_IMON",
-        "SSDB_IMON",
-        "STPMCP_IMON",
-        "IOBULK_IMON",
-        "STRMCP_IMON",
-        "MDM25P_14_T",
-        "MDM25P_15_T",
-        "MDM25P_16_T",
-        "MDM51P_27_T",
-        "IO_HVPS_T",
-        "LVPS_12V_T",
-        "LVPS_5V_T",
-        "LVPS_3P3V_T",
-        "LVPS_3P3V",
-        "LVPS_5V",
-        "LVPS_N5V",
-        "LVPS_12V",
-        "LVPS_N12V",
-        "LVPS_3P3V_I",
-        "LVPS_5V_I",
-        "LVPS_N5V_I",
-        "LVPS_12V_I",
-        "LVPS_N12V_I",
-        "CDH_1P5V",
-        "CDH_1P8V",
-        "CDH_3P3V",
-        "CDH_12V",
-        "CDH_N12V",
-        "CDH_5V",
-        "CDH_5V_ADC",
-        "CDH_PROCESSOR_T",
-        "CDH_1P8V_LDO_T",
-        "CDH_1P5V_LDO_T",
-        "CDH_SDRAM_T",
-        "SNSR_HVPS_T",
-    ]
-
-    filename_eu = Path(
-        f"{imap_module_directory}/codice/tests/data/idle_export_eu"
-        f".COD_NHK_20230822_122700 2.csv"
-    )
-    eu_csv = pd.read_csv(filename_eu)
-
-    # Filter the DataFrame to keep only columns that match the mnemonics_to process
-    filtered_eu_data = eu_csv[mnemonics_to_process]
-
-    return filtered_eu_data
-
-
-@pytest.fixture(scope="session")
-def read_and_filter_analog_data():
-    """Read and filter analog data from an Excel file.
-
-    Returns
-    -------
-    filtered_analog_data : pandas DataFrame
-        The filtered analog data
-    """
-
-    # TODO: Change the path to the Excel file once the new TEL document is updated
-    excel_file_path = Path(
-        "/Users/gamo6782/Desktop/repos/imap_processing/tools"
-        "/xtce_generation/TLM_COD.xlsx"
-    )
-
-    # Read analog data from an Excel file into a pandas DataFrame
-    analog = pd.read_excel(excel_file_path, sheet_name="AnalogConversions")
-
-    # Filterd analog data to keep only rows with 'packetName' as 'COD_NHK'
-    filtered_analog = analog[analog["packetName"] == "COD_NHK"].copy()
-
-    # List of columns to remove
-    columns_to_remove = [
-        "packetName",
-        "convertAs",
-        "segNumber",
-        "lowValue",
-        "highValue",
-    ]
-
-    # Drop the specified columns
-    filtered_analog.drop(columns=columns_to_remove, inplace=True)
-
-    # Convert all string columns to uppercase
-    # TODO: This will be replaced once the new TEL document is updated
-    filtered_analog = filtered_analog.applymap(
-        lambda x: x.upper() if isinstance(x, str) else x
-    )
-
-    return filtered_analog
-
-
-@pytest.fixture(scope="session")
-def validate_unit_conversion(validation_data, filtered_analog_data):
-    """Convert validation data to engineering units using the coefficients from the
-    AnalogConversions packet and compare it with filtered_analog_data.
+def convert_raw_to_eu(dataset: xr.Dataset, packet_name):
+    """Convert raw data to engineering unit.
 
     Parameters
     ----------
-    validation_data : pandas DataFrame
-        The validation data with raw values
-    filtered_analog_data : pandas DataFrame
-        The DataFrame containing coefficients for unit conversion
+    dataset : xr.Dataset
+        Raw data.
+    packet_name: str
+        Packet name
 
     Returns
     -------
-    engineering_data : pandas DataFrame
-        The DataFrame with engineering unit values
+    xr.Dataset
+        Raw data converted to engineering unit as needed.
     """
-    engineering_data = pd.DataFrame(
-        np.nan, index=validation_data.index, columns=validation_data.columns
+    print(f"Type of 'dataset': {type(dataset)}")  # Debugging line
+
+    conversion_table_path = (
+        Path(imap_module_directory) / "codice/tests/data/eu_unit_lookup_table.csv"
     )
 
-    for mnemonic in validation_data.columns:
-        matching_row = filtered_analog_data[filtered_analog_data.index == mnemonic]
+    # Make sure there is a column called "index" with unique
+    # value such as 0, 1, 2, 3, ...
+    eu_conversion_table = pd.read_csv(
+        conversion_table_path,
+        index_col="index",
+    )
 
-        if not matching_row.empty:
-            coefficients = matching_row[
-                ["c0", "c1", "c2", "c3", "c4", "c5", "c6", "c7"]
-            ].values[0]
+    # Look up all metadata fields for the packet name
+    metadata_list = eu_conversion_table.loc[
+        eu_conversion_table["packetName"] == packet_name
+    ]
 
-            raw_data = validation_data[mnemonic]
+    # for each metadata field, convert raw value to engineering unit
+    for field in metadata_list.index:
+        metadata_field = metadata_list.loc[field]["mnemonic"]
 
-            # Handle NaN values by excluding them from the calculations
-            mask = ~np.isnan(raw_data)
+        # Debugging line
+        print(f"Processing metadata field: {metadata_field}")
 
-            # Create a polynomial using the coefficients
-            poly = np.polynomial.Polynomial(coefficients)
+        # Check if the metadata_field is present in the dataset
+        if metadata_field in dataset:
+            # On this line, we are getting the coefficients from the
+            # table and then reverse them because np.polyval is
+            # expecting coefficients in descending order
+            coeff_values = metadata_list.loc[
+                metadata_list["mnemonic"] == metadata_field
+            ].values[0][6:][::-1]
 
-            # Evaluate the polynomial on the raw data
-            engineering_unit_data = poly(raw_data[mask])
+            # Convert the raw value to engineering unit
+            dataset[metadata_field].data = np.polyval(
+                coeff_values, dataset[metadata_field].data
+            )
+        else:
+            print(f"Warning: Metadata field {metadata_field} not found in the dataset.")
 
-            # Assign the result to the engineering_data DataFrame
-            engineering_data.loc[mask, mnemonic] = engineering_unit_data
-
-            print(f"Converted {mnemonic} to engineering units")
-
-    return engineering_data
+    return dataset
 
 
-def test_housekeeping_data(decom_test_data, validation_data):
+def test_housekeeping_data(decom_test_data, raw_data):
     """Compare the decommutated housekeeping data to the validation data.
 
     Parameters
@@ -244,7 +132,7 @@ def test_housekeeping_data(decom_test_data, validation_data):
     first_data = decom_test_data[0]
 
     # Get the corresponding row in validation_data based on the "SHCOARSE" value
-    validation_row = validation_data.loc[first_data.data["SHCOARSE"].raw_value]
+    validation_row = raw_data.loc[first_data.data["SHCOARSE"].raw_value]
 
     # Compare raw values of housekeeping data
     for key, value in first_data.data.items():
@@ -273,41 +161,29 @@ def test_ways_to_get_data(decom_test_data):
     assert data_value_using_key == data_value_using_list
 
 
-# TODO: Add test for unit conversion of engineering data and eu data
-def test_unit_conversion_coefficients(filtered_analog_data, validation_data):
-    """Test if the coefficients in engineering data match the units in EU data
-    by mnemonics.
+def test_derived_eu_data(decom_test_data, raw_data):
+    """Test the derived engineering unit data.
 
     Parameters
     ----------
-    filtered_analog_data : pandas DataFrame
-        The DataFrame containing coefficients for unit conversion
-    validation_data : pandas DataFrame
-        The validation data with raw values
+    decom_test_data : List[Packet]
+        The decommuted housekeeping packet data
+    raw_data : pandas DataFrame
+        The raw validation data
     """
-    # TODO: Call the function to perform unit conversion
-    engineering_data = validate_unit_conversion(validation_data, filtered_analog_data)
 
-    # Ensure that the engineering data has the same mnemonics as filtered_eu_data
-    assert set(engineering_data.columns) == set(filtered_analog_data.index)
+    # Assuming 'First_mnemonic' is the first derived EU mnemonic, modify accordingly
+    derived_eu_data_key = "First_mnemonic"
 
-    # Check if coefficients in engineering data match the units in EU data by mnemonics
-    for mnemonic in engineering_data.columns:
-        # Get the corresponding row in filtered_analog_data based on the mnemonic
-        matching_row = filtered_analog_data[filtered_analog_data.index == mnemonic]
+    # Take the first decom_packet
+    first_data = decom_test_data[0]
 
-        # Check if the row is not empty
-        assert not matching_row.empty
+    # Check if the derived EU data is present in the decommuted data
+    assert derived_eu_data_key in first_data.data
 
-        # Extract coefficients from filtered_analog_data
-        coefficients_from_filtered_analog = matching_row[
-            ["c0", "c1", "c2", "c3", "c4", "c5", "c6", "c7"]
-        ].values[0]
+    # Get the corresponding value from the raw validation data
+    validation_row = raw_data.loc[first_data.data["SHCOARSE"].raw_value]
+    validation_value = validation_row[derived_eu_data_key]
 
-        # Extract coefficients from engineering_data
-        coefficients_from_engineering_data = engineering_data.loc[:, mnemonic].values
-
-        # Check if the coefficients match
-        np.testing.assert_array_almost_equal(
-            coefficients_from_engineering_data, coefficients_from_filtered_analog
-        )
+    # Compare the derived EU data values
+    assert first_data.data[derived_eu_data_key].raw_value == validation_value
