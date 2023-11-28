fa50e8e75ccea37bf7e24b25b3753f47b8b1326f
Matthew Bourque <matthew.bourque@lasp.colorado.edu>
2023-11-28 15:52:44 -0700

    Updates to reflect changes in module/function names and reorg; updated some docstrings as well

--- imap_processing/codice/codice_l0.py
+++ imap_processing/codice/codice_l0.py
@@ -1,3 +1,20 @@
+"""Perform CoDICE L0 processing.
+
+This module contains a function to decommutate CoDICE CCSDS packets using
+XTCE packet definitions.
+
+For more information on this process and the latest versions of the packet
+definitions, see https://lasp.colorado.edu/galaxy/display/IMAP/CoDICE.
+
+Use
+---
+
+    from imap_processing.codice.codice_l0 import decom_packets
+    packet_file = '/path/to/raw_ccsds_20230822_122700Z_idle.bin'
+    xtce_document = '/path/to/P_COD_NHK.xml'
+    packet_list = decom_packets(packet_file, xtce_document)
+"""
+
 from imap_processing import decom, imap_module_directory
 
 
