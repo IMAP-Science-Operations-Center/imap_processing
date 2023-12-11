f03e02fa5fe6d579fdd0fd6af2d756f08b9e1712
Sean Hoyt <sean.hoyt@lasp.colorado.edu>
2023-12-11 14:56:50 -0700

    added TODOs

--- imap_processing/lo/l0/science_direct_events.py
+++ imap_processing/lo/l0/science_direct_events.py
@@ -1,12 +1,59 @@
 from dataclasses import dataclass
+from itertools import compress
 
 import imap_processing.lo.l0.compression_tables as ct
 from imap_processing.ccsds.ccsds_data import CcsdsData
 from imap_processing.lo.l0.lol0 import LoL0
 
+# TODO: Talk to Colin to get better names for tables
+
 
 @dataclass
 class ScienceDirectEvents(LoL0):
+    """L1A Science Drect Events data.
+
+    The Science Direct Events class handles the parsing and
+    decompression of L0 to L1A data.
+
+    Attributes
+    ----------
+    SHCOARSE : int
+        Spacecraft time
+    COUNT: int
+        Number of direct events
+    DATA: str
+        Compressed TOF Direct Event time tagged data.
+    TOF0: int
+        Time of Flight 0 value for direct event.
+    TOF1: int
+        Time of Flight 1 value for direct event.
+    TOF2: int
+        Time of Flight 2 value for direct event.
+    TOF3: int
+        Time of Flight 3 value for direct event.
+    TIME: int
+        time tag for the direct event
+        #TODO: Is this different than the SHCOARSE?
+    ENERGY: int
+        energy of the direct event ENA.
+    POS: int
+        Position of the direct event ENA
+        #TODO: Is this the position on the final detector?
+    CKSM: int
+        #TODO: There's a checksum in the packet and another in
+        the dedcompressed data? Are these different?
+
+    Methods
+    -------
+    __init__(packet, software_vesion, packet_file_name):
+        Constructor, uses the CCSDS packet, version of the software, and
+        the name of the packet file to parse and store information about
+        the Direct Event packet data.
+    decompress_data():
+        Decompresses the Science Direct Event TOF  data.
+
+    """
+
     SHCOARSE: int
     COUNT: int
     DATA: str
@@ -15,41 +62,85 @@ class ScienceDirectEvents(LoL0):
     TOF1: int
     TOF2: int
     TOF3: int
+    TIME: int
+    ENERGY: int
+    POS: int
+    # TODO: Maybe all the below variables should be local variables instead of
+    # instance variables for the class to keep all the instance variables data
+    # fields from the packet?
     case_number: int
-    DATA_DECOMPRESSED: str
     binary_strings: list
     tof_decoder: list
+    remaining_bits: dict
+    parsed_bits: list
 
     def __init__(self, packet, software_version: str, packet_file_name: str):
         super().__init__(software_version, packet_file_name, CcsdsData(packet.header))
         self.parse_data(packet)
 
     def decompress_data(self):
+        """Decompress the Lo Science Direct Events data."""
         self._find_decompression_case()
         self._find_bit_length_for_case()
         self._find_binary_strings()
-
-        # separate data bits into its parts
-        parse_bits = {}
-        bit_start = 0
-        for field, bit_length in self.tof_decoder.items():
-            parse_bits[field] = self.DATA[bit_start:bit_length]
-            bit_start = bit_start + bit_length
+        self._find_remaining_bits()
+        self._parse_binary()
+        self._decode_fields()
 
     def _find_bit_length_for_case(self):
+        # The case number determines which bit length table to use.
+        # This table shows how the TOF bits should be parsed in the
+        # binary data.
         self.tof_decoder = ct.tof_bit_length_table[self.case_number]
 
     def _find_binary_strings(self):
-        hex_strings = ct.other_decoder[self.case_number]
-        self.binary_strings = [
-            self._hexadecimal_to_binary(hex_string)
-            for hex_string in hex_strings
-            if hex_string != ""
-        ]
+        # TODO: Not sure how I can describe the "hex_table" better.
+        # Ask Colin for a better name suggestions for this table.
+        hex_strings = ct.hex_table[self.case_number]
+        self.binary_strings = {
+            field: self._hexadecimal_to_binary(hex_string) if hex_string else ""
+            for field, hex_string in hex_strings.items()
+        }
 
     def _hexadecimal_to_binary(self, hex_string: str):
-        return bin(int(hex_string))[2:]
+        # TODO: Is 16 bits accurate here? Will this change depending
+        # on the Row/Column of the hex?
+        return bin(int(hex_string, 16))[2:].zfill(16)
 
     def _find_decompression_case(self):
         # TODO: Do I read the binary left to right or right to left?
         self.case_number = ct.tof_case_table[self.DATA[0:4]]
+
+    def _find_remaining_bits(self):
+        self.remaining_bits = {}
+        for field, binary_string in self.binary_strings.items():
+            bit_list = [bool(int(bit)) for bit in list(binary_string)]
+            second_tof_table = ct.another_tof_table
+            # TODO: Temporary: Need to figure out how to handle different lengths
+            if bit_list != []:
+                bit_list = bit_list[-12:]
+            ########
+            self.remaining_bits[field] = list(compress(second_tof_table, bit_list))
+
+    def _parse_binary(self):
+        # separate data bits into its parts
+        self.parsed_bits = {}
+        bit_start = 0
+        for field, bit_length in self.tof_decoder.items():
+            self.parsed_bits[field] = self.DATA[bit_start : bit_start + bit_length]
+            bit_start = bit_start + bit_length
+
+    def _decode_fields(self):
+        for field, bits in self.parsed_bits.items():
+            # TODO: Temporary - figure out how to handled lengths not matching
+            # TODO: What about TIME with 16bits and there are only 12 possible remaining
+            # bits columns in the second table?
+            remaining_bits = self.remaining_bits[field][-len(bits) :]
+            bit_list = [int(bit) for bit in list(bits)]
+            decompressed_data = sum(
+                bit[0] * bit[1] for bit in zip(bit_list, remaining_bits)
+            )
+            # TODO: Is it better to explicitly set the TOF fields? There
+            # shouldn't be any invalid names because the field names are
+            # coming from compression_tables.py
+            setattr(self, field, decompressed_data)
---
+++ imap_processing/lo/tests/test_science_direct_events.py
@@ -0,0 +1,185 @@
+from imap_processing.lo.l0.science_direct_events import ScienceDirectEvents
+
+# from imap_processing import imap_module_directory
+
+
+def test_find_decompression_case():
+    # Arrange
+    de = ScienceDirectEvents("fake_packet", "0", "fakepacketname")
+    de.DATA = "000100010101"
+    case_number_expected = 1
+
+    # Act
+    de._find_decompression_case()
+    case_number_true = de.case_number
+
+    # Assert
+    assert case_number_true == case_number_expected
+
+
+def test_hexadecimal_to_binary():
+    # Arrange
+    de = ScienceDirectEvents("fake_packet", "0", "fakepacketname")
+    de.DATA = "000100010101"
+    binary_string_expected = "0000011111111110"
+
+    # Act
+    binary_string_true = de._hexadecimal_to_binary("0x07FE")
+
+    # Assert
+    assert binary_string_true == binary_string_expected
+
+
+def test_find_bit_length_for_case():
+    # Arrange
+    de = ScienceDirectEvents("fake_packet", "0", "fakepacketname")
+    de.DATA = "000100010101"
+    tof_decoder_expected = {
+        "ENERGY": 3,
+        "POS": 0,
+        "TOF0": 10,
+        "TOF1": 9,
+        "TOF2": 9,
+        "TOF3": 0,
+        "CKSM": 0,
+        "TIME": 16,
+    }
+    de._find_decompression_case()
+
+    # Act
+    de._find_bit_length_for_case()
+    tof_decoder_true = de.tof_decoder
+
+    # Assert
+    assert tof_decoder_true == tof_decoder_expected
+
+
+def test_find_binary_strings():
+    # Arrange
+    de = ScienceDirectEvents("fake_packet", "0", "fakepacketname")
+    de.DATA = "000100010101"
+    de._find_decompression_case()
+    binary_strings_expected = {
+        "TIME": "0000111111111111",
+        "TOF0": "0000011111111110",
+        "TOF1": "0000011111111100",
+        "TOF2": "0000001111111110",
+        "TOF3": "",
+        "POS": "",
+        "CKSM": "",
+    }
+
+    # Act
+    de._find_binary_strings()
+    binary_strings_true = de.binary_strings
+
+    # Assert
+    assert binary_strings_true == binary_strings_expected
+
+
+def test_find_remaining_bits():
+    # Arrange
+    de = ScienceDirectEvents("fake_packet", "0", "fakepacketname")
+    de.DATA = "000100010101"
+    de._find_decompression_case()
+    de._find_binary_strings()
+    remaining_bits_expected = {
+        "TIME": [
+            327.68,
+            163.84,
+            81.82,
+            40.96,
+            20.48,
+            10.24,
+            5.12,
+            2.56,
+            1.28,
+            0.64,
+            0.32,
+            0.16,
+        ],
+        "TOF0": [
+            163.84,
+            81.82,
+            40.96,
+            20.48,
+            10.24,
+            5.12,
+            2.56,
+            1.28,
+            0.64,
+            0.32,
+        ],
+        "TOF1": [
+            163.84,
+            81.82,
+            40.96,
+            20.48,
+            10.24,
+            5.12,
+            2.56,
+            1.28,
+            0.64,
+        ],
+        "TOF2": [
+            81.82,
+            40.96,
+            20.48,
+            10.24,
+            5.12,
+            2.56,
+            1.28,
+            0.64,
+            0.32,
+        ],
+        "TOF3": [],
+        "POS": [],
+        "CKSM": [],
+    }
+
+    # Act
+    de._find_remaining_bits()
+    remaining_bits_true = de.remaining_bits
+
+    # Assert
+    assert remaining_bits_true == remaining_bits_expected
+
+
+def test_parse_binary():
+    # Arrange
+    de = ScienceDirectEvents("fake_packet", "0", "fakepacketname")
+    de.DATA = "00010001010100110101110011110011101110100110100"
+    de._find_decompression_case()
+    de._find_bit_length_for_case()
+    de._find_binary_strings()
+    parsed_bits_expected = {
+        "ENERGY": "000",
+        "POS": "",
+        "TOF0": "1000101010",
+        "TOF1": "011010111",
+        "TOF2": "001111001",
+        "TOF3": "",
+        "CKSM": "",
+        "TIME": "1101110100110100",
+    }
+
+    # Act
+    de._parse_binary()
+    parsed_bits_true = de.parsed_bits
+
+    # Assert
+    assert parsed_bits_true == parsed_bits_expected
+
+
+def test_decode_fields():
+    # Arrange
+    de = ScienceDirectEvents("fake_packet", "0", "fakepacketname")
+    de.DATA = "00010001010100110101110011110011101110100110100"
+    de._find_decompression_case()
+    de._find_bit_length_for_case()
+    de._find_binary_strings()
+    de._parse_binary()
+
+    # Act
+
+    # Assert
