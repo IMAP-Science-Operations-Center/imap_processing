cd2a359951b3e4bb35dced0b4c728f73dbe42f1b
Matthew Bourque <matthew.bourque@lasp.colorado.edu>
2023-12-13 09:53:43 -0700

    Updates to fix pre-commit hook checks

--- imap_processing/run_processing.py
+++ imap_processing/run_processing.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-"""Serves as a command line utility to run the processing for a specific
-instrument and data level.
+
+"""Run the processing for a specific instrument & data level.
+
+This module serves as a command line utility to invoke the processing for
+a user-supplied instrument and data level.
 
 Use
 ---
@@ -43,8 +46,7 @@ def _parse_args():
 
 
 def _validate_args(args):
-    """Ensure that the provided arguments are valid before kicking off the
-    processing.
+    """Ensure that the  arguments are valid before kicking off the processing.
 
     Parameters
     ----------
@@ -57,7 +59,8 @@ def _validate_args(args):
         )
     if args.level not in processing_levels[args.instrument]:
         raise ValueError(
-            f"{args.level} is not a supported data level for the {args.instrument} instrument, valid levels are: {processing_levels[args.instrument]}"
+            f"{args.level} is not a supported data level for the {args.instrument}"
+            f" instrument, valid levels are: {processing_levels[args.instrument]}"
         )
 
 
@@ -70,8 +73,11 @@ class ProcessInstrument(ABC):
         The data level to process (e.g. ``l1a``)
     """
 
+    def __init__(self, level):
+        self.level = level
+
     @abstractmethod
-    def process(level):
+    def process(self):
         """Perform instrument specific processing."""
         raise NotImplementedError
 
@@ -79,81 +85,81 @@ class ProcessInstrument(ABC):
 class Codice(ProcessInstrument):
     """Process CoDICE."""
 
-    def process(level):
+    def process(self):
         """Perform CoDICE specific processing."""
-        print(f"Processing CoDICE {level}")
+        print(f"Processing CoDICE {self.level}")
 
 
 class Glows(ProcessInstrument):
     """Process GLOWS."""
 
-    def process(level):
+    def process(self):
         """Perform GLOWS specific processing."""
-        print(f"Processing GLOWS {level}")
+        print(f"Processing GLOWS {self.level}")
 
 
 class Hi(ProcessInstrument):
     """Process IMAP-Hi."""
 
-    def process(level):
+    def process(self):
         """Perform IMAP-Hi specific processing."""
-        print(f"Processing IMAP-Hi {level}")
+        print(f"Processing IMAP-Hi {self.level}")
 
 
 class Hit(ProcessInstrument):
     """Process HIT."""
 
-    def process(level):
+    def process(self):
         """Perform HIT specific processing."""
-        print(f"Processing HIT {level}")
+        print(f"Processing HIT {self.level}")
 
 
 class Idex(ProcessInstrument):
     """Process IDEX."""
 
-    def process(level):
+    def process(self):
         """Perform IDEX specific processing."""
-        print(f"Processing IDEX {level}")
+        print(f"Processing IDEX {self.level}")
 
 
 class Lo(ProcessInstrument):
     """Process IMAP-Lo."""
 
-    def process(level):
+    def process(self):
         """Perform IMAP-Lo specific processing."""
-        print(f"Processing IMAP-Lo {level}")
+        print(f"Processing IMAP-Lo {self.level}")
 
 
 class Mag(ProcessInstrument):
     """Process MAG."""
 
-    def process(level):
+    def process(self):
         """Perform MAG specific processing."""
-        print(f"Processing MAG {level}")
+        print(f"Processing MAG {self.level}")
 
 
 class Swapi(ProcessInstrument):
     """Process SWAPI."""
 
-    def process(level):
+    def process(self):
         """Perform SWAPI specific processing."""
-        print(f"Processing SWAPI {level}")
+        print(f"Processing SWAPI {self.level}")
 
 
 class Swe(ProcessInstrument):
     """Process SWE."""
 
-    def process(level):
+    def process(self):
         """Perform SWE specific processing."""
-        print(f"Processing SWE {level}")
+        print(f"Processing SWE {self.level}")
 
 
 class Ultra(ProcessInstrument):
     """Process IMAP-Ultra."""
 
-    def process(level):
+    def process(self):
         """Perform IMAP-Ultra specific processing."""
-        print(f"Processing IMAP-Ultra {level}")
+        print(f"Processing IMAP-Ultra {self.level}")
 
 
 if __name__ == "__main__":
@@ -162,4 +168,5 @@ if __name__ == "__main__":
 
     # Determine which function to invoke
     cls = getattr(sys.modules[__name__], args.instrument.capitalize())
-    cls.process(args.level)
+    instrument = cls(args.level)
+    instrument.process()
