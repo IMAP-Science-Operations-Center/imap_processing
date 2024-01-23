14b4f8129f748f5f6236b0d3b089d85fd9bdff56
Greg Lucas <greg.m.lucas@gmail.com>
2024-01-23 15:30:19 -0700

    MNT: Consolidate fixtures into a single mock

--- imap_processing/tests/test_io.py
+++ imap_processing/tests/test_io.py
@@ -10,7 +10,7 @@ import imap_processing.io
 
 
 @pytest.fixture()
-def mock_download_urlopen():
+def mock_urlopen():
     """Mock urlopen to return a file-like object."""
     mock_data = b"Mock file content"
     with patch("urllib.request.urlopen") as mock_urlopen:
@@ -19,28 +19,10 @@ def mock_download_urlopen():
         yield mock_urlopen
 
 
-@pytest.fixture()
-def mock_query_urlopen():
-    """Mock urlopen to return a file-like object."""
-    # Mock data for response, empty list
-    mock_data = json.dumps([]).encode("utf-8")
-
-    with patch("urllib.request.urlopen") as mock_urlopen:
-        mock_response = mock_urlopen.return_value.__enter__.return_value
-        mock_response.read.return_value = mock_data
-        yield mock_urlopen
-
-
-@pytest.fixture()
-def mock_upload_urlopen():
-    """Mock urlopen to return a file-like object."""
-    # Mock data for response, empty list
-    mock_data = b"https://s3-test-bucket.com"
-
-    with patch("urllib.request.urlopen") as mock_urlopen:
-        mock_response = mock_urlopen.return_value.__enter__.return_value
-        mock_response.read.return_value = mock_data
-        yield mock_urlopen
+def _set_mock_data(mock_urlopen, data):
+    """Set the data returned by the mock urlopen."""
+    mock_response = mock_urlopen.return_value.__enter__.return_value
+    mock_response.read.return_value = data
 
 
 @pytest.mark.parametrize(
@@ -55,7 +37,7 @@ def mock_upload_urlopen():
         ("imap/test/config/file.txt", "imap/test/config/file.txt"),
     ],
 )
-def test_download(mock_download_urlopen, filepath, destination):
+def test_download(mock_urlopen, filepath, destination):
     # Call the download function
     result = imap_processing.io.download(filepath)
 
@@ -70,10 +52,10 @@ def test_download(mock_download_urlopen, filepath, destination):
         assert f.read() == b"Mock file content"
 
     # Should have only been one call to urlopen
-    mock_download_urlopen.assert_called_once()
+    mock_urlopen.assert_called_once()
 
     # Assert that the correct URL was used for the download
-    urlopen_calls = mock_download_urlopen.mock_calls
+    urlopen_calls = mock_urlopen.mock_calls
     # Check the arguments passed to urlopen
     # We pass a Request object, so need to get that with args[0]
     request_sent = urlopen_calls[0].args[0]
@@ -85,7 +67,7 @@ def test_download(mock_download_urlopen, filepath, destination):
     assert request_sent.method == "GET"
 
 
-def test_download_already_exists(mock_download_urlopen):
+def test_download_already_exists(mock_urlopen):
     # Call the download function
     filepath = "a/b.txt"
     # set up the destination and create a file
@@ -96,7 +78,7 @@ def test_download_already_exists(mock_download_urlopen):
     result = imap_processing.io.download(filepath)
     assert result == destination
     # Make sure we didn't make any requests
-    assert mock_download_urlopen.call_count == 0
+    assert mock_urlopen.call_count == 0
 
 
 @pytest.mark.parametrize(
@@ -115,42 +97,47 @@ def test_download_already_exists(mock_download_urlopen):
         {"instrument": "test-instrument", "level": "test-level"},
     ],
 )
-def test_query(mock_query_urlopen, query_params):
+def test_query(mock_urlopen, query_params):
+    _set_mock_data(mock_urlopen, json.dumps([]).encode("utf-8"))
     response = imap_processing.io.query(**query_params)
     # No data found, and JSON decoding works as expected
     assert response == list()
 
     # Should have only been one call to urlopen
-    mock_query_urlopen.assert_called_once()
+    mock_urlopen.assert_called_once()
     # Assert that the correct URL was used for the query
-    urlopen_call = mock_query_urlopen.mock_calls[0].args[0]
+    urlopen_call = mock_urlopen.mock_calls[0].args[0]
     called_url = urlopen_call.full_url
     expected_url_encoded = f"https://api.test.com/query?{urlencode(query_params)}"
     assert called_url == expected_url_encoded
 
 
-def test_query_no_params(mock_query_urlopen):
+def test_query_no_params(mock_urlopen):
     with pytest.raises(ValueError, match="At least one query"):
         imap_processing.io.query()
     # Should not have made any calls to urlopen
-    assert mock_query_urlopen.call_count == 0
+    assert mock_urlopen.call_count == 0
 
 
-def test_upload_no_file():
+def test_upload_no_file(mock_urlopen):
     path = Path("/non-existant/file.txt")
     assert not path.exists()
     with pytest.raises(FileNotFoundError):
         imap_processing.io.upload(path)
+    # Should not have made any calls to urlopen
+    assert mock_urlopen.call_count == 0
 
 
-def test_upload_not_relative_to_base(monkeypatch):
+def test_upload_not_relative_to_base(monkeypatch, mock_urlopen):
     # Change the base directory to something else temporarily
     monkeypatch.setitem(imap_processing.config, "DATA_DIR", Path.cwd() / "/a/b/c")
     with pytest.raises(ValueError, match="File"):
         imap_processing.io.upload(Path(__file__))
+    assert mock_urlopen.call_count == 0
 
 
-def test_upload(mock_upload_urlopen):
+def test_upload(mock_urlopen):
+    _set_mock_data(mock_urlopen, b"https://s3-test-bucket.com")
     # Call the upload function
     file_to_upload = imap_processing.config["DATA_DIR"] / "test-file.txt"
     with open(file_to_upload, "wb") as f:
@@ -161,13 +148,13 @@ def test_upload(mock_upload_urlopen):
     # Should have been two calls to urlopen
     # 1. To get the s3 upload url
     # 2. To upload the file to the url returned in 1.
-    assert mock_upload_urlopen.call_count == 2
-    print(mock_upload_urlopen.mock_calls)
+    assert mock_urlopen.call_count == 2
+
     # We get all returned calls, but we only need the calls
     # where we sent requests
     mock_calls = [
         call
-        for call in mock_upload_urlopen.mock_calls
+        for call in mock_urlopen.mock_calls
         if len(call.args) and isinstance(call.args[0], Request)
     ]
 
