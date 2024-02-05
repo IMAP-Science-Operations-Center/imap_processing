e6e3d056bf03a214fd46008988b23ec2b925bce0
Maxine Hartnett <maxine.hartnett@lasp.colorado.edu>
2024-02-05 16:26:34 -0700

    Update CLI to include MAG file generation

--- imap_processing/mag/l1a/mag_l1a.py
+++ imap_processing/mag/l1a/mag_l1a.py
@@ -5,7 +5,7 @@ from imap_processing.cdf.utils import write_cdf
 from imap_processing.mag.l0 import decom_mag
 
 
-def mag_l1a(packet_filepath, output_filepath):
+def mag_l1a(packet_filepath, cdf_filepath):
     """
     Process MAG L0 data into L1A CDF files at cdf_filepath.
 
@@ -13,11 +13,11 @@ def mag_l1a(packet_filepath, output_filepath):
     ----------
     packet_filepath:
         Packet files for processing
-    output_filepath:
+    cdf_filepath:
         Directory for output
     """
     mag_l0 = decom_mag.decom_packets(packet_filepath)
 
     mag_datasets = decom_mag.export_to_xarray(mag_l0)
 
-    write_cdf(mag_datasets, Path(output_filepath))
+    write_cdf(mag_datasets, Path(cdf_filepath))
