7b9f2855dbee0bba4e29e66593bb8112677a5d05
Matthew Bourque <matthew.bourque@lasp.colorado.edu>
2024-02-13 12:58:50 -0700

    Fixed path issue in HIT tests

--- imap_processing/tests/hit/test_hit_decom.py
+++ imap_processing/tests/hit/test_hit_decom.py
@@ -1,15 +1,14 @@
 import pytest
 
+from imap_processing import imap_module_directory
 from imap_processing.hit.l0 import hit_l1a_decom
 
 
 @pytest.fixture(scope="session")
 def decom_test_data():
     """Read test data from file"""
-    packet_file = (
-        "imap_processing/tests/hit/PREFLIGHT_raw_record_2023_256_15_59_04_apid1251.pkts"
-    )
-    xtce = "imap_processing/hit/packet_definitions/P_HIT_HSKP.xml"
+    packet_file = f"{imap_module_directory}/tests/hit/PREFLIGHT_raw_record_2023_256_15_59_04_apid1251.pkts"  # noqa
+    xtce = f"{imap_module_directory}/hit/packet_definitions/P_HIT_HSKP.xml"
     data_packet_list = hit_l1a_decom.decom_hit_packets(packet_file, xtce)
     return data_packet_list
 
