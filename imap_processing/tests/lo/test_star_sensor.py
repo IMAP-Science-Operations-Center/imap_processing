a0ac2e90431245287c71c4a6e9ac6732b968aebb
Sean Hoyt <sean.hoyt@lasp.colorado.edu>
2024-02-13 10:03:11 -0700

    comment fixes

--- imap_processing/tests/lo/test_star_sensor.py
+++ imap_processing/tests/lo/test_star_sensor.py
@@ -1,10 +1,12 @@
+import pytest
+
 from imap_processing.lo.l0.data_classes.star_sensor import StarSensor
 
 
 # Going to wait until validation data is available to test that
 # the values are correct. Currently only check for the shape
 # of the resulting data.
-# @pytest.mark.skip(reason="no data to initialize with")
+@pytest.mark.skip(reason="no data to initialize with")
 def test_science_counts():
     ## Arrange
     sc = StarSensor("fake_packet", "version", "packet_name")
