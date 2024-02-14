746def6e178b20e8af41007e9dd230189d334424
Maxine Hartnett <maxine.hartnett@lasp.colorado.edu>
2024-02-14 15:28:23 -0700

    Updating CLI, finishing mag and swe calls

--- imap_processing/cli.py
+++ imap_processing/cli.py
@@ -11,12 +11,11 @@ Use
 """
 
 import argparse
-import os
-import re
 import sys
 from abc import ABC, abstractmethod
 from json import loads
 from pathlib import Path
+from urllib.error import HTTPError
 
 import imap_data_access
 from cdflib.xarray import cdf_to_xarray
@@ -37,54 +36,26 @@ from imap_processing.swe.l1a.swe_l1a import swe_l1a
 from imap_processing.swe.l1b.swe_l1b import swe_l1b
 
 
-def extract_filename_components(filename: str):
-    """
-    Extract all components from filename.
-
-    Parameters
-    ----------
-    filename : str
-        Path of dependency data.
-
-    Returns
-    -------
-    components : dict
-        Dictionary containing components.
-
-    """
-    pattern = (
-        r"^imap_"
-        r"(?P<instrument>[^_]+)_"
-        r"(?P<datalevel>[^_]+)_"
-        r"(?P<descriptor>[^_]*)_?"  # optional
-        r"(?P<startdate>\d{8})_"
-        r"(?P<enddate>\d{8})_"
-        r"(?P<version>v\d{2}-\d{2})"
-        r"\.(cdf|pkts)$"
-    )
-    match = re.match(pattern, filename)
-    if match is None:
-        return
-    components = match.groupdict()
-    return components
-
-
 def _parse_args():
     """Parse the command line arguments.
 
     The expected input format is:
-    "Command": [
-        "--instrument",
-        "swe",
-        "--level",
-        "l1b",
-        "--time",
-        "(2023-09-27, 2023-09-27)",
-        "--file_path",
-        "imap/swe/l1b/2023/09/imap_swe_l1b_lveng-hk_20230927_20230927_v01-00.cdf",
-        "--dependency",
-        "[{'instrument': 'swe', 'level': 'l0', 'version': 'v00-01'}]"
-      ]
+    TODO: should dependency dict use file path, or instrument/datalevel/version/date?
+    --instrument "mag"
+    --level "l1a"
+    --start_date "20231212"
+    --end_date "20231212"
+    --version "v00-01"
+    --dependency \"[
+        {
+            'instrument': 'mag',
+            'data_level': 'l0',
+            'descriptor': 'sci',
+            'version': 'v00-01',
+            'start_date': '20231212',
+            'end_date': '20231212'
+        }]\"
+    --use-remote
 
     Returns
     -------
@@ -95,12 +66,6 @@ def _parse_args():
         "This command line program invokes the processing pipeline "
         "for a specific instrument and data level. Example usage: "
         '"python run_processing --instrument swe --level l1a".'
-    )
-    data_dir_help = (
-        "Directory to use for reading and writing IMAP data. "
-        "The default is an 'imap-data/' folder in the "
-        "current working directory. This can also be "
-        "set using the IMAP_DATA_DIR environment variable."
     )
     instrument_help = (
         "The instrument to process. Acceptable values are: "
@@ -127,20 +92,49 @@ def _parse_args():
     #     required=True,
     #     help="Full path to the file in the S3 bucket.",
     # )
+    # TODO: Remove this dependency from batch_starter, then remove it from here.
     parser.add_argument(
         "--file_path",
         type=str,
-        required=True,
+        required=False,
         help="Full path to the output file in the S3 bucket.",
     )
 
+    parser.add_argument(
+        "--start-date",
+        type=str,
+        required=True,
+        help="Start time for the output data. Format: YYYY-MM-DD",
+    )
+
+    parser.add_argument(
+        "--end-date",
+        type=str,
+        required=False,
+        help="End time for the output data. If not provided, start_time will be used "
+        "for end_time. Format: YYYY-MM-DD",
+    )
+    # TODO: Will need to add some way of including pointing numbers
+
+    parser.add_argument(
+        "--version",
+        type=str,
+        required=True,
+        help="Version of the data",
+    )
     parser.add_argument(
         "--dependency",
         type=str,
         required=True,
         help=dependency_help,
     )
-    parser.add_argument("--data-dir", type=str, required=False, help=data_dir_help)
+
+    parser.add_argument(
+        "--use-remote",
+        action="store_true",
+        required=False,
+        help="Upload completed output files to IMAP API.",
+    )
     args = parser.parse_args()
 
     return args
@@ -154,7 +148,7 @@ def _validate_args(args):
     args : argparse.Namespace
         An object containing the parsed arguments and their values
     """
-    if args.instrument not in imap_processing.INSTRUMENTS:
+    if args.instrument not in imap_data_access.VALID_INSTRUMENTS:
         raise ValueError(
             f"{args.instrument} is not in the supported instrument list: "
             f"{imap_processing.INSTRUMENTS}"
@@ -165,12 +159,6 @@ def _validate_args(args):
             " instrument, valid levels are: "
             f"{imap_processing.PROCESSING_LEVELS[args.instrument]}"
         )
-    if args.data_dir:
-        data_path = Path(args.data_dir)
-        if not data_path.exists():
-            raise ValueError(f"Data directory {args.data_dir} does not exist")
-        # Set the data directory to the user-supplied value
-        imap_data_access.config["DATA_DIR"] = data_path
 
 
 class ProcessInstrument(ABC):
@@ -187,11 +175,28 @@ class ProcessInstrument(ABC):
         format: "[{'instrument': 'swe', 'level': 'l0', 'version': 'v00-01'}]"
     """
 
-    def __init__(self, level: str, file_path: str, dependency_str: str) -> None:
+    def __init__(
+        self,
+        level: str,
+        dependency_str: str,
+        start_date: str,
+        end_date: str,
+        version: str,
+        use_remote: bool,
+    ) -> None:
         self.level = level
-        self.file_path = file_path
+
         # Convert string into a dictionary
         self.dependencies = loads(dependency_str.replace("'", '"'))
+
+        self.start_date = start_date
+        self.end_date = end_date
+        if not end_date:
+            self.end_date = start_date
+            print(f"Setting end time to start time: {start_date}")
+
+        self.version = version
+        self.use_remote = use_remote
 
     def download_dependencies(self):
         """Download the dependencies for the instrument.
@@ -203,19 +208,28 @@ class ProcessInstrument(ABC):
         """
         file_list = []
         for dep in self.dependencies:
-            val = imap_data_access.query(
-                instrument=dep["instrument"],
-                data_level=dep["level"],
-                version=dep["version"],
-            )
+            try:
+                # TODO: Validate dep dict
+                # TODO: determine what dependency information is optional
+                val = imap_data_access.query(
+                    instrument=dep["instrument"],
+                    data_level=dep["level"],
+                    version=dep["version"],
+                    start_date=dep["start_date"],
+                    end_date=dep["end_date"],
+                    descriptor=dep["descriptor"],
+                )
+            except HTTPError as e:
+                raise ValueError(f"Unable to download files from {dep}") from e
+
             if not val:
                 raise FileNotFoundError(
                     f"File not found for required dependency "
-                    f"{dep} while attempting to create file "
-                    f"{self.file_path}. This should never occur"
-                    f"in real processing."
+                    f"{dep} while attempting to create file."
+                    f"This should never occur "
+                    f"in normal processing."
                 )
-            # TODO: needs time range
+
             file_list.append(imap_data_access.download(val[0]["file_path"]))
         return file_list
 
@@ -288,9 +302,17 @@ class Mag(ProcessInstrument):
                     f"Unexpected dependencies found for MAG L1A:"
                     f"{file_paths}. Expected only one dependency."
                 )
-            mag_l1a(file_paths[0], self.file_path)
+            filename = imap_data_access.ScienceFilePath.generate_from_inputs(
+                "mag", "l1a", "raw", self.start_date, self.end_date, "v01-01"
+            )
+            mag_l1a(file_paths[0], filename.construct_path())
+            print(f"Generated file: {filename.construct_path()}")
 
-        imap_data_access.upload(self.file_path)
+            if self.use_remote:
+                print(f"Uploading file from: {filename.construct_path()}")
+                # TODO: figure out data_dir, because now this fails.
+                #  Should switch to using IMAP_DATA_DIR env var.
+                imap_data_access.upload(filename.construct_path())
 
 
 class Swapi(ProcessInstrument):
@@ -310,52 +332,52 @@ class Swe(ProcessInstrument):
         """Perform SWE specific processing."""
         # self.file_path example:
         # imap/swe/l1a/2023/09/imap_swe_l1a_sci_20230927_20230927_v01-00.cdf
+        dependencies = self.download_dependencies()
         print(f"Processing SWE {self.level}")
+
+        # TODO: currently assumes just the first path returned is the one to use
+
         if self.level == "l1a":
-            # TODO: change this after we have updated dependency or download
-            # path from Batch Job command.
-            # create download path
-            download_path = self.file_path.replace("l1a", "l0").replace("cdf", "pkts")
-            print(f"download_path: {download_path}")
-            output_path = imap_data_access.download(download_path)
-            processed_data = swe_l1a(output_path)
+            processed_data = swe_l1a(Path(dependencies[0]))
             for data in processed_data:
                 # write data to cdf
+                file = imap_data_access.ScienceFilePath.generate_from_inputs(
+                    "swe",
+                    "l1a",
+                    data["descriptor"],
+                    self.start_date,
+                    self.end_date,
+                    self.version,
+                )
+
                 cdf_file_path = write_cdf(
-                    data=data["data"], descriptor=data["descriptor"]
+                    data=data["data"], filepath=file.construct_path()
                 )
-                # TODO: undo this after demo. overwrite version with
-                # what was passed.
-                current_version = str(cdf_file_path).split(".")[0][-6:]
-                new_version = str(output_path).split(".")[0][-6:]
-                cdf_file_path = Path.replace(
-                    cdf_file_path,
-                    str(cdf_file_path).replace(current_version, new_version),
-                )
-                imap_data_access.upload(cdf_file_path)
-                print(f"uploaded {cdf_file_path}")
+
+                print(f"processed file path: {cdf_file_path}")
+
+                if self.use_remote:
+                    imap_data_access.upload(cdf_file_path)
+                    print(f"Uploading file: {cdf_file_path}")
+
         elif self.level == "l1b":
-            # create download path
-            download_path = self.file_path.replace("l1b", "l1a")
-            print(f"download_path: {download_path}")
-            output_path = imap_data_access.download(download_path)
-            print(f"downloaded folder {output_path}")
             # read CDF file
-            l1a_dataset = cdf_to_xarray(output_path)
+            l1a_dataset = cdf_to_xarray(dependencies[0])
             processed_data = swe_l1b(l1a_dataset)
-            descriptor = os.path.basename(self.file_path).split("_")[3]
-            processed_file_path = write_cdf(data=processed_data, descriptor=descriptor)
-            print(f"processed file path: {processed_file_path}")
-            # TODO: undo this after demo. overwrite version with
-            # what was passed.
-            current_version = str(processed_file_path).split(".")[0][-6:]
-            new_version = str(output_path).split(".")[0][-6:]
-            processed_file_path = Path.replace(
-                processed_file_path,
-                str(processed_file_path).replace(current_version, new_version),
+            # TODO: Update this descriptor
+            descriptor = "test"
+            file = imap_data_access.ScienceFilePath.generate_from_inputs(
+                "swe", "l1b", descriptor, self.start_date, self.end_date, self.version
             )
-            imap_data_access.upload(processed_file_path)
-            print(f"finished uploading - {processed_file_path}")
+
+            cdf_file_path = write_cdf(
+                data=processed_data, filepath=file.construct_path()
+            )
+            print(f"processed file path: {cdf_file_path}")
+            if self.use_remote:
+                imap_data_access.upload(cdf_file_path)
+                print(f"Uploading file: {cdf_file_path}")
+
         else:
             print("No code to process this level")
 
@@ -380,7 +402,14 @@ def main():
 
     _validate_args(args)
     cls = getattr(sys.modules[__name__], args.instrument.capitalize())
-    instrument = cls(args.level, args.file_path, args.dependency)
+    instrument = cls(
+        args.level,
+        args.dependency,
+        args.start_date,
+        args.end_date,
+        args.version,
+        args.use_remote,
+    )
     instrument.process()
 
 
