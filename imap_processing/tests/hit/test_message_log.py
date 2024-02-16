315b49edff9ef7be83b65bb6de1d2348a4ab5115
Sean Hoyt <sean.hoyt@lasp.colorado.edu>
2024-02-16 14:09:28 -0700

    added initial message log data class

--- imap_processing/tests/hit/test_message_log.py
+++ imap_processing/tests/hit/test_message_log.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 from imap_processing import decom, imap_module_directory
 
 
@@ -10,9 +8,9 @@ def test_message_log():
     )
     xtce_file = imap_module_directory / "hit/packet_definitions/P_HIT_MSGLOG.xml"
 
-    #validation_data = pd.read_csv(validation_file)
+    # validation_data = pd.read_csv(validation_file)
     packets = decom.decom_packets(test_file.resolve(), xtce_file.resolve())
 
     for pkt_idx, packet in enumerate(packets):
-        #print(packet)
+        # print(packet)
         print(packet.data["TEXT"].raw_value)
