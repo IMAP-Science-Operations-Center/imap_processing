d067d9dda69937050480aee14647ab07d9c10d28
Sean Hoyt <sean.hoyt@lasp.colorado.edu>
2024-02-16 11:17:10 -0700

    initial dataclass

---
+++ imap_processing/hit/l0/data_classes/message_log.py
@@ -0,0 +1,40 @@
+"""L1A HIT Message Log data class."""
+from dataclasses import dataclass
+
+import numpy as np
+
+from imap_processing.ccsds.ccsds_data import CcsdsData
+from imap_processing.hit.l0.utils.hit_base import HITBase
+
+
+@dataclass
+class MessageLog(HITBase):
+    """L1A HIT Message Log data.
+
+    The HIT Message Log data class handles the decommutation
+    and parsing of L0 to L1A data.
+
+    Attributes
+    ----------
+    SHCOARSE : int
+        Spacecraft time.
+    TEXT: str
+        Message log text in ASCII with newline characters (0x10) separating
+        lines. Last byte is always NULL (0), and packet is padded with zeros if
+        no more messages.
+
+    Methods
+    -------
+    __init__(packet, software_vesion, packet_file_name):
+        Uses the CCSDS packet, version of the software, and
+        the name of the packet file to parse and store information about
+        the Houskeeping packet data.
+    """
+
+    SHCOARSE: int
+    TEXT: str
+
+    def __init__(self, packet, software_version: str, packet_file_name: str):
+        """Intialization method for Housekeeping Data class."""
+        super().__init__(software_version, packet_file_name, CcsdsData(packet.header))
+        self.parse_data(packet)
