8477dfe2ed60d2e3d41063b1dd3aadeb6ced748d
Sean Hoyt <sean.hoyt@lasp.colorado.edu>
2024-02-19 15:30:38 -0700

    added message log tests

--- imap_processing/tests/hit/test_message_log.py
+++ imap_processing/tests/hit/test_message_log.py
@@ -1,5 +1,7 @@
 from imap_processing import decom, imap_module_directory
-
+from imap_processing.hit.l0.data_classes.message_log import MessageLog
+import pandas as pd
+import bitstring
 
 def test_message_log():
     test_file = imap_module_directory / "tests/hit/test_data/msglog_sample.ccsds"
@@ -8,9 +10,10 @@ def test_message_log():
     )
     xtce_file = imap_module_directory / "hit/packet_definitions/P_HIT_MSGLOG.xml"
 
-    # validation_data = pd.read_csv(validation_file)
+    validation_data = pd.read_csv(validation_file)
     packets = decom.decom_packets(test_file.resolve(), xtce_file.resolve())
-
+    failure_count = 0
     for pkt_idx, packet in enumerate(packets):
-        # print(packet)
-        print(packet.data["TEXT"].raw_value)
+        msg_log = MessageLog(packet, "0.0", "mslog_sample.ccsds")
+        assert msg_log.TEXT == bitstring.Bits(hex=validation_data["TEXT"][pkt_idx]).bin
+        
