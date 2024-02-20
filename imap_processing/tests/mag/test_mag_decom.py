8d4e8bbb810977c3178e020925f10b5655d225bc
Maxine Hartnett <maxine.hartnett@lasp.colorado.edu>
2024-02-20 10:40:49 -0700

    Fixing MAG L0 tests

--- imap_processing/tests/mag/test_mag_decom.py
+++ imap_processing/tests/mag/test_mag_decom.py
@@ -1,72 +1,38 @@
-import dataclasses
 from pathlib import Path
+
+import pandas as pd
 
 from imap_processing.mag.l0.decom_mag import decom_packets
 
 
-def test_burst_decom():
-    current_directory = Path(__file__).parent
-    burst_test_file = current_directory / "mag_single_burst.pkts"
-    l0 = decom_packets(str(burst_test_file))[0]
-
-    assert l0.ccsds_header.PKT_APID == 1068
-    assert l0.ccsds_header.SRC_SEQ_CTR == 1
-    assert l0.ccsds_header.PKT_LEN == 1821
-
-    expected = dataclasses.replace(
-        l0,
-        SHCOARSE=435954632,
-        PUS_STYPE=21,
-        PUS_SSUBTYPE=3,
-        COMPRESSION=0,
-        MAGO_ACT=1,
-        MAGI_ACT=1,
-        PRI_SENS=0,
-        PRI_VECSEC=6,
-        SEC_VECSEC=3,
-        PRI_COARSETM=435954625,
-        PRI_FNTM=64610,
-        SEC_COARSETM=435954625,
-        SEC_FNTM=64607,
-    )
-    assert l0 == expected
-
-
-def test_norm_decom():
-    current_directory = Path(__file__).parent
-    burst_test_file = current_directory / "mag_single_norm.pkts"
-    l0 = decom_packets(str(burst_test_file))[0]
-
-    assert l0.ccsds_header.PKT_APID == 1052
-    assert l0.ccsds_header.SRC_SEQ_CTR == 16
-    assert l0.ccsds_header.PKT_LEN == 221
-
-    expected = dataclasses.replace(
-        l0,
-        SHCOARSE=435954831,
-        PUS_STYPE=21,
-        PUS_SSUBTYPE=7,
-        COMPRESSION=0,
-        MAGO_ACT=1,
-        MAGI_ACT=1,
-        PRI_SENS=0,
-        PRI_VECSEC=1,
-        SEC_VECSEC=1,
-        PRI_COARSETM=435954821,
-        PRI_FNTM=64581,
-        SEC_COARSETM=435954821,
-        SEC_FNTM=64582,
-    )
-    assert l0 == expected
-
-
 def test_mag_decom():
-    """Testing multiple packets"""
     current_directory = Path(__file__).parent
-    burst_test_file = current_directory / "mag_multiple_packets.pkts"
+    # burst_test_file = current_directory / "science_data_test_REAL.pkts"
+    burst_test_file = current_directory / "mag_l0_test_data.pkts"
     l0 = decom_packets(str(burst_test_file))
 
-    assert len(l0) == 125
-    for val in l0:
-        assert val.ccsds_header.PKT_APID == 1052
-        assert len(val.VECTORS) > 0
+    expected_output = pd.read_csv("mag_l0_test_output.csv")
+
+    index = 0
+    for test in l0:
+        assert test.ccsds_header.PKT_APID == expected_output["PHAPID"][index]
+        assert test.ccsds_header.SRC_SEQ_CTR == expected_output["PHSEQCNT"][index]
+        assert test.ccsds_header.PKT_LEN == expected_output["PHDLEN"][index]
+
+        assert test.SHCOARSE == expected_output["SHCOARSE"][index]
+        assert test.PUS_STYPE == expected_output["PUS_STYPE"][index]
+        assert test.PUS_SSUBTYPE == expected_output["PUS_SSUBTYPE"][index]
+        assert test.COMPRESSION == expected_output["COMPRESSION"][index]
+        assert test.MAGO_ACT == expected_output["FOB_ACT"][index]
+        assert test.MAGI_ACT == expected_output["FIB_ACT"][index]
+        assert test.PRI_SENS == expected_output["PRI_SENS"][index]
+        assert test.PRI_VECSEC == expected_output["PRI_VECSEC"][index]
+        assert test.SEC_VECSEC == expected_output["SEC_VECSEC"][index]
+        assert test.PRI_COARSETM == expected_output["PRI_COARSETM"][index]
+        assert test.PRI_FNTM == expected_output["PRI_FNTM"][index]
+        assert test.SEC_COARSETM == expected_output["SEC_COARSETM"][index]
+        assert test.SEC_FNTM == expected_output["SEC_FNTM"][index]
+
+        index += 1
+
+    assert len(l0) == len(expected_output.index)
