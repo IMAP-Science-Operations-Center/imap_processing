d5c03e02b294782c376a245a3de0ddec1afd4804
Tenzin Choedon <tenzin.choedon@lasp.colorado.edu>
2024-02-22 14:54:14 -0700

    added rest of test for SWAPI decom

--- imap_processing/tests/swapi/test_swapi_decom.py
+++ imap_processing/tests/swapi/test_swapi_decom.py
@@ -53,32 +53,32 @@ def test_swapi_sci_data(decom_test_data):
     first_data = sci_packets[0]
     validation_data = raw_validation_data.loc[first_data.data["SHCOARSE"].raw_value]
 
-    # compare raw values of science data
+    # compare raw values of validation data
     for key, value in first_data.data.items():
-        # check if the data is the same, for SHCOARSE we need the name of the column.
-        # This is done because pandas removed it from the main columns to make it the
-        # index.
+        # check if the data is the same
         if key == "PLAN_ID_SCIENCE":
             # We had to work around this because HK and SCI packet uses
             # PLAN_ID but they uses different length of bits.
             assert value.raw_value == validation_data["PLAN_ID"]
         elif key == "SPARE_2_SCIENCE":
-            # Same for this SPARE_2 as PLAN_ID
+            # Same for this SPARE_2 as above case
             assert value.raw_value == validation_data["SPARE_2"]
         elif key == "MODE":
-            # Because validation data uses ENUM instead of raw value
+            # Because validation data uses derived value instead of raw value
             assert value.derived_value == validation_data[key]
         elif "RNG" in key:
-            # Can't compare because validation data has "RAW" as value
-            continue
+            assert value.derived_value == validation_data[key]
         else:
+            # for SHCOARSE we need the name of the column.
+            # This is done because pandas removed it from the
+            # main columns to make it the index.
             assert value.raw_value == (
                 validation_data[key] if key != "SHCOARSE" else validation_data.name
             )
 
 
 def test_swapi_hk_data(decom_test_data):
-    """This test and validate raw data of SWAPI raw science data."""
+    """This test and validate raw data of SWAPI raw housekeeping data."""
     # read validation data
     test_data_path = imap_module_directory / "tests/swapi/l0_validation_data"
     raw_validation_data = pd.read_csv(
@@ -91,11 +91,8 @@ def test_swapi_hk_data(decom_test_data):
     first_data = hk_packets[0]
     validation_data = raw_validation_data.loc[first_data.data["SHCOARSE"].raw_value]
 
-    # compare raw values of science data
+    # compare raw values of validation data
     for key, value in first_data.data.items():
-        # check if the data is the same, for SHCOARSE we need the name of the column.
-        # This is done because pandas removed it from the main columns to make it the
-        # index.
         if key == "PLAN_ID_HK":
             # We had to work around this because HK and SCI packet uses
             # PLAN_ID but they uses different length of bits.
@@ -103,10 +100,40 @@ def test_swapi_hk_data(decom_test_data):
         elif key == "SPARE_2_HK":
             # Same for this SPARE_2 as PLAN_ID
             assert value.raw_value == validation_data["SPARE_2"]
-        elif "RNG" in key or key == "SHCOARSE":
-            # Can't compare because validation data has "RAW" as value
+        elif key == "SHCOARSE":
+            # for SHCOARSE we need the name of the column.
+            # This is done because pandas removed it from the main columns
+            # to make it the index.
+            assert value.raw_value == validation_data.name
+        elif key == "N5_V":
+            # TODO: remove this elif after getting good validation data
+            # Validation data has wrong value for N5_V
             continue
         else:
-            assert value.raw_value == (
-                validation_data[key] if key != "SHCOARSE" else validation_data.name
-            )
+            assert value.raw_value == validation_data[key]
+
+
+def test_swapi_aut_data(decom_test_data):
+    """This test and validate raw data of SWAPI raw autonomy data."""
+    # read validation data
+    test_data_path = imap_module_directory / "tests/swapi/l0_validation_data"
+    raw_validation_data = pd.read_csv(
+        test_data_path / "idle_export_raw.SWP_AUT_20231012_125245.csv",
+        index_col="SHCOARSE",
+    )
+
+    grouped_data = group_by_apid(decom_test_data)
+    aut_packets = grouped_data[SWAPIAPID.SWP_AUT]
+    first_data = aut_packets[0]
+    validation_data = raw_validation_data.loc[first_data.data["SHCOARSE"].raw_value]
+
+    # compare raw values of science data
+    for key, value in first_data.data.items():
+        if key == "SHCOARSE":
+            assert value.raw_value == validation_data.name
+        elif key == "SPARE_1_AUT":
+            # We had to work around this because HK and SCI packet uses
+            # SPARE_1 but they uses different length of bits.
+            assert value.raw_value == validation_data["SPARE_1"]
+        else:
+            assert value.raw_value == validation_data[key]
