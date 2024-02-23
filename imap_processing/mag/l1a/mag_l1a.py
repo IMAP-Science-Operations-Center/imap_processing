1cf4ce96ff56c1f1c39c60281fddabd717c797c4
Maxine Hartnett <maxine.hartnett@lasp.colorado.edu>
2024-02-23 09:51:19 -0700

    minor updates

--- imap_processing/mag/l1a/mag_l1a.py
+++ imap_processing/mag/l1a/mag_l1a.py
@@ -8,25 +8,27 @@ from imap_processing.mag.l0 import decom_mag
 __logger__ = logging.getLogger(__name__)
 
 
-def mag_l1a(packet_filepath, cdf_filepath_norm, cdf_filepath_burst):
+def mag_l1a(packet_filepath, output_filepath_norm, ouptput_filepath_burst):
     """
     Process MAG L0 data into L1A CDF files at cdf_filepath.
 
     Parameters
     ----------
-    packet_filepath:
+    packet_filepath :
         Packet files for processing
-    cdf_filepath:
-        Directory for output
+    output_filepath_norm :
+        Full directory and filename for raw-norm CDF file
+    ouptput_filepath_burst :
+        Full directory and filename for raw-burst CDF file
     """
     mag_l0 = decom_mag.decom_packets(packet_filepath)
 
     mag_norm, mag_burst = decom_mag.export_to_xarray(mag_l0)
 
     if mag_norm is not None:
-        write_cdf(mag_norm, Path(cdf_filepath_norm))
-        logging.info(f"Created CDF file at {cdf_filepath_norm}")
+        write_cdf(mag_norm, Path(output_filepath_norm))
+        logging.info(f"Created CDF file at {output_filepath_norm}")
 
     if mag_burst is not None:
-        write_cdf(mag_burst, Path(cdf_filepath_burst))
-        logging.info(f"Created CDF file at {cdf_filepath_norm}")
+        write_cdf(mag_burst, Path(ouptput_filepath_burst))
+        logging.info(f"Created CDF file at {output_filepath_norm}")
