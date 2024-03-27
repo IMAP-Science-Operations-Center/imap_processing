b4a3274586717db4a9c9fde1d1cdcfd23e9aed3f
Tenzin Choedon <tenzin.choedon@lasp.colorado.edu>
2024-03-27 14:42:02 -0600

    better names in tests

--- imap_processing/tests/hi/test_l1a_sci_de.py
+++ imap_processing/tests/hi/test_l1a_sci_de.py
@@ -34,19 +34,19 @@ def test_create_dataset():
         "de_tag": 3,
     }
     # Test for good data
-    de_data_list = [metaevent, directevent_1, directevent_2, directevent_3]
-    packet_time = [123] * len(de_data_list)
-    dataset = create_dataset(de_data_list, packet_time)
+    list_with_meta_first = [metaevent, directevent_1, directevent_2, directevent_3]
+    packet_time = [123] * len(list_with_meta_first)
+    dataset = create_dataset(list_with_meta_first, packet_time)
     assert dataset["epoch"].shape == (3,)
 
     # Test for missing metaevent
-    de_data_list = [directevent_1, directevent_2, directevent_3]
-    packet_time = [123] * len(de_data_list)
-    dataset = create_dataset(de_data_list, packet_time)
+    list_with_no_meta = [directevent_1, directevent_2, directevent_3]
+    packet_time = [123] * len(list_with_no_meta)
+    dataset = create_dataset(list_with_no_meta, packet_time)
     assert dataset is None
 
     # test for first metaevent missing in the list
-    de_data_list = [
+    list_with_meta_middle = [
         directevent_1,
         directevent_2,
         metaevent,
@@ -54,7 +54,19 @@ def test_create_dataset():
         directevent_2,
         directevent_3,
     ]
-    packet_time = [123] * len(de_data_list)
-    dataset = create_dataset(de_data_list, packet_time)
+    packet_time = [123] * len(list_with_meta_middle)
+    dataset = create_dataset(list_with_meta_middle, packet_time)
     assert dataset["epoch"].shape == (3,)
     np.testing.assert_array_equal(dataset["trigger_id"].data, [1, 2, 3])
+
+    list_with_meta_last = [
+        directevent_1,
+        directevent_2,
+        directevent_3,
+        directevent_1,
+        directevent_2,
+        metaevent,
+    ]
+    packet_time = [123] * len(list_with_meta_last)
+    dataset = create_dataset(list_with_meta_last, packet_time)
+    assert dataset["epoch"].shape == (0,)
