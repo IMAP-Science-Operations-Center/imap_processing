a37fe593d5329019d57f5613e72e93113b3d6a36
Maxine Hartnett <117409426+maxinelasp@users.noreply.github.com>
2024-03-27 13:59:13 -0600

    Updating mag CDF generation to match new filenames (#370)
    * Updating mag CDF generation to match new filenames
    
    * PR updates
    
    * Updating test to remove path

--- imap_processing/mag/l1a/mag_l1a.py
+++ imap_processing/mag/l1a/mag_l1a.py
@@ -1,6 +1,5 @@
 """Methods for decomming packets, processing to level 1A, and writing CDFs for MAG."""
 import logging
-from pathlib import Path
 
 from imap_processing.cdf.utils import write_cdf
 from imap_processing.mag.l0 import decom_mag
@@ -8,7 +7,7 @@ from imap_processing.mag.l0 import decom_mag
 logger = logging.getLogger(__name__)
 
 
-def mag_l1a(packet_filepath, output_filepath_norm, ouptput_filepath_burst):
+def mag_l1a(packet_filepath):
     """
     Process MAG L0 data into L1A CDF files at cdf_filepath.
 
@@ -16,19 +15,15 @@ def mag_l1a(packet_filepath, output_filepath_norm, ouptput_filepath_burst):
     ----------
     packet_filepath :
         Packet files for processing
-    output_filepath_norm :
-        Full directory and filename for raw-norm CDF file
-    ouptput_filepath_burst :
-        Full directory and filename for raw-burst CDF file
     """
     mag_l0 = decom_mag.decom_packets(packet_filepath)
 
     mag_norm, mag_burst = decom_mag.export_to_xarray(mag_l0)
 
     if mag_norm is not None:
-        write_cdf(mag_norm, Path(output_filepath_norm))
-        logger.info(f"Created CDF file at {output_filepath_norm}")
+        file = write_cdf(mag_norm)
+        logger.info(f"Created CDF file at {file}")
 
     if mag_burst is not None:
-        write_cdf(mag_burst, Path(ouptput_filepath_burst))
-        logger.info(f"Created CDF file at {output_filepath_norm}")
+        file = write_cdf(mag_burst)
+        logger.info(f"Created CDF file at {file}")
