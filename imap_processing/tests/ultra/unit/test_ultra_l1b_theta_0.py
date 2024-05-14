fac6c72475e229f31d62d9d2c9cdb8b7213b12e7
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2024-05-13 19:16:33 -0600

    ssd addition

--- imap_processing/tests/ultra/unit/test_ultra_l1b_theta_0.py
+++ imap_processing/tests/ultra/unit/test_ultra_l1b_theta_0.py
@@ -17,7 +17,9 @@ from imap_processing.ultra.l1b.ultra_l1b import (
     get_front_y_position,
     get_particle_velocity,
     get_path_length,
+    get_ssd_positions,
 )
+
 
 @pytest.fixture()
 def decom_ultra_events(ccsds_path_theta_0, xtce_path):
@@ -46,13 +48,13 @@ def indices_start_type_1_or_2(decom_ultra_events, decom_ultra_aux):
 
     # Create the dataset
     events_dataset = create_dataset(
-        {ULTRA_EVENTS.apid[0]: decom_ultra_events,
-         ULTRA_AUX.apid[0]: decom_ultra_aux}
+        {ULTRA_EVENTS.apid[0]: decom_ultra_events, ULTRA_AUX.apid[0]: decom_ultra_aux}
     )
 
     # Remove start_type with fill values
-    events_dataset = events_dataset.where(events_dataset["START_TYPE"] !=
-                                          GlobalConstants.INT_FILLVAL, drop=True)
+    events_dataset = events_dataset.where(
+        events_dataset["START_TYPE"] != GlobalConstants.INT_FILLVAL, drop=True
+    )
 
     # Check top and bottom
     index_1 = np.where(events_dataset["START_TYPE"] == 1)[0]
@@ -70,12 +72,12 @@ def indices_stop_type_1_or_2(decom_ultra_events, decom_ultra_aux):
 
     # Create the dataset
     events_dataset = create_dataset(
-        {ULTRA_EVENTS.apid[0]: decom_ultra_events,
-         ULTRA_AUX.apid[0]: decom_ultra_aux}
+        {ULTRA_EVENTS.apid[0]: decom_ultra_events, ULTRA_AUX.apid[0]: decom_ultra_aux}
     )
     # Remove start_type with fill values
-    events_dataset = events_dataset.where(events_dataset["START_TYPE"] !=
-                                          GlobalConstants.INT_FILLVAL, drop=True)
+    events_dataset = events_dataset.where(
+        events_dataset["START_TYPE"] != GlobalConstants.INT_FILLVAL, drop=True
+    )
 
     # Check top and bottom
     index_1 = np.where(events_dataset["STOP_TYPE"] == 1)[0]
@@ -84,11 +86,32 @@ def indices_stop_type_1_or_2(decom_ultra_events, decom_ultra_aux):
     return index_1, index_2, events_dataset
 
 
+@pytest.fixture()
+def indices_stop_type_8_to_15(decom_ultra_events, decom_ultra_aux):
+    """
+    A pytest fixture to extract indices from events_dataset where STOP_TYPE is 1 or 2
+    and COUNT is not 0. Assumes the dataset is structured with an 'epoch' dimension.
+    """
+
+    # Create the dataset
+    events_dataset = create_dataset(
+        {ULTRA_EVENTS.apid[0]: decom_ultra_events, ULTRA_AUX.apid[0]: decom_ultra_aux}
+    )
+    # Remove start_type with fill values
+    events_dataset = events_dataset.where(
+        events_dataset["START_TYPE"] != GlobalConstants.INT_FILLVAL, drop=True
+    )
+
+    # Check top and bottom
+    index = np.where(events_dataset["STOP_TYPE"] >= 8)[0]
+
+    return index, events_dataset
+
+
 def test_xf(
     indices_start_type_1_or_2,
     events_fsw_comparison_theta_0,
 ):
-
     indices_1, indices_2, events_dataset = indices_start_type_1_or_2
 
     df = pd.read_csv(events_fsw_comparison_theta_0)
@@ -96,22 +119,25 @@ def test_xf(
     selected_rows_1 = df_filt.iloc[indices_1]
     selected_rows_2 = df_filt.iloc[indices_2]
 
-    xf_1 = get_front_x_position(events_dataset["START_TYPE"].data[indices_1],
-                              events_dataset["START_POS_TDC"].data[indices_1])
-    xf_2 = get_front_x_position(events_dataset["START_TYPE"].data[indices_2],
-                              events_dataset["START_POS_TDC"].data[indices_2])
+    xf_1 = get_front_x_position(
+        events_dataset["START_TYPE"].data[indices_1],
+        events_dataset["START_POS_TDC"].data[indices_1],
+    )
+    xf_2 = get_front_x_position(
+        events_dataset["START_TYPE"].data[indices_2],
+        events_dataset["START_POS_TDC"].data[indices_2],
+    )
 
     # The value 180 was added to xf_1 since that is the offset from the FSW xft_off
-    assert np.allclose(xf_1+180, selected_rows_1.Xf.values.astype('float'), rtol=1e-3)
+    assert np.allclose(xf_1 + 180, selected_rows_1.Xf.values.astype("float"), rtol=1e-3)
     # The value 25 was subtracted from xf_2 since that is the offset from the FSW xft_off
-    assert np.allclose(xf_2-25, selected_rows_2.Xf.values.astype('float'), rtol=1e-3)
+    assert np.allclose(xf_2 - 25, selected_rows_2.Xf.values.astype("float"), rtol=1e-3)
 
 
 @pytest.fixture()
-def tof(indices_stop_type_1_or_2,
-        events_fsw_comparison_theta_0):
+def tof(indices_stop_type_1_or_2, events_fsw_comparison_theta_0):
     indices_1, indices_2, events_dataset = indices_stop_type_1_or_2
-    indices = np.concatenate((indices_1,indices_2))
+    indices = np.concatenate((indices_1, indices_2))
     indices.sort()
 
     df = pd.read_csv(events_fsw_comparison_theta_0)
@@ -119,9 +145,7 @@ def tof(indices_stop_type_1_or_2,
     selected_rows_1 = df_filt.iloc[indices]
 
     tof, t2, xb, yb = get_back_positions(
-        indices,
-        events_dataset,
-        selected_rows_1.Xf.values.astype('float')
+        indices, events_dataset, selected_rows_1.Xf.values.astype("float")
     )
     return tof, t2, xb, yb
 
@@ -133,42 +157,70 @@ def test_xb_yb(
 ):
     _, _, xb, yb = tof
     indices_1, indices_2, events_dataset = indices_stop_type_1_or_2
-    indices = np.concatenate((indices_1,indices_2))
+    indices = np.concatenate((indices_1, indices_2))
 
     df = pd.read_csv(events_fsw_comparison_theta_0)
     df_filt = df[df["StartType"] != -1]
     selected_rows_1 = df_filt.iloc[indices]
 
-    np.testing.assert_array_equal(xb[indices],
-                                  selected_rows_1["Xb"].astype('float'))
-    np.testing.assert_array_equal(yb[indices],
-                                  selected_rows_1["Yb"].astype('float'))
+    np.testing.assert_array_equal(xb[indices], selected_rows_1["Xb"].astype("float"))
+    np.testing.assert_array_equal(yb[indices], selected_rows_1["Yb"].astype("float"))
+
+
+@pytest.fixture()
+def tof_ssd(indices_stop_type_8_to_15, events_fsw_comparison_theta_0):
+    indices, events_dataset = indices_stop_type_8_to_15
+
+    df = pd.read_csv(events_fsw_comparison_theta_0)
+    df_filt = df[df["StartType"] != -1]
+    selected_rows_1 = df_filt.iloc[indices]
+
+    tof, xb, yb = get_ssd_positions(
+        indices, events_dataset, selected_rows_1.Xf.values.astype("float")
+    )
+    return xb, yb, tof
+
+
+def test_xb_yb_ssd(
+    indices_stop_type_8_to_15,
+    tof_ssd,
+    events_fsw_comparison_theta_0,
+):
+    xb, yb, tof = tof_ssd
+    indices_1, indices_2, events_dataset = indices_stop_type_1_or_2
+    indices = np.concatenate((indices_1, indices_2))
+
+    df = pd.read_csv(events_fsw_comparison_theta_0)
+    df_filt = df[df["StartType"] != -1]
+    selected_rows_1 = df_filt.iloc[indices]
+
+    np.testing.assert_array_equal(xb[indices], selected_rows_1["Xb"].astype("float"))
+    np.testing.assert_array_equal(yb[indices], selected_rows_1["Yb"].astype("float"))
 
 
 def test_yf(
     indices_start_type_1_or_2,
     indices_stop_type_1_or_2,
     events_fsw_comparison_theta_0,
-    tof
+    tof,
 ):
-    _, _, events_dataset = indices_start_type_1_or_2
+    index_1, index_2, events_dataset = indices_start_type_1_or_2
 
     df = pd.read_csv(events_fsw_comparison_theta_0)
     df_filt = df[df["StartType"] != -1]
 
-    d, yf = get_front_y_position(events_dataset,
-                                 df_filt.Yb.values.astype('float'))
+    d, yf = get_front_y_position(events_dataset, df_filt.Yb.values.astype("float"))
 
-    assert yf == pytest.approx(df_filt["Yf"].astype('float'), 1e-3)
+    assert yf == pytest.approx(df_filt["Yf"].astype("float"), 1e-3)
 
-    xf_test = df_filt["Xf"].astype('float').values
-    yf_test = df_filt["Yf"].astype('float').values
+    xf_test = df_filt["Xf"].astype("float").values
+    yf_test = df_filt["Yf"].astype("float").values
 
-    xb_test = df_filt["Xb"].astype('float').values
-    yb_test = df_filt["Yb"].astype('float').values
+    xb_test = df_filt["Xb"].astype("float").values
+    yb_test = df_filt["Yb"].astype("float").values
 
     r = get_path_length((xf_test, yf_test), (xb_test, yb_test), d)
-    assert r == pytest.approx(df_filt["r"].astype('float'), rel=1e-3)
+    assert r == pytest.approx(df_filt["r"].astype("float"), rel=1e-3)
 
     # TODO: test get_energy_pulse_height
     # pulse_height = events_dataset["ENERGY_PH"].data[index]
@@ -177,49 +229,30 @@ def test_yf(
     # TODO: needs lookup table to test bin
     tof, t2, xb, yb = tof
     indices_1, indices_2, events_dataset = indices_stop_type_1_or_2
-    indices = np.concatenate((indices_1,indices_2))
+    indices = np.concatenate((indices_1, indices_2))
     indices.sort()
 
-    energy = df_filt["Xf"].iloc[indices].astype('float')
-    r = df_filt["Xf"].iloc[indices].astype('float')
+    energy = df_filt["Xf"].iloc[indices].astype("float")
+    r = df_filt["r"].iloc[indices].astype("float")
 
-    ctof, bin = determine_species_pulse_height(energy, tof[indices], r)
-    assert ctof * 100 == pytest.approx(df_filt["cTOF"].iloc[indices].astype('float'), rel=1e-3)
-    #
-    # energy = float(df["Xf"].iloc[index])
-    #
-    # # TODO: needs lookup table to test bin
-    # ctof, bin = determine_species_pulse_height(energy, tof, r)
-    # assert ctof * 100 == pytest.approx(float(df["cTOF"].iloc[index]), rel=1e-3)
+    ctof, bin = determine_species_pulse_height(energy, tof[indices] * 100, r)
+    assert ctof.values == pytest.approx(
+        df_filt["cTOF"].iloc[indices].astype("float").values, rel=1e-3
+    )
 
+    vhat_x, vhat_y, vhat_z = get_particle_velocity(
+        (xf_test[indices], yf_test[indices]),
+        (xb_test[indices], yb_test[indices]),
+        d[indices],
+        tof[indices],
+    )
 
-def test_positions_3(
-    tests_indices,
-    events_fsw_comparison_theta_0,
-):
-    """TODO."""
-
-    indices, events_dataset = tests_indices
-
-    df = pd.read_csv(events_fsw_comparison_theta_0)
-    df.replace("FILL", GlobalConstants.INT_FILLVAL, inplace=True)
-    selected_rows = df.iloc[indices]
-
-
-    if events_dataset["STOP_TYPE"].data[index] in [1, 2]:
-
-        energy = float(df["Xf"].iloc[index])
-
-
-
-        velocity = get_particle_velocity((xf, yf), (xb, yb), d, tof)
-
-        assert velocity[0] == pytest.approx(
-            float(df["vhatX"].iloc[index]), rel=1e-2
-        )
-        assert velocity[1] == pytest.approx(
-            float(df["vhatY"].iloc[index]), rel=1e-2
-        )
-        assert velocity[2] == pytest.approx(
-            float(df["vhatZ"].iloc[index]), rel=1e-2
-        )
+    assert vhat_x == pytest.approx(
+        df_filt["vhatX"].iloc[indices].astype("float").values, rel=1e-2
+    )
+    assert vhat_y == pytest.approx(
+        df_filt["vhatY"].iloc[indices].astype("float").values, rel=1e-2
+    )
+    assert vhat_z == pytest.approx(
+        df_filt["vhatZ"].iloc[indices].astype("float").values, rel=1e-2
+    )
