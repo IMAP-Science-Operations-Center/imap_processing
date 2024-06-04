024cfa98a272a7d982158ea6186b2001baf5c874
Sean Hoyt <sean.hoyt@lasp.colorado.edu>
2024-06-04 16:28:35 -0600

    added lo l1a intital code

--- imap_processing/lo/l1a/lo_l1a.py
+++ imap_processing/lo/l1a/lo_l1a.py
@@ -1,27 +1,28 @@
-import xarray as xr
-import numpy as np
-from dataclasses import fields
 from collections import namedtuple
-from imap_processing.cdf.imap_cdf_manager import ImapCdfAttributes
+from dataclasses import fields
+
+import xarray as xr
+
 from imap_processing.cdf.global_attrs import ConstantCoordinates
+from imap_processing.cdf.imap_cdf_manager import ImapCdfAttributes
 from imap_processing.cdf.utils import write_cdf
-from imap_processing.lo.l0.lo_apid import LoAPID
-from imap_processing.lo.l0.data_classes.science_direct_events import ScienceDirectEvents
 from imap_processing.lo.l0.data_classes.science_counts import ScienceCounts
+from imap_processing.lo.l0.data_classes.science_direct_events import ScienceDirectEvents
 from imap_processing.lo.l0.data_classes.star_sensor import StarSensor
+
+
 def lo_l1a(dependency):
+    # TODO: decom the CCSDS file
 
-    #TODO: decom the CCSDS file
+    # TODO: sort the decommed packets by SHCOARSE
 
-    #TODO: sort the decommed packets by SHCOARSE
+    # TODO: group the packets by APID
 
-    #TODO: group the packets by APID
+    # TODO: Loop through packets
+    # TODO: Inside Loop: Check for each APID and Initialize data class object with packet contents, add to list
 
-    #TODO: Loop through packets
-    #TODO: Inside Loop: Check for each APID and Initialize data class object with packet contents, add to list
-
-    #TODO: Create dataset
-    #TODO: Write CDF
+    # TODO: Create dataset
+    # TODO: Write CDF
 
     attr_mgr = ImapCdfAttributes()
     attr_mgr.add_instrument_global_attrs(instrument="lo")
@@ -41,15 +42,17 @@ def lo_l1a(dependency):
     elif "_spin_" in dependency:
         # Spin data class doesn't exist yet. fake the fields() output
         data_field_tup = namedtuple("data_field_tup", ["name"])
-        data_fields = [data_field_tup("NUM_COMPLETED"),
-                       data_field_tup("ACQ_END"),
-                       data_field_tup("SPIN_SECONDS"),
-                       data_field_tup("SPIN_SUBSECONDS"),
-                       data_field_tup("ESA_P_DAC"),
-                       data_field_tup("ESA_N_DAC"),
-                       data_field_tup("VAL_PERIOD"),
-                       data_field_tup("VAL_SPIN"),
-                       data_field_tup("SOURCE")]
+        data_fields = [
+            data_field_tup("NUM_COMPLETED"),
+            data_field_tup("ACQ_END"),
+            data_field_tup("SPIN_SECONDS"),
+            data_field_tup("SPIN_SUBSECONDS"),
+            data_field_tup("ESA_P_DAC"),
+            data_field_tup("ESA_N_DAC"),
+            data_field_tup("VAL_PERIOD"),
+            data_field_tup("VAL_SPIN"),
+            data_field_tup("SOURCE"),
+        ]
         logical_source = "imap_lo_l1a_spin"
 
     dataset = create_datasets(attr_mgr, logical_source, data_fields)
@@ -58,7 +61,6 @@ def lo_l1a(dependency):
 
 
 def create_datasets(attr_mgr, logical_source, data_fields):
-
     # NOTE: At this point, we keep epoch time as raw value from packet
     # which is in seconds and spacecraft time. Some instrument uses this
     # raw value in processing. If you want to convert this to datetime
@@ -72,20 +74,27 @@ def create_datasets(attr_mgr, logical_source, data_fields):
 
     dataset = xr.Dataset(
         coords={"epoch": epoch_time},
-        attrs=attr_mgr.get_global_attributes(logical_source)
+        attrs=attr_mgr.get_global_attributes(logical_source),
     )
 
     for field in data_fields:
         field = field.name.lower()
-        if field not in ["shcoarse", "ground_sw_version", "packet_file_name", "ccsds_header", "data"]:
+        if field not in [
+            "shcoarse",
+            "ground_sw_version",
+            "packet_file_name",
+            "ccsds_header",
+            "data",
+        ]:
             print(f"FIELD: {field}")
-            dims = [value for key, value in attr_mgr.get_variable_attributes(field).items() if "DEPEND" in key]
+            dims = [
+                value
+                for key, value in attr_mgr.get_variable_attributes(field).items()
+                if "DEPEND" in key
+            ]
             print(f"DIMS: {dims}")
             dataset[field] = xr.DataArray(
-                [0, 0, 0],
-                dims=dims,
-                attrs=attr_mgr.get_variable_attributes(field)
+                [0, 0, 0], dims=dims, attrs=attr_mgr.get_variable_attributes(field)
             )
 
     return dataset
-
--- imap_processing/tests/lo/test_lo_l1a.py
+++ imap_processing/tests/lo/test_lo_l1a.py
@@ -1,3 +1,5 @@
 from imap_processing.lo.l1a.lo_l1a import lo_l1a
+
+
 def test_lo_l1a():
     print(lo_l1a("imap_lo_l1a_de_20240604_v01.pkts"))
