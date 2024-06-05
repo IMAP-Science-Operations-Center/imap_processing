d04efe85c617ebcc599412b316b869d3279d30f5
Tim Plummer <Timothy.Plummer@lasp.colorado.edu>
2024-06-05 14:29:15 -0600

    begin l1b modification for DE processing

--- imap_processing/hi/l1b/hi_l1b.py
+++ imap_processing/hi/l1b/hi_l1b.py
@@ -3,6 +3,9 @@
 import logging
 from pathlib import Path
 
+import numpy as np
+import xarray as xr
+
 from imap_processing import imap_module_directory
 from imap_processing.cdf.utils import load_cdf
 from imap_processing.hi.hi_cdf_attrs import hi_hk_l1b_global_attrs
@@ -26,12 +29,16 @@ def hi_l1b(l1a_cdf_path: Path):
     processed_data : xarray.Dataset
         Processed xarray dataset
     """
+    logger.info(f"Running Hi L1B processing on file: {l1a_cdf_path.name}")
     l1a_dataset = load_cdf(l1a_cdf_path)
-    packet_enum = HIAPID(l1a_dataset["pkt_apid"].data[0])
+    logical_source_parts = l1a_dataset["logical_source"].split("_")
+    # TODO: apid is not currently stored in all L1A data but should be.
+    #    Use apid to determine what L1B processing function to call
 
     # Housekeeping processing
-    if packet_enum in (HIAPID.H45_APP_NHK, HIAPID.H90_APP_NHK):
-        logger.info(f"Running Hi L1B processing on file: {l1a_cdf_path.name}")
+    if logical_source_parts[-1].endswith("hk"):
+        # if packet_enum in (HIAPID.H45_APP_NHK, HIAPID.H90_APP_NHK):
+        packet_enum = HIAPID(l1a_dataset["pkt_apid"].data[0])
         conversion_table_path = (
             imap_module_directory / "hi" / "l1b" / "hi_eng_unit_convert_table.csv"
         )
@@ -42,9 +49,34 @@ def hi_l1b(l1a_cdf_path: Path):
             comment="#",
             converters={"mnemonic": str.lower},
         )
+
         l1b_dataset.attrs.update(hi_hk_l1b_global_attrs.output())
         return l1b_dataset
-    elif packet_enum in (HIAPID.H45_SCI_DE, HIAPID.H90_SCI_DE):
-        raise NotImplementedError(
-            f"L1B processing not implemented for file: {l1a_cdf_path.name}"
-        )
+    elif logical_source_parts[-1].endswith("de"):
+        l1b_dataset = annotate_direct_events(l1a_dataset)
+
+
+def annotate_direct_events(l1a_dataset):
+    """
+    Perform Hi L1B processing on direct event data.
+
+    Parameters
+    ----------
+    l1a_dataset: xarray.Dataset
+        L1A direct event data.
+
+    Returns
+    -------
+    xarray.Dataset
+        L1B direct event data.
+    """
+    coords = dict()
+    # epoch contains a single element
+    # TODO: What value to use. SPDF says times should indicate the center
+    #    value of the time bin.
+    coords["epoch"] = xr.DataArray(
+        np.empty(1, dtype="datetime64[ns]"),
+        name="epoch",
+        dims=["epoch"],
+        # attrs=ConstantCoordinates.EPOCH,
+    )
