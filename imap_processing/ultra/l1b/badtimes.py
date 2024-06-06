841147882afb924f0fb731d784e90c4fa7ef61a1
Laura Sandoval <46567335+laspsandoval@users.noreply.github.com>
2024-06-06 11:43:50 -0600

    Sit 3 Ultra L1b and L1c (#581)
    * Sit 3 Ultra L1b and L1c

---
+++ imap_processing/ultra/l1b/badtimes.py
@@ -0,0 +1,24 @@
+"""Calculates Badtimes."""
+
+from imap_processing.ultra.utils.ultra_l1_utils import create_dataset
+
+
+def calculate_badtimes(extended_spin_dict, name):
+    """
+    Create dataset with defined datatypes for Badtimes Data.
+
+    Parameters
+    ----------
+    extended_spin_dict: : dict
+        L1b data dictionary.
+    name: str
+        Name of the dataset.
+
+    Returns
+    -------
+    badtimes_dataset : xarray.Dataset
+        Dataset containing the data.
+    """
+    badtimes_dataset = create_dataset(extended_spin_dict, name, "l1b")
+
+    return badtimes_dataset
---
+++ imap_processing/ultra/l1c/histogram.py
@@ -0,0 +1,32 @@
+"""Calculates Histogram."""
+
+import numpy as np
+
+from imap_processing.ultra.utils.ultra_l1_utils import create_dataset
+
+
+def calculate_histogram(histogram_dataset, name):
+    """
+    Create dictionary with defined datatype for Histogram Data.
+
+    Parameters
+    ----------
+    histogram_dataset: xarray.Dataset
+        Dataset containing histogram data.
+
+    Returns
+    -------
+    dataset : xarray.Dataset
+        Dataset containing the data.
+    """
+    histogram_dict = {}
+
+    # Placeholder for calculations
+    epoch = histogram_dataset.coords["epoch"].values
+
+    histogram_dict["epoch"] = epoch
+    histogram_dict["sid"] = np.zeros(len(epoch), dtype=np.uint8)
+
+    dataset = create_dataset(histogram_dict, name, "l1c")
+
+    return dataset
