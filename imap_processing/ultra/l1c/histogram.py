c8ec77598369329410e90e51e4fee3de9a3d2140
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2024-06-06 16:30:39 -0600

    revert for histogram

--- imap_processing/ultra/l1c/histogram.py
+++ imap_processing/ultra/l1c/histogram.py
@@ -1,37 +1,33 @@
 """Calculates Histogram."""
 
+import numpy as np
+
 from imap_processing.ultra.utils.ultra_l1_utils import create_dataset
 
 
-def calculate_histogram(histogram_l1a_dataset, name, instrument_id):
+def calculate_histogram(histogram_dataset, name):
     """
     Create dictionary with defined datatype for Histogram Data.
 
     Parameters
     ----------
-    histogram_l1a_dataset: xarray.Dataset
+    histogram_dataset: xarray.Dataset
         Dataset containing histogram data.
-    name: str
-        Name of the dataset.
-    instrument_id: int
-        Instrument ID.
 
     Returns
     -------
-    histogram_l1c_dataset : xarray.Dataset
+    dataset : xarray.Dataset
         Dataset containing the data.
     """
-    keys_to_copy = [
-        "epoch",
-        "sid",
-        "row",
-        "column",
-        "SHCOARSE",
-        "SPIN",
-        "PACKETDATA",
-    ]
+    histogram_dict = {}
 
-    histogram_dict = {key.lower(): histogram_l1a_dataset[key] for key in keys_to_copy}
-    histogram_l1c_dataset = create_dataset(histogram_dict, name, "l1c", instrument_id)
+    # Placeholder for calculations
+    # TODO: come back and update this data structure.
+    epoch = histogram_dataset.coords["epoch"].values
 
-    return histogram_l1c_dataset
+    histogram_dict["epoch"] = epoch
+    histogram_dict["sid"] = np.zeros(len(epoch), dtype=np.uint8)
+
+    dataset = create_dataset(histogram_dict, name, "l1c")
+
+    return dataset
