c0c7c2af6ac4bbc7169230006b9452412d37e83e
Veronica Martinez <39746325+vmartinez-cu@users.noreply.github.com>
2024-06-06 11:45:26 -0600

    Create HIT L1A CDF for Housekeeping (#583)
    * Create L1A CDFs
    
    -Renamed hit_l1a_decom.py to hit_l1a.py and moved file to l1a dir
    -Added code to create hit l1a xarray datasets and write CDF files
    -Updated test_hit_decom.py to reflect changes in l1a processing
    code
    -Updated hit_cdf_attrs.py to add adc_channels and attributes to
    use as a second dims value for leak_i data variable. Also,
    updated some data variable names to match fields in hk
    dataclasses to easily find matching attributes
    -Added test for hit_l1a code
    
    * Update location of code that creates l1a data products in docs to fix error with PR #583
    
    * Update imap_processing/hit/l1a/hit_l1a.py
    
    Co-authored-by: Greg Lucas <greg.m.lucas@gmail.com>
    
    * Add import for Path library. Update sorting of packets to use global sort_by_time function
    
    * Update imap_processing/hit/l1a/hit_l1a.py
    
    remove .keys() from for loop. not necessary
    
    Co-authored-by: Greg Lucas <greg.m.lucas@gmail.com>
    
    * Add item to list of things to ignore to bypass sphynx errors in PR
    
    * Update imap_processing/hit/l1a/hit_l1a.py
    
    use list comprehension
    
    Co-authored-by: Greg Lucas <greg.m.lucas@gmail.com>
    
    * Update imap_processing/hit/l1a/hit_l1a.py
    
    remove print statement used for development
    
    Co-authored-by: Greg Lucas <greg.m.lucas@gmail.com>
    
    * update comment
    
    * Rename variable for clairity and use APID Enum to avoid using the actual number
    
    * Add TODOs, update HitAPID attributes, update type hint for hit_l1a_data function to comply with python 3.9
    
    * Update imap_processing/tests/hit/test_hit_l1a.py
    
    Simplify test by calling hit_l1a_data function
    
    Co-authored-by: Greg Lucas <greg.m.lucas@gmail.com>
    
    * Combine hit test files under test_hit_l1a.py
    
    * Convert shcoarse to datetime for the epoch xarray data and update cdf file name in test
    
    * Remove redundant comments for apids, add cdf filepaths to log, add TODO regarding logging levels to use
    
    ---------
    
    Co-authored-by: Greg Lucas <greg.m.lucas@gmail.com>

---
+++ imap_processing/tests/hit/test_hit_l1a.py
@@ -0,0 +1,102 @@
+import pathlib
+
+import pytest
+import xarray as xr
+
+from imap_processing import imap_module_directory, utils
+from imap_processing.hit.l0.data_classes.housekeeping import Housekeeping
+from imap_processing.hit.l1a import hit_l1a
+
+
+@pytest.fixture(scope="module")
+def packet_filepath():
+    """Set path to test data file"""
+
+    return imap_module_directory / "tests/hit/test_data/hskp_sample.ccsds"
+
+
+@pytest.fixture(scope="module")
+def unpacked_packets(packet_filepath):
+    """Read test data from file
+
+    Parameters
+    ----------
+    packet_filepath : str
+        Path to ccsds file
+
+    Returns
+    -------
+    sorted_packets : list[space_packet_parser.parser.Packet]
+        A sorted list of decommutated packets
+    """
+    packets = hit_l1a.decom_packets(packet_filepath)
+    sorted_packets = utils.sort_by_time(packets, "SHCOARSE")
+    return sorted_packets
+
+
+def test_group_data(unpacked_packets):
+    """Group packet data classes by apid
+
+    Creates data classes for decommutated packets and
+    groups them by apid
+
+    Parameters
+    ----------
+    unpacked_packets : list[space_packet_parser.parser.Packet]
+        A sorted list of decommutated packets
+    """
+    grouped_data = hit_l1a.group_data(unpacked_packets)
+    assert len(grouped_data.keys()) == 1
+    assert all([isinstance(i, Housekeeping) for i in grouped_data[1251]])
+
+
+def test_create_datasets(unpacked_packets):
+    """Create xarray datasets
+
+    Creates a xarray dataset for each apid and stores
+    them in a dictionary
+
+    Parameters
+    ----------
+    unpacked_packets : list[space_packet_parser.parser.Packet]
+        A sorted list of decommutated packets
+    """
+    grouped_data = hit_l1a.group_data(unpacked_packets)
+    skip_keys = [
+        "shcoarse",
+        "ground_sw_version",
+        "packet_file_name",
+        "ccsds_header",
+        "leak_i_raw",
+    ]
+    datasets_by_apid = hit_l1a.create_datasets(grouped_data, skip_keys=skip_keys)
+    assert len(datasets_by_apid.keys()) == 1
+    assert isinstance(datasets_by_apid[hit_l1a.HitAPID.HIT_HSKP], xr.Dataset)
+
+
+def test_hit_l1a(packet_filepath):
+    """Create L1A CDF files
+
+    Creates a CDF file for each apid dataset and stores all
+    cdf file paths in a list
+
+    Parameters
+    ----------
+    packet_filepath : str
+        Path to ccsds file
+    """
+    cdf_filepaths = hit_l1a.hit_l1a_data(packet_filepath)
+    assert len(cdf_filepaths) == 1
+    assert isinstance(cdf_filepaths[0], pathlib.PurePath)
+    assert cdf_filepaths[0].name == "imap_hit_l1a_hk_20100105_v001.cdf"
+
+
+def test_total_datasets(unpacked_packets):
+    """Test if total number of datasets is correct"""
+    # assert len(unpacked_packets) == total_datasets
+
+
+def test_dataset_dims_length(unpacked_packets):
+    """Test if the time dimension length in the dataset is correct"""
+    # grouped_data = hit_l1a.group_data(unpacked_packets)
+    # assert grouped_data["hit_l1a.HitAPID.HIT_HSKP"].dims["epoch"] == num_packet_times
