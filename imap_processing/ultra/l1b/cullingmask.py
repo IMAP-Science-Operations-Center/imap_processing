4ff03ac92cf112db30273dff631062997c447fdb
Daralynn Rhode <daralynnrhode@gmail.com>
2024-06-13 14:35:58 -0600

    numpydoc updates

--- imap_processing/ultra/l1b/cullingmask.py
+++ imap_processing/ultra/l1b/cullingmask.py
@@ -1,4 +1,4 @@
-"""Calculates Culling Mask."""
+"""Calculate Culling Mask."""
 
 from imap_processing.ultra.utils.ultra_l1_utils import create_dataset
 
@@ -9,14 +9,14 @@ def calculate_cullingmask(extended_spin_dict, name):
 
     Parameters
     ----------
-    extended_spin_dict: dict
+    extended_spin_dict : dict
         L1b data dictionary.
-    name: str
+    name : str
         Name of the dataset.
 
     Returns
     -------
-    cullingmask_dataset : xarray.Dataset
+    dataset
         Dataset containing the data.
     """
     cullingmask_dataset = create_dataset(extended_spin_dict, name, "l1b")
