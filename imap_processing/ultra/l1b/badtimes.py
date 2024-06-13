d11f0a0cbaefc1e2f265ea2b42af355149e7c896
Daralynn Rhode <daralynnrhode@gmail.com>
2024-06-13 14:37:20 -0600

    numpydoc updates

--- imap_processing/ultra/l1b/badtimes.py
+++ imap_processing/ultra/l1b/badtimes.py
@@ -1,4 +1,4 @@
-"""Calculates Badtimes."""
+"""Calculate Badtimes."""
 
 from imap_processing.ultra.utils.ultra_l1_utils import create_dataset
 
@@ -9,14 +9,14 @@ def calculate_badtimes(extended_spin_dict, name):
 
     Parameters
     ----------
-    extended_spin_dict: dict
+    extended_spin_dict : dict
         L1b data dictionary.
-    name: str
+    name : str
         Name of the dataset.
 
     Returns
     -------
-    badtimes_dataset : xarray.Dataset
+    xarray.Dataset
         Dataset containing the data.
     """
     badtimes_dataset = create_dataset(extended_spin_dict, name, "l1b")
