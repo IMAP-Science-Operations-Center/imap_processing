272ba6fbab008358d2e9bcb4f8bdb1161511550a
Ana Manica <ana@manica.org>
2024-06-13 09:49:25 -0600

    Writing initial TODO comments.

--- imap_processing/cdf/tests/test_cdf_attribute_manager.py
+++ imap_processing/cdf/tests/test_cdf_attribute_manager.py
@@ -2,6 +2,21 @@ from pathlib import Path
 
 from imap_processing.cdf.cdf_attribute_manager import CdfAttributeManager
 
+# test __init__(self, data_dir: Path): (?)
+# test load_global_attributes(self, file_path: str):
+# test _load_yaml_data(file_path: str | Path) -> dict: (?)
+# test load_variable_attributes(self, file_name: str) -> None:
+# test get_variable_attributes(self, variable_name: str) -> dict:
+
+# test _load_default_global_attr_schema(sef) -> dict: (?)
+# test _load_default_variable_attr_schema(self) -> dict: (?)
+def test_default_attr_schema():
+    # TODO: initialize CdfAttributeManager object
+    # TODO: assert statement to test default global schema
+    # TODO: assert statement to test default variable schema
+
+def test_load_yaml_data():
+    # TODO: Check if file is properly loaded in ... ?
 
 # @pytest.mark.xfail(reason="Missing IMAP specific global schema")
 def test_global_attribute():
@@ -37,7 +52,6 @@ def test_global_attribute():
     assert mag_l1a_global_attrs["Descriptor"] == "MAG>Magnetometer"
     assert mag_l1a_global_attrs["Logical_source"] == "imap_mag_l1a_norm-raw"
 
-
 def test_variable_attribute():
     cdf_manager = CdfAttributeManager(Path(__file__).parent.parent / "config")
     cdf_manager.load_global_attributes("imap_default_global_cdf_attrs.yaml")
@@ -56,3 +70,5 @@ def test_variable_attribute():
     for variable_attrs in cdf_manager.variable_attributes.values():
         for attr in expected_attributes:
             assert attr in variable_attrs.keys()
+
+    # TODO: Call, and test get_variable_attributes
