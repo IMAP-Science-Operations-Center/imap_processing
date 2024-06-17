d0c3be227e99686b8320412437b7a54fd51dcaca
Ana Manica <ana@manica.org>
2024-06-17 13:24:05 -0600

    Test

--- imap_processing/cdf/tests/test_imap_cdf_manager.py
+++ imap_processing/cdf/tests/test_imap_cdf_manager.py
@@ -3,6 +3,7 @@ from imap_processing.cdf.cdf_attribute_manager import CdfAttributeManager
 from imap_processing.cdf.imap_cdf_manager import ImapCdfAttributes
 
 # test add_instrument_global_attrs(self, instrument: str):
+# Test
 def test_add_instrument_global_attrs():
     # Create an ImapCdfAttributes object
     imap_cdf_manager = ImapCdfAttributes()
