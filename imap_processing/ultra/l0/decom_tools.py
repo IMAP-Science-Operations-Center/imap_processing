2623d328e5b6935a81336288aead0397012bc293
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2024-06-17 13:25:01 -0600

    revert

--- imap_processing/ultra/l0/decom_tools.py
+++ imap_processing/ultra/l0/decom_tools.py
@@ -188,9 +188,9 @@ def decompress_image(
     blocks_per_row = cols // pixels_per_block
 
     # Compressed pixel matrix
-    p = np.zeros((rows, cols), dtype=np.uint32)
+    p = np.zeros((rows, cols), dtype=np.int16)
     # Decompressed pixel matrix
-    p_decom = np.zeros((rows, cols), dtype=np.uint32)
+    p_decom = np.zeros((rows, cols), dtype=np.int16)
 
     pos = 0  # Starting position in the binary string
 
