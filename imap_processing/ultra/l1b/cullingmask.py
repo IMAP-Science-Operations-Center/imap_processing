9578ddca921419b8adb0b370148eb325680ee940
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2024-06-17 10:42:02 -0600

    added extra parameters

--- imap_processing/ultra/l1b/cullingmask.py
+++ imap_processing/ultra/l1b/cullingmask.py
@@ -3,14 +3,14 @@
 from imap_processing.ultra.utils.ultra_l1_utils import create_dataset
 
 
-def calculate_cullingmask(extended_spin_dict, name):
+def calculate_cullingmask(extendedspin_dataset, name):
     """
     Create dataset with defined datatype for Culling Mask Data.
 
     Parameters
     ----------
-    extended_spin_dict: dict
-        L1b data dictionary.
+    extendedspin_dataset: xarray.Dataset
+        Dataset containing rates data.
     name: str
         Name of the dataset.
 
@@ -19,6 +19,19 @@ def calculate_cullingmask(extended_spin_dict, name):
     cullingmask_dataset : xarray.Dataset
         Dataset containing the data.
     """
-    cullingmask_dataset = create_dataset(extended_spin_dict, name, "l1b")
+    keys_to_copy = [
+        "epoch",
+        "spin_number",
+        "spin_start_time",
+        "avg_spin_period",
+        "rate_start_pulses",
+        "rate_stop_pulses",
+        "rate_coin_pulses",
+        "rate_processed_events",
+        "rate_rejected_events",
+    ]
+
+    cullingmask_dict = {key: extendedspin_dataset[key] for key in keys_to_copy}
+    cullingmask_dataset = create_dataset(cullingmask_dict, name, "l1b")
 
     return cullingmask_dataset
