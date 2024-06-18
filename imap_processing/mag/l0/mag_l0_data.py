e97d169eab7951a978ff573c9aa911c66021b965
Daralynn Rhode <daralynnrhode@gmail.com>
2024-06-18 11:22:04 -0600

    numpydoc updates for mag

--- imap_processing/mag/l0/mag_l0_data.py
+++ imap_processing/mag/l0/mag_l0_data.py
@@ -11,14 +11,15 @@ from imap_processing.ccsds.ccsds_data import CcsdsData
 
 
 class Mode(IntEnum):
-    """Enum class for MAG mode.
+    """
+    Enum class for MAG mode.
 
     Attributes
     ----------
     BURST : int
-        APID for Burst mode data
+        APID for Burst mode data.
     NORMAL : int
-        ApID for Normal mode data
+        ApID for Normal mode data.
     """
 
     BURST = 1068
@@ -27,49 +28,50 @@ class Mode(IntEnum):
 
 @dataclass
 class MagL0:
-    """Data class for MAG Level 0 data.
+    """
+    Data class for MAG Level 0 data.
 
     Attributes
     ----------
     ccsds_header: CcsdsData
-        CCSDS Header data
+        CCSDS Header data.
     SHCOARSE: int
-        Mission elapsed time
+        Mission elapsed time.
     PUS_SPARE1: int
-        ESA standard headers, PUS Spare 1
+        ESA standard headers, PUS Spare 1.
     PUS_VERSION: int
-        PUS Version Number
+        PUS Version Number.
     PUS_SPARE2: int
-        PUS Spare 2
+        PUS Spare 2.
     PUS_STYPE: int
-        PUS Service Type
+        PUS Service Type.
     PUS_SSUBTYPE: int
-        PUS Service Subtype - tells number of seconds of data
+        PUS Service Subtype - tells number of seconds of data.
     COMPRESSION: int
         Science Data Compression Flag - indicates if the data compressed - throw error
-        if 1
+        if 1.
     MAGO_ACT: int
-        MAGO Active Status - if MAGo is active. May also be referred to as "FOB"
+        MAGO Active Status - if MAGo is active. May also be referred to as "FOB".
     MAGI_ACT: int
-        MAGI Active Status - if MAGi is active.  May also be referred to as "FIB"
+        MAGI Active Status - if MAGi is active.  May also be referred to as "FIB".
     PRI_SENS: int
-        Primary Sensor - 0 is MAGo, 1 is MAGi
+        Primary Sensor - 0 is MAGo, 1 is MAGi.
     SPARE1: int
-        Spare
+        Spare.
     PRI_VECSEC: int
-        Primary Vectors per Second - lookup for L1b
+        Primary Vectors per Second - lookup for L1b.
     SEC_VECSEC: int
-        Secondary Vectors per second
+        Secondary Vectors per second.
     SPARE2: int
-        Spare
+        Spare.
     PRI_COARSETM: int
-        Primary Coarse Time for first vector, seconds
+        Primary Coarse Time for first vector, seconds.
     PRI_FNTM: int
-        Primary Fine Time for first vector, subseconds
+        Primary Fine Time for first vector, subseconds.
     SEC_COARSETM: int
-        Secondary Coarse Time for first vector, seconds
+        Secondary Coarse Time for first vector, seconds.
     SEC_FNTM: int
-        Secondary Fine Time for first vector, subseconds
+        Secondary Fine Time for first vector, subseconds.
     VECTORS: numpy.ndarray or str
         MAG Science Vectors - divide based on PRI_VECSEC and PUS_SSUBTYPE for vector
         counts. There is a post init call to convert a string into a numpy array -
@@ -98,10 +100,11 @@ class MagL0:
     VECTORS: np.ndarray | str
 
     def __post_init__(self):
-        """Convert Vectors attribute from string to bytearray if needed.
+        """
+        Convert Vectors attribute from string to bytearray if needed.
 
         Also convert encoded "VECSEC" (vectors per second) into proper vectors per
-        second values
+        second values.
         """
         # Convert string output from space_packet_parser to numpy array of
         # big-endian bytes
