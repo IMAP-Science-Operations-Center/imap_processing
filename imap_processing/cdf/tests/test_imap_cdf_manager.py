308d09a52c1377ebcbc56b4b74edacbc2663542c
Ana Manica <ana@manica.org>
2024-06-17 20:17:26 -0600

    Trying different things to pass pre-checks

--- imap_processing/cdf/tests/test_imap_cdf_manager.py
+++ imap_processing/cdf/tests/test_imap_cdf_manager.py
@@ -1,9 +1,10 @@
-from pathlib import Path
-from imap_processing.cdf.cdf_attribute_manager import CdfAttributeManager
+# from pathlib import Path
+# from imap_processing.cdf.cdf_attribute_manager import CdfAttributeManager
 from imap_processing.cdf.imap_cdf_manager import ImapCdfAttributes
 
 # test add_instrument_global_attrs(self, instrument: str):
-# Test
+
+
 def test_add_instrument_global_attrs():
     # Create an ImapCdfAttributes object
     imap_cdf_manager = ImapCdfAttributes()
@@ -18,10 +19,11 @@ def test_add_instrument_global_attrs():
     # Testing reloading data
     imap_cdf_manager.add_instrument_global_attrs("swe")
 
-    #Testing again
+    # Testing again
     swe_instrument = imap_cdf_manager.get_global_attributes("imap_swe_l1a_sci")
     assert swe_instrument["Data_type"] == "L1A_SCI>Level-1A Science data"
     assert swe_instrument["Project"] == "STP>Solar-Terrestrial Physics"
+
 
 # test add_instrument_variable_attrs(self, instrument: str, level: str):
 def test_add_instrument_variable_attrs():
