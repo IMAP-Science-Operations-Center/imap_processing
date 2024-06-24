4a7712a7b9493666fcba8f1a86b48cf3c53f0c4e
Ana Manica <ana@manica.org>
2024-06-24 16:17:59 -0600

    Final Final Commit

--- imap_processing/cdf/imap_cdf_manager.py
+++ imap_processing/cdf/imap_cdf_manager.py
@@ -16,8 +16,8 @@ class ImapCdfAttributes(CdfAttributeManager):
 
     Parameters
     ----------
-    source_dir : Path
-            Source directory.
+    source_dir : Path or None
+        Source directory.
     """
 
     def __init__(self, source_dir=None):
@@ -26,7 +26,7 @@ class ImapCdfAttributes(CdfAttributeManager):
 
         Parameters
         ----------
-        source_dir
+        source_dir : Path or None
             Source directory.
         """
         if source_dir is None:
