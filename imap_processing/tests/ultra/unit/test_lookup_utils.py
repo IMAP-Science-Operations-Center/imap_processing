50b03908b235d1ce2fa112a89604cedfed9bed7a
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2024-07-03 15:28:23 -0600

    remove unnecessary files

--- imap_processing/tests/ultra/unit/test_lookup_utils.py
+++ imap_processing/tests/ultra/unit/test_lookup_utils.py
@@ -15,7 +15,6 @@ base_path = f"{imap_module_directory}/ultra/lookup_tables"
 
 def test_get_y_adjust():
     """Tests function get_y_adjust."""
-    # TODO: Add more test cases and data
 
     yadjust_path = f"{base_path}/yadjust.csv"
     yadjust_df = pd.read_csv(yadjust_path).set_index("dYLUT")
@@ -28,7 +27,6 @@ def test_get_y_adjust():
 
 def test_get_stop_norm():
     """Tests function get_stop_norm."""
-    # TODO: Add more test cases and data
 
     tdc_norm_path = f"{base_path}/ultra45_tdc_norm.csv"
     tdc_norm_df = pd.read_csv(tdc_norm_path, header=1)
@@ -41,7 +39,6 @@ def test_get_stop_norm():
 
 def test_get_back_position():
     """Tests function get_back_position."""
-    # TODO: Add more test cases and data
 
     back_pos_path = f"{base_path}/ultra45_back-pos-luts.csv"
     back_pos_df = pd.read_csv(back_pos_path, index_col="Index_offset")
@@ -54,7 +51,6 @@ def test_get_back_position():
 
 def test_get_egy_norm():
     """Tests function get_egy_norm."""
-    # TODO: Add more test cases and data
 
     egy_norm_path = f"{base_path}/EgyNorm.mem.csv"
     egy_norm_df = pd.read_csv(egy_norm_path)
@@ -66,7 +62,6 @@ def test_get_egy_norm():
 
 def test_get_image_params():
     """Tests function get_image_params."""
-    # TODO: Add more test cases and data
 
     image_params = get_image_params("XFTLTOFF")
 
