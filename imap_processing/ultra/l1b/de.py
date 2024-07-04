627d963c51705c23a9d57d252a1e68e40041725c
Daralynn Rhode <143308810+daralynnrhode@users.noreply.github.com>
2024-07-04 10:14:44 -0600

    Add mypy Pre-Commit For Ultra Pt.3 (#663)
    * config file update
    
    * pyproject update, exclude all but ultra folder
    
    * mypy update for ultra folder
    
    * updates
    
    * last fix
    
    * d
    
    * empty commit
    
    * a
    
    * last
    
    * fixes
    
    * precheck errors from merges
    
    * precheck errors from merge
    
    * last hopefully

--- imap_processing/ultra/l1b/de.py
+++ imap_processing/ultra/l1b/de.py
@@ -1,11 +1,12 @@
 """Calculate Annotated Direct Events."""
 
 import numpy as np
+import xarray as xr
 
 from imap_processing.ultra.utils.ultra_l1_utils import create_dataset
 
 
-def calculate_de(de_dataset, name):
+def calculate_de(de_dataset: xr.Dataset, name: str) -> xr.Dataset:
     """
     Create dataset with defined datatypes for Direct Event Data.
 
