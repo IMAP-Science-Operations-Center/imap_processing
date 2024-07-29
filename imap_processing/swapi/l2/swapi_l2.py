0b90194822da6815ecfaa3f1bf1dce553b61017c
Tenzin Choedon <tenzin.choedon@lasp.colorado.edu>
2024-07-29 09:53:44 -0600

    Feedback changes. Update SWAPI L2 processing

--- imap_processing/swapi/l2/swapi_l2.py
+++ imap_processing/swapi/l2/swapi_l2.py
@@ -10,89 +10,24 @@ from imap_processing.swapi.swapi_utils import SWAPIAPID
 logger = logging.getLogger(__name__)
 
 
-def process_science(l1_dataset: xr.Dataset, data_version: str) -> xr.Dataset:
+TIME_PER_BIN = 0.167  # seconds
+
+
+def swapi_l2(l1_dataset: xr.Dataset, data_version: str) -> xr.Dataset:
     """
     Produce science data to L2.
 
     To process science data to L2, we need to:
     - convert counts to rates. This is done by dividing the counts by the
-        t_bin time. t_bin is the exposure time per energy bin which is
+        TIME_PER_BIN time. TIME_PER_BIN is the exposure time per energy bin which is
         obtained by dividing the time for one complete sweep
         (12 s, coarse + fine sweep) by the total energy steps (72),
-        i.e., t_bin = 12/72 = 0.167 s. This will be constant.
+        i.e., TIME_PER_BIN = 12/72 = 0.167 s. This will be constant.
 
     - update uncertainty. Calculate new uncertainty value using
-        SWP_PCEM_ERR data from level one and divide by t_bin. Eg.
-            SWP_PCEM_UNC = SWP_PCEM_ERR / t_bin
+        SWP_PCEM_ERR data from level one and divide by TIME_PER_BIN. Eg.
+            SWP_PCEM_UNC = SWP_PCEM_ERR / TIME_PER_BIN
         Do the same for SCEM and COIN data.
-
-    Parameters
-    ----------
-    l1_dataset : xarray.Dataset
-        The L1 data input.
-    data_version : str
-        Version of the data product being created.
-
-    Returns
-    -------
-    data : xarray.Dataset
-        Processed data to L2.
-    """
-    # Load the CDF attributes
-    cdf_manager = ImapCdfAttributes()
-    cdf_manager.add_instrument_global_attrs("swapi")
-    cdf_manager.load_variable_attributes("imap_swapi_variable_attrs.yaml")
-
-    t_bin = 0.167  # seconds
-    l2_dataset = l1_dataset.copy()
-    l2_dataset.attrs = cdf_manager.get_global_attributes("imap_swapi_l2_sci")
-    l2_dataset.attrs["Data_version"] = data_version
-    # convert counts to rate
-    l2_dataset["swp_pcem_rate"] = l1_dataset["swp_pcem_counts"] / t_bin
-    l2_dataset["swp_scem_rate"] = l1_dataset["swp_scem_counts"] / t_bin
-    l2_dataset["swp_coin_rate"] = l1_dataset["swp_coin_counts"] / t_bin
-    # update attrs
-    l2_dataset["swp_pcem_rate"].attrs = cdf_manager.get_variable_attributes("pcem_rate")
-    l2_dataset["swp_scem_rate"].attrs = cdf_manager.get_variable_attributes("scem_rate")
-    l2_dataset["swp_coin_rate"].attrs = cdf_manager.get_variable_attributes("coin_rate")
-
-    # update uncertainty
-    l2_dataset["swp_pcem_unc"] = l1_dataset["swp_pcem_err"] / t_bin
-    l2_dataset["swp_scem_unc"] = l1_dataset["swp_scem_err"] / t_bin
-    l2_dataset["swp_coin_unc"] = l1_dataset["swp_coin_err"] / t_bin
-    # update attrs
-    l2_dataset["swp_pcem_unc"].attrs = cdf_manager.get_variable_attributes(
-        "pcem_uncertainty"
-    )
-    l2_dataset["swp_scem_unc"].attrs = cdf_manager.get_variable_attributes(
-        "scem_uncertainty"
-    )
-    l2_dataset["swp_coin_unc"].attrs = cdf_manager.get_variable_attributes(
-        "coin_uncertainty"
-    )
-
-    # drop the counts and err variables
-    l2_dataset = l2_dataset.drop_vars(
-        [
-            "swp_pcem_counts",
-            "swp_scem_counts",
-            "swp_coin_counts",
-            "swp_pcem_err",
-            "swp_scem_err",
-            "swp_coin_err",
-        ]
-    )
-
-    # TODO: add thruster firing flag
-    # TODO: add other flags
-    logger.info("SWAPI L2 processing complete")
-
-    return l2_dataset
-
-
-def swapi_l2(l1_dataset: xr.Dataset, data_version: str) -> xr.Dataset:
-    """
-    Generate science data to L2.
 
     Parameters
     ----------
@@ -112,4 +47,59 @@ def swapi_l2(l1_dataset: xr.Dataset, data_version: str) -> xr.Dataset:
     if apid != SWAPIAPID.SWP_SCI:
         raise ValueError(f"APID {apid} is not supported for L2 processing")
 
-    return process_science(l1_dataset, data_version)
+    # Load the CDF attributes
+    cdf_manager = ImapCdfAttributes()
+    cdf_manager.add_instrument_global_attrs("swapi")
+    cdf_manager.load_variable_attributes("imap_swapi_variable_attrs.yaml")
+
+    # Copy over only certain variables from L1 to L2 dataset
+    l1_data_keys = [
+        "epoch",
+        "energy",
+        "energy_label",
+        "swp_pcem_flags",
+        "swp_scem_flags",
+        "swp_coin_flags",
+    ]
+    l2_dataset = l1_dataset[l1_data_keys].copy()
+    # Copy over attributes
+    l2_dataset.attrs = l1_dataset.attrs
+    # Update L2 specific attributes
+    l2_dataset.attrs["Data_version"] = data_version
+    l2_global_attrs = cdf_manager.get_global_attributes("imap_swapi_l2_sci")
+    l2_dataset.attrs["Data_level"] = l2_global_attrs["Data_level"]
+    l2_dataset.attrs["Data_type"] = l2_global_attrs["Data_type"]
+    l2_dataset.attrs["Logical_source"] = l2_global_attrs["Logical_source"]
+    l2_dataset.attrs["Logical_source_description"] = l2_global_attrs[
+        "Logical_source_description"
+    ]
+
+    # convert counts to rate
+    l2_dataset["swp_pcem_rate"] = l1_dataset["swp_pcem_counts"] / TIME_PER_BIN
+    l2_dataset["swp_scem_rate"] = l1_dataset["swp_scem_counts"] / TIME_PER_BIN
+    l2_dataset["swp_coin_rate"] = l1_dataset["swp_coin_counts"] / TIME_PER_BIN
+    # update attrs
+    l2_dataset["swp_pcem_rate"].attrs = cdf_manager.get_variable_attributes("pcem_rate")
+    l2_dataset["swp_scem_rate"].attrs = cdf_manager.get_variable_attributes("scem_rate")
+    l2_dataset["swp_coin_rate"].attrs = cdf_manager.get_variable_attributes("coin_rate")
+
+    # update uncertainty
+    l2_dataset["swp_pcem_unc"] = l1_dataset["swp_pcem_err"] / TIME_PER_BIN
+    l2_dataset["swp_scem_unc"] = l1_dataset["swp_scem_err"] / TIME_PER_BIN
+    l2_dataset["swp_coin_unc"] = l1_dataset["swp_coin_err"] / TIME_PER_BIN
+    # update attrs
+    l2_dataset["swp_pcem_unc"].attrs = cdf_manager.get_variable_attributes(
+        "pcem_uncertainty"
+    )
+    l2_dataset["swp_scem_unc"].attrs = cdf_manager.get_variable_attributes(
+        "scem_uncertainty"
+    )
+    l2_dataset["swp_coin_unc"].attrs = cdf_manager.get_variable_attributes(
+        "coin_uncertainty"
+    )
+
+    # TODO: add thruster firing flag
+    # TODO: add other flags
+    logger.info("SWAPI L2 processing complete")
+
+    return l2_dataset
--- imap_processing/tests/swapi/test_swapi_l2.py
+++ imap_processing/tests/swapi/test_swapi_l2.py
@@ -1,31 +1,25 @@
+import numpy as np
+
 from imap_processing import imap_module_directory
-from imap_processing.cdf.utils import load_cdf, write_cdf
+from imap_processing.cdf.utils import write_cdf
 from imap_processing.swapi.l1.swapi_l1 import swapi_l1
-from imap_processing.swapi.l2.swapi_l2 import swapi_l2
-from imap_processing.swapi.swapi_utils import SWAPIAPID
+from imap_processing.swapi.l2.swapi_l2 import TIME_PER_BIN, swapi_l2
 
 
-def test_swapi_l1_cdf():
+def test_swapi_l2_cdf():
     """Test housekeeping processing and CDF file creation"""
     l0_data_path = (
         f"{imap_module_directory}/tests/swapi/l0_data/"
         "imap_swapi_l0_raw_20231012_v001.pkts"
     )
     processed_data = swapi_l1(l0_data_path, data_version="v001")
+    l1_dataset = processed_data[0]
 
-    # sci cdf file
-    cdf_filename = "imap_swapi_l1_sci-1min_20100101_v001.cdf"
-
-    cdf_path = write_cdf(processed_data[0])
-    assert cdf_path.name == cdf_filename
-
-    # process science data to l2
-    l1_dataset = load_cdf(cdf_path)
-    assert l1_dataset.attrs["Apid"] == f"{SWAPIAPID.SWP_SCI}"
-    assert l1_dataset.attrs["Plan_id"] == "0"
-    assert l1_dataset.attrs["Sweep_table"] == "0"
-
-    # print("after ready l1 file ", l1_dataset.attrs.keys())
-    l2_processed_data = swapi_l2(l1_dataset, data_version="v001")
-    l2_cdf = write_cdf(l2_processed_data)
+    l2_dataset = swapi_l2(l1_dataset, data_version="v001")
+    l2_cdf = write_cdf(l2_dataset)
     assert l2_cdf.name == "imap_swapi_l2_sci-1min_20100101_v001.cdf"
+
+    # Test uncertainty variables are as expected
+    np.testing.assert_array_equal(
+        l2_dataset["swp_pcem_unc"], l1_dataset["swp_pcem_err"] / TIME_PER_BIN
+    )
