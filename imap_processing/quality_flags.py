0f294ff4068042e7054dec5b62f6f1ddc5af2b69
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2024-07-31 13:07:21 -0600

    test

--- imap_processing/quality_flags.py
+++ imap_processing/quality_flags.py
@@ -3,13 +3,28 @@
 from enum import IntFlag
 
 
-class ModifyFlags(IntFlag):
-    """
-    Quality flags modified.
+class CommonFlags(IntFlag):
+    """Common quality flags."""
 
-    This can be deleted once Python 3.11 is the
-    last support version of Python.
-    """
+    NONE = 0x0
+    INF = 2**0  # bit 0, Infinite value
+    NEG = 2**1  # bit 1, Negative value
+
+
+class ENAFlags(IntFlag):
+    """Common ENA flags."""
+
+    BADSPIN = 2**2  # bit 2, Bad spin
+
+
+class ImapUltraFlags(IntFlag):
+    """IMAP Ultra flags."""
+
+    NONE = CommonFlags.NONE
+    INF = CommonFlags.INF  # bit 0
+    NEG = CommonFlags.NEG  # bit 1
+    BADSPIN = ENAFlags.BADSPIN  # bit 2
+    FLAG1 = 2**3  # bit 2
 
     @property
     def name(self) -> str:
@@ -24,35 +39,11 @@ class ModifyFlags(IntFlag):
         if self._name_ is not None:
             return self._name_
 
-        members = [member for member in ModifyFlags if member & self == member]
+        members = [member for member in ImapUltraFlags if member & self == member]
         return "|".join(str(m).split(".", 1)[-1] for m in members if m != 0x0)
 
 
-class CommonFlags(ModifyFlags):
-    """Common quality flags."""
-
-    NONE = 0x0
-    INF = 2**0  # bit 0, Infinite value
-    NEG = 2**1  # bit 1, Negative value
-
-
-class ENAFlags(ModifyFlags):
-    """Common ENA flags."""
-
-    BADSPIN = 2**2  # bit 2, Bad spin
-
-
-class ImapUltraFlags(ModifyFlags):
-    """IMAP Ultra flags."""
-
-    NONE = CommonFlags.NONE
-    INF = CommonFlags.INF  # bit 0
-    NEG = CommonFlags.NEG  # bit 1
-    BADSPIN = ENAFlags.BADSPIN  # bit 2
-    FLAG1 = 2**3  # bit 2
-
-
-class ImapLoFlags(ModifyFlags):
+class ImapLoFlags(IntFlag):
     """IMAP Lo flags."""
 
     NONE = CommonFlags.NONE
@@ -61,11 +52,43 @@ class ImapLoFlags(ModifyFlags):
     BADSPIN = ENAFlags.BADSPIN  # bit 2
     FLAG2 = 2**3  # bit 2
 
+    @property
+    def name(self) -> str:
+        """
+        Override the default name property to handle combined flags.
 
-class HitFlags(ModifyFlags):
+        Returns
+        -------
+        combined_name : str
+            The combined name of the individual flags.
+        """
+        if self._name_ is not None:
+            return self._name_
+
+        members = [member for member in ImapLoFlags if member & self == member]
+        return "|".join(str(m).split(".", 1)[-1] for m in members if m != 0x0)
+
+
+class HitFlags(IntFlag):
     """Hit flags."""
 
     NONE = CommonFlags.NONE
     INF = CommonFlags.INF  # bit 0
     NEG = CommonFlags.NEG  # bit 1
     FLAG3 = 2**2  # bit 2
+
+    @property
+    def name(self) -> str:
+        """
+        Override the default name property to handle combined flags.
+
+        Returns
+        -------
+        combined_name : str
+            The combined name of the individual flags.
+        """
+        if self._name_ is not None:
+            return self._name_
+
+        members = [member for member in HitFlags if member & self == member]
+        return "|".join(str(m).split(".", 1)[-1] for m in members if m != 0x0)
