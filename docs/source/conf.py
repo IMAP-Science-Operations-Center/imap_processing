99053745e18d55a90ec161bb96d48ea771ff0b9c
Daralynn Rhode <daralynnrhode@gmail.com>
2024-08-06 14:24:54 -0600

    sphinx update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,6 +44,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinxcontrib.openapi",
     "numpydoc",
+    "sphinx_autodoc_typehints",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
