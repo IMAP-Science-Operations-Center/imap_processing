8ff11368f894ca6d0838658321c4125198ea88f3
Tim Plummer <Timothy.Plummer@lasp.colorado.edu>
2024-08-07 17:45:58 -0600

    Add comments about why ensure_spice tests expect an exception to be raised

--- imap_processing/tests/spice/test_kernels.py
+++ imap_processing/tests/spice/test_kernels.py
@@ -52,12 +52,18 @@ def test_ensure_spice_emus_mk_path(func, use_test_metakernel):
 def test_ensure_spice_time_kernels():
     """Test functionality of ensure spice with timekernels set"""
     wrapped = kernels.ensure_spice(spice.et2utc, time_kernels_only=True)
+    # TODO: Update/remove this test when a decision has been made about
+    #   whether IMAP will use the time_kernels_only functionality and the
+    #   ensure_spice decorator has been update.
     with pytest.raises(NotImplementedError):
         _ = wrapped(577365941.184, "ISOC", 3) == "2018-04-18T23:24:31.998"
 
 
 def test_ensure_spice_key_error():
-    """Test functionality of ensure spice with timekernels set"""
+    """Test functionality of ensure spice when all branches fail"""
     wrapped = kernels.ensure_spice(spice.et2utc)
+    # The ensure_spice decorator should raise a SpiceyError when all attempts to
+    # furnish a set of kernels with sufficient coverage for the spiceypy
+    # functions that it decorates.
     with pytest.raises(SpiceyError):
         _ = wrapped(577365941.184, "ISOC", 3) == "2018-04-18T23:24:31.998"
