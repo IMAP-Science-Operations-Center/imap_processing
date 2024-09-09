98674c931bf5732b6f6667b16e7db29215ed916b
Laura Sandoval <46567335+laspsandoval@users.noreply.github.com>
2024-09-09 16:38:23 -0600

    Define spatial bins for pset (#819)
    * Define spatial bins for pset

--- imap_processing/tests/ultra/unit/test_ultra_l1c_pset_bins.py
+++ imap_processing/tests/ultra/unit/test_ultra_l1c_pset_bins.py
@@ -2,7 +2,10 @@
 
 import numpy as np
 
-from imap_processing.ultra.l1c.ultra_l1c_pset_bins import build_energy_bins
+from imap_processing.ultra.l1c.ultra_l1c_pset_bins import (
+    build_energy_bins,
+    build_spatial_bins,
+)
 
 
 def test_build_energy_bins():
@@ -13,7 +16,20 @@ def test_build_energy_bins():
     assert len(energy_bin_start) == 90
     assert len(energy_bin_end) == 90
 
-    # Comparison to expected values
+    # Comparison to expected values.
     np.testing.assert_allclose(energy_bin_end[0], 3.6795, atol=1e-4)
     np.testing.assert_allclose(energy_bin_start[-1], 299.9724, atol=1e-4)
     np.testing.assert_allclose(energy_bin_end[-1], 315.3556, atol=1e-4)
+
+
+def test_build_spatial_bins():
+    """Tests build_spatial_bins function."""
+    az_bin_edges, el_bin_edges = build_spatial_bins()
+
+    assert az_bin_edges[0] == 0
+    assert az_bin_edges[-1] == 360
+    assert len(az_bin_edges) == 721
+
+    assert el_bin_edges[0] == -90
+    assert el_bin_edges[-1] == 90
+    assert len(el_bin_edges) == 361
