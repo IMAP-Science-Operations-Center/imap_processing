e48fa367de05fe951109608ffe46097a21ce8427
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2024-09-11 15:32:15 -0600

    first pass
    added midpoints
    
    modify tests
    
    update tests
    
    needs fix
    
    minor updates
    
    updated cartesian to spherical
    
    added bin_id
    
    update test
    
    update test
    
    update test
    
    update test
    
    update test
    
    update test
    
    update test
    
    update test
    
    update test
    
    update test
    
    update test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test

--- imap_processing/tests/ultra/unit/test_ultra_l1c_pset_bins.py
+++ imap_processing/tests/ultra/unit/test_ultra_l1c_pset_bins.py
@@ -1,30 +1,51 @@
 "Tests bins for pointing sets"
 
 import numpy as np
+import pytest
 
 from imap_processing.ultra.l1c.ultra_l1c_pset_bins import (
+    bin_data,
     build_energy_bins,
     build_spatial_bins,
+    cartesian_to_spherical,
+    create_unique_identifiers,
+    get_histogram,
 )
+
+
+@pytest.fixture()
+def test_data():
+    """Test data fixture."""
+    vx_sc = np.array([-186.5575, 508.5697, 508.5697, 508.5697])
+    vy_sc = np.array([-707.5707, -516.0282, -516.0282, -516.0282])
+    vz_sc = np.array([618.0569, 892.6931, 892.6931, 892.6931])
+    energy = np.array([3.384, 3.385, 200, 200])
+    v = np.column_stack((vx_sc, vy_sc, vz_sc))
+
+    return v, energy
 
 
 def test_build_energy_bins():
     """Tests build_energy_bins function."""
-    energy_bin_start, energy_bin_end = build_energy_bins()
+    energy_bin_edges = build_energy_bins()
+    energy_bin_start = energy_bin_edges[:-1]
+    energy_bin_end = energy_bin_edges[1:]
 
-    assert energy_bin_start[0] == 3.5
-    assert len(energy_bin_start) == 90
-    assert len(energy_bin_end) == 90
+    assert energy_bin_start[0] == 0
+    assert energy_bin_start[1] == 3.385
+    assert len(energy_bin_edges) == 25
 
     # Comparison to expected values.
-    np.testing.assert_allclose(energy_bin_end[0], 3.6795, atol=1e-4)
-    np.testing.assert_allclose(energy_bin_start[-1], 299.9724, atol=1e-4)
-    np.testing.assert_allclose(energy_bin_end[-1], 315.3556, atol=1e-4)
+    np.testing.assert_allclose(energy_bin_end[1], 4.137, atol=1e-4)
+    np.testing.assert_allclose(energy_bin_start[-1], 279.810, atol=1e-4)
+    np.testing.assert_allclose(energy_bin_end[-1], 341.989, atol=1e-4)
 
 
 def test_build_spatial_bins():
     """Tests build_spatial_bins function."""
-    az_bin_edges, el_bin_edges = build_spatial_bins()
+    az_bin_edges, el_bin_edges, az_bin_midpoints, el_bin_midpoints = (
+        build_spatial_bins()
+    )
 
     assert az_bin_edges[0] == 0
     assert az_bin_edges[-1] == 360
@@ -33,3 +54,110 @@ def test_build_spatial_bins():
     assert el_bin_edges[0] == -90
     assert el_bin_edges[-1] == 90
     assert len(el_bin_edges) == 361
+
+    assert len(az_bin_midpoints) == 720
+    np.testing.assert_allclose(az_bin_midpoints[0], 0.25, atol=1e-4)
+    np.testing.assert_allclose(az_bin_midpoints[-1], 359.75, atol=1e-4)
+
+    assert len(el_bin_midpoints) == 360
+    np.testing.assert_allclose(el_bin_midpoints[0], -89.75, atol=1e-4)
+    np.testing.assert_allclose(el_bin_midpoints[-1], 89.75, atol=1e-4)
+
+
+def test_cartesian_to_spherical(test_data):
+    """Tests cartesian_to_spherical function."""
+    v, _ = test_data
+
+    az_sc, el_sc, r = cartesian_to_spherical(v)
+
+    # MATLAB code outputs:
+    np.testing.assert_allclose(
+        np.radians(az_sc), np.array([1.31300, 2.34891]), atol=1e-05, rtol=0
+    )
+    np.testing.assert_allclose(
+        np.radians(el_sc), np.array([-0.70136, -0.88901]), atol=1e-05, rtol=0
+    )
+
+
+def test_get_histogram(test_data):
+    """Tests get_histogram function."""
+    v, energy = test_data
+
+    az_bin_edges, el_bin_edges, az_bin_midpoints, el_bin_midpoints = (
+        build_spatial_bins()
+    )
+    energy_bin_edges = build_energy_bins()
+
+    hist, non_zero_counts = get_histogram(
+        v, energy, az_bin_edges, el_bin_edges, energy_bin_edges
+    )
+
+    assert hist.shape == (
+        len(az_bin_edges) - 1,
+        len(el_bin_edges) - 1,
+        len(energy_bin_edges) - 1,
+    )
+    assert hist.sum() == np.sum(non_zero_counts)
+
+    az_indices, el_indices, energy_bin_id = np.argwhere(hist > 0).T
+    az, el, _ = cartesian_to_spherical(v)
+    assert np.allclose(
+        np.unique(az), np.unique(az_bin_midpoints[az_indices]), atol=0.25
+    )
+    assert np.allclose(
+        np.unique(el), np.unique(el_bin_midpoints[el_indices]), atol=0.25
+    )
+
+    for az_idx, el_idx, energy_idx, count in zip(
+        az_indices, el_indices, energy_bin_id, non_zero_counts
+    ):
+        assert hist[az_idx, el_idx, energy_idx] == count
+
+
+def test_create_unique_identifiers(test_data):
+    """Tests create_unique_identifiers function."""
+    v, energy = test_data
+
+    az_bin_edges, el_bin_edges, az_bin_midpoints, el_bin_midpoints = (
+        build_spatial_bins()
+    )
+    energy_bin_edges = build_energy_bins()
+
+    hist, non_zero_counts = get_histogram(
+        v, energy, az_bin_edges, el_bin_edges, energy_bin_edges
+    )
+
+    counts, bin_id, midpoints = create_unique_identifiers(
+        hist, az_bin_midpoints, el_bin_midpoints, non_zero_counts
+    )
+
+    assert counts.size == len(bin_id[0]) * len(bin_id[1]) * len(bin_id[2])
+    assert sum(non_zero_counts) == np.sum(counts)
+    assert np.array_equal(midpoints[0], az_bin_midpoints[np.unique(bin_id[0])])
+    assert np.array_equal(midpoints[1], el_bin_midpoints[np.unique(bin_id[1])])
+
+
+def test_bin_data(test_data):
+    """Tests bin_data function."""
+    v, energy = test_data
+
+    counts, bin_id, midpoints, energy_edge_start = bin_data(v, energy)
+    az_bin_edges, el_bin_edges, az_bin_midpoints, el_bin_midpoints = (
+        build_spatial_bins()
+    )
+    energy_bin_edges = build_energy_bins()
+
+    hist, non_zero_counts = get_histogram(
+        v, energy, az_bin_edges, el_bin_edges, energy_bin_edges
+    )
+
+    az_indices = bin_id[0]  # Attitude indices
+    el_indices = bin_id[1]  # Elevation indices
+    energy_indices = bin_id[2]  # Energy bin indices
+
+    for i, az_idx in enumerate(np.unique(az_indices)):
+        for j, el_idx in enumerate(np.unique(el_indices)):
+            for k, energy_idx in enumerate(np.unique(energy_indices)):
+                expected_count = hist[az_idx, el_idx, energy_idx]
+                actual_count = counts[i, j, k]
+                assert actual_count == expected_count
--- imap_processing/ultra/l1c/ultra_l1c_pset_bins.py
+++ imap_processing/ultra/l1c/ultra_l1c_pset_bins.py
@@ -1,35 +1,39 @@
 """Module to create bins for pointing sets."""
 
 import numpy as np
+from numpy.typing import NDArray
+
+# TODO: add species binning.
 
 
-def build_energy_bins() -> tuple[np.ndarray, np.ndarray]:
+def build_energy_bins() -> NDArray[np.float64]:
     """
     Build energy bin boundaries.
 
     Returns
     -------
-    energy_bin_start : np.ndarray
-        Array of energy bin start values.
-    energy_bin_end : np.ndarray
-        Array of energy bin end values.
+    energy_bin_edges : np.ndarray
+        Array of energy bin edges.
     """
-    alpha = 0.05  # deltaE/E
-    energy_start = 3.5  # energy start for the Ultra grids
-    n_bins = 90  # number of energy bins
+    # TODO: these value will almost certainly change.
+    alpha = 0.2  # deltaE/E
+    energy_start = 3.385  # energy start for the Ultra grids
+    n_bins = 23  # number of energy bins
 
     # Calculate energy step
     energy_step = (1 + alpha / 2) / (1 - alpha / 2)
 
     # Create energy bins.
-    bin_edges = energy_start * energy_step ** np.arange(n_bins + 1)
-    energy_bin_start = bin_edges[:-1]
-    energy_bin_end = bin_edges[1:]
+    energy_bin_edges = energy_start * energy_step ** np.arange(n_bins + 1)
+    # Add a zero to the left side for outliers and round to nearest 3 decimal places.
+    energy_bin_edges = np.around(np.insert(energy_bin_edges, 0, 0), 3)
 
-    return energy_bin_start, energy_bin_end
+    return energy_bin_edges
 
 
-def build_spatial_bins(spacing: float = 0.5) -> tuple[np.ndarray, np.ndarray]:
+def build_spatial_bins(
+    spacing: float = 0.5,
+) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """
     Build spatial bin boundaries for azimuth and elevation.
 
@@ -44,11 +48,221 @@ def build_spatial_bins(spacing: float = 0.5) -> tuple[np.ndarray, np.ndarray]:
         Array of azimuth bin boundary values.
     el_bin_edges : np.ndarray
         Array of elevation bin boundary values.
+    az_bin_midpoints : np.ndarray
+        Array of azimuth bin midpoint values.
+    el_bin_midpoints : np.ndarray
+        Array of elevation bin midpoint values.
     """
     # Azimuth bins from 0 to 360 degrees.
     az_bin_edges = np.arange(0, 360 + spacing, spacing)
+    az_bin_midpoints = az_bin_edges[:-1] + spacing / 2  # Midpoints between edges
 
     # Elevation bins from -90 to 90 degrees.
     el_bin_edges = np.arange(-90, 90 + spacing, spacing)
+    el_bin_midpoints = el_bin_edges[:-1] + spacing / 2  # Midpoints between edges
 
-    return az_bin_edges, el_bin_edges
+    return az_bin_edges, el_bin_edges, az_bin_midpoints, el_bin_midpoints
+
+
+def cartesian_to_spherical(
+    v: tuple[np.ndarray, np.ndarray, np.ndarray],
+) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Convert cartesian coordinates to spherical coordinates.
+
+    Parameters
+    ----------
+    v : tuple[np.ndarray, np.ndarray, np.ndarray]
+        The x,y,z-components of the velocity vector.
+
+    Returns
+    -------
+    az : np.ndarray
+        The azimuth angles in degrees.
+    el : np.ndarray
+        The elevation angles in degrees.
+    r : np.ndarray
+        The radii, or magnitudes, of the vectors.
+    """
+    vx, vy, vz = np.hsplit(v, 3)
+    vx, vy, vz = vx.flatten(), vy.flatten(), vz.flatten()  # Flatten the arrays
+
+    # Magnitude of the velocity vector
+    magnitude_v = np.sqrt(vx**2 + vy**2 + vz**2)
+
+    vhat_x = -vx / magnitude_v
+    vhat_y = -vy / magnitude_v
+    vhat_z = -vz / magnitude_v
+
+    # Convert from cartesian to spherical coordinates (azimuth, elevation)
+    # Radius (magnitude)
+    r = np.sqrt(vhat_x**2 + vhat_y**2 + vhat_z**2)
+
+    # Elevation angle (angle from the z-axis, range: [-pi/2, pi/2])
+    el = np.arcsin(vhat_z / r)
+
+    # Azimuth angle (angle in the xy-plane, range: [0, 2*pi])
+    az = np.arctan2(vhat_y, vhat_x)
+
+    # Ensure azimuth is from 0 to 2PI
+    az = az % (2 * np.pi)
+
+    return np.degrees(az), np.degrees(el), r
+
+
+def get_histogram(
+    v: tuple[np.ndarray, np.ndarray, np.ndarray],
+    energy: np.ndarray,
+    az_bin_edges: np.ndarray,
+    el_bin_edges: np.ndarray,
+    energy_bin_edges: np.ndarray,
+) -> tuple[NDArray, NDArray]:
+    """
+    Compute a 3D histogram of the particle data.
+
+    Parameters
+    ----------
+    v : tuple[np.ndarray, np.ndarray, np.ndarray]
+        The x,y,z-components of the velocity vector.
+    energy : np.ndarray
+        The particle energy.
+    az_bin_edges : np.ndarray
+        Array of azimuth bin boundary values.
+    el_bin_edges : np.ndarray
+        Array of elevation bin boundary values.
+    energy_bin_edges : np.ndarray
+        Array of energy bin edges.
+
+    Returns
+    -------
+    hist : np.ndarray
+        A 3D histogram array.
+    non_zero_counts : np.ndarray
+        An array of non-zero counts.
+    """
+    az, el, _ = cartesian_to_spherical(v)
+
+    # 3D binning.
+    hist, _ = np.histogramdd(
+        sample=(az, el, energy), bins=[az_bin_edges, el_bin_edges, energy_bin_edges]
+    )
+
+    non_zero_counts = hist[hist > 0]
+
+    return hist, non_zero_counts
+
+
+def create_unique_identifiers(
+    hist: np.ndarray,
+    az_bin_midpoints: np.ndarray,
+    el_bin_midpoints: np.ndarray,
+    non_zero_counts: np.ndarray,
+) -> tuple:
+    """
+    Create unique identifiers for spatial bins and generate non-zero counts.
+
+    Parameters
+    ----------
+    hist : np.ndarray
+        3D histogram with counts for azimuth, elevation, and energy bins.
+    az_bin_midpoints : np.ndarray
+        Midpoints of the azimuth bins.
+    el_bin_midpoints : np.ndarray
+        Midpoints of the elevation bins.
+    non_zero_counts : np.ndarray
+        Counts of particles in each non-zero bin.
+
+    Returns
+    -------
+    counts : np.ndarray
+        A 2D array containing the binned counts for each spatial and energy bin.
+    bin_id : tuple[np.ndarray, np.ndarray]
+        Unique identifiers for the spatial and energy bins.
+    midpoints : tuple[np.ndarray, np.ndarray]
+        Azimuth and elevation midpoints for the spatial bins.
+    """
+    # Extract the indices of all non-zero elements in the 3D hist array
+    az_bin_id, el_bin_id, energy_bin_id = np.argwhere(hist > 0).T
+
+    # Unique identifier for spatial bins by combining azimuth and elevation indices
+    bin_id = (np.unique(az_bin_id), np.unique(el_bin_id), np.unique(energy_bin_id))
+
+    # Get unique azimuth and elevation midpoints
+    midpoints = (
+        np.unique(az_bin_midpoints[az_bin_id]),
+        np.unique(el_bin_midpoints[el_bin_id]),
+    )
+
+    # Determine positions in unique coordinates for assigning counts
+    az_positions = np.searchsorted(bin_id[0], az_bin_id)
+    el_positions = np.searchsorted(bin_id[1], el_bin_id)
+    energy_positions = np.searchsorted(bin_id[2], energy_bin_id)
+
+    counts = np.zeros((len(bin_id[0]), len(bin_id[1]), len(bin_id[2])))
+    counts[az_positions, el_positions, energy_positions] = non_zero_counts
+
+    return counts, bin_id, midpoints
+
+
+def bin_data(v: tuple[np.ndarray, np.ndarray, np.ndarray], energy: np.ndarray) -> tuple:
+    """
+    Bin the data spatially and by energy.
+
+    Parameters
+    ----------
+    v : tuple[np.ndarray, np.ndarray, np.ndarray]
+        A 2D array containing the x, y, z components of velocity vector.
+    energy : np.ndarray
+        A 1D array with shape (N,) containing the particle energy.
+
+    Returns
+    -------
+    counts : np.ndarray
+        A 2D array containing the binned counts for each spatial and energy bin.
+    bin_id : tuple[np.ndarray, np.ndarray]
+        Unique identifiers for the spatial and energy bins.
+    midpoints : tuple[np.ndarray, np.ndarray]
+        Azimuth and elevation midpoints for the spatial bins.
+    energy_edge_start : np.ndarray
+        The starting edges of the energy bins corresponding to non-zero counts.
+
+    Notes
+    -----
+    The returned data can be used to construct a xarray Dataset as follows:
+
+    ds = xr.Dataset(
+        {
+            "az_midpoint": ("az_bin_id", midpoints[0]),
+            "el_midpoint": ("el_bin_id", midpoints[1]),
+            "energy_edge_start": ("energy_bin_id", energy_edge_start),
+            "counts": (("az_bin_id", "el_bin_id","energy_bin_id"), counts),
+        },
+        coords={
+            "az_bin_id": bin_id[0],
+            "el_bin_id": bin_id[1],
+            "energy_bin_id": bin_id[2],
+        }
+    )
+    """
+    az_bin_edges, el_bin_edges, az_bin_midpoints, el_bin_midpoints = (
+        build_spatial_bins()
+    )
+    energy_bin_edges = build_energy_bins()
+
+    # Compute the 3D histogram of the particle data
+    hist, non_zero_counts = get_histogram(
+        v, energy, az_bin_edges, el_bin_edges, energy_bin_edges
+    )
+
+    # Get the starting edges for energy bins
+    energy_bin_start = energy_bin_edges[:-1]
+
+    # Create unique identifiers and map non-zero counts to 2D histogram array
+    counts, bin_id, midpoints = create_unique_identifiers(
+        hist, az_bin_midpoints, el_bin_midpoints, non_zero_counts
+    )
+
+    _, _, energy_bin = np.argwhere(hist > 0).T
+    energy_edge_start = energy_bin_start[energy_bin]
+
+    return counts, bin_id, midpoints, energy_edge_start
