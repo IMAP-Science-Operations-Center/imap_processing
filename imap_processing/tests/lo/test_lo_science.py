8f1fef9a1a3f2f21f4de3286f6d6835637e82ceb
Sean Hoyt <sean.hoyt@lasp.colorado.edu>
2024-10-09 13:13:44 -0600

    Lo L1A Direct Event Refactor (#971)
    * refactored DE
    
    * added parse_de_bin function for better readability
    
    * added unit test for lo parse_events
    
    * added test for parse_de_bin
    
    * split up parse_events parsing into fixed and variable field functions
    
    * added loop through fields
    
    * added new unit tests for fixed and variable field functinos
    
    * removed unneeded de dataclass
    
    * using data bits tuples to get de fields

---
+++ imap_processing/tests/lo/test_lo_science.py
@@ -0,0 +1,148 @@
+import numpy as np
+import pytest
+import xarray as xr
+
+from imap_processing.cdf.imap_cdf_manager import ImapCdfAttributes
+from imap_processing.lo.l0.lo_science import (
+    parse_de_bin,
+    parse_events,
+    parse_fixed_fields,
+    parse_variable_fields,
+)
+
+
+@pytest.fixture()
+def fake_de_dataset():
+    # DE One
+    absent_1 = "0000"  # case 0
+    time_1 = "000001100100"  # 100
+    energy_1 = "010"  # 2
+    mode_1 = "1"
+    tof0_1 = "0000000000"
+    # TOF1 not transmitted
+    tof2_1 = "000000010"  # 2
+    tof3_1 = "000011"  # 3
+    cksm_1 = "0000"  # 0
+    # POS not transmitted
+
+    # DE Two
+    absent_2 = "1010"  # case 10
+    time_2 = "000001100100"  # 100
+    energy_2 = "010"  # 2
+    mode_2 = "1"
+    # TOF0 not transmitted
+    tof1_2 = "000000001"  # 1
+    # TOF2, TOF3, CKSM not transmitted
+    pos_2 = "00"  # 0
+
+    de_data = (
+        absent_1
+        + time_1
+        + energy_1
+        + mode_1
+        + tof0_1
+        + tof2_1
+        + tof3_1
+        + cksm_1
+        + absent_2
+        + time_2
+        + energy_2
+        + mode_2
+        + tof1_2
+        + pos_2
+    )
+    dataset = xr.Dataset(
+        data_vars=dict(
+            count=(["time"], np.array([2])),
+            data=(["time"], np.array([de_data])),
+        )
+    )
+
+    return dataset
+
+
+@pytest.fixture()
+def attr_mgr():
+    attr_mgr = ImapCdfAttributes()
+    attr_mgr.add_instrument_global_attrs(instrument="lo")
+    attr_mgr.add_instrument_variable_attrs(instrument="lo", level="l1a")
+    attr_mgr.add_global_attribute("Data_version", "v000")
+    return attr_mgr
+
+
+@pytest.fixture()
+def initialized_dataset(fake_de_dataset, attr_mgr):
+    fake_de_dataset.attrs["bit_pos"] = 0
+    de_fields = [
+        "coincidence_type",
+        "de_time",
+        "esa_step",
+        "mode",
+        "tof0",
+        "tof1",
+        "tof2",
+        "tof3",
+        "cksm",
+        "pos",
+    ]
+    for field in de_fields:
+        fake_de_dataset[field] = xr.DataArray(
+            np.full(2, attr_mgr.get_variable_attributes(field)["FILLVAL"]),
+            dims="direct_events",
+        )
+    return fake_de_dataset
+
+
+def test_parse_events(fake_de_dataset, attr_mgr):
+    # Act
+    dataset = parse_events(fake_de_dataset, attr_mgr)
+
+    # Assert
+    np.testing.assert_array_equal(dataset["coincidence_type"].values, np.array([0, 10]))
+    np.testing.assert_array_equal(dataset["de_time"].values, np.array([100, 100]))
+    np.testing.assert_array_equal(dataset["esa_step"].values, np.array([2, 2]))
+    np.testing.assert_array_equal(dataset["mode"].values, np.array([1, 1]))
+    np.testing.assert_array_equal(dataset["tof0"].values, np.array([0 << 1, 65535]))
+    np.testing.assert_array_equal(dataset["tof1"].values, np.array([65535, 1 << 1]))
+    np.testing.assert_array_equal(dataset["tof2"].values, np.array([2 << 1, 65535]))
+    np.testing.assert_array_equal(dataset["tof3"].values, np.array([3 << 1, 65535]))
+    np.testing.assert_array_equal(dataset["cksm"].values, np.array([0 << 1, 255]))
+    np.testing.assert_array_equal(dataset["pos"].values, np.array([255, 0]))
+
+
+def test_parse_fixed_fields(initialized_dataset):
+    # Act
+    dataset = parse_fixed_fields(initialized_dataset, 0, 0)
+
+    # Assert
+    np.testing.assert_array_equal(
+        dataset["coincidence_type"].values, np.array([0, 255])
+    )
+    np.testing.assert_array_equal(dataset["de_time"].values, np.array([100, 65535]))
+    np.testing.assert_array_equal(dataset["esa_step"].values, np.array([2, 255]))
+    np.testing.assert_array_equal(dataset["mode"].values, np.array([1, 255]))
+
+
+def test_parse_variable_fields(initialized_dataset):
+    # Arrange
+    initialized_dataset["coincidence_type"].values = np.array([0, 255])
+    initialized_dataset["mode"].values = np.array([1, 255])
+    initialized_dataset.attrs["bit_pos"] = 20
+
+    # Act
+    dataset = parse_variable_fields(initialized_dataset, 0, 0)
+
+    # Assert
+    np.testing.assert_array_equal(dataset["tof0"].values, np.array([0 << 1, 65535]))
+    np.testing.assert_array_equal(dataset["tof1"].values, np.array([65535, 65535]))
+    np.testing.assert_array_equal(dataset["tof2"].values, np.array([2 << 1, 65535]))
+    np.testing.assert_array_equal(dataset["tof3"].values, np.array([3 << 1, 65535]))
+    np.testing.assert_array_equal(dataset["cksm"].values, np.array([0 << 1, 255]))
+    np.testing.assert_array_equal(dataset["pos"].values, np.array([255, 255]))
+
+
+def test_parse_de_bin(initialized_dataset):
+    # Act
+    parsed_int = parse_de_bin(initialized_dataset, 0, 4, 0)
+    # Assert
+    assert parsed_int == 0
