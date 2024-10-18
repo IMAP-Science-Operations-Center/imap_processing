a514cb5d3633300433acb9f684e0f4abad00ded1
Tim Plummer <Timothy.Plummer@lasp.colorado.edu>
2024-10-18 09:11:21 -0600

    Added simulated Hi L1A file for integrated L1B test Added kernels needed for calculating geometry to imap_ena_sim_metakernel.temlate Updated Hi L1B integration test to use simulated L1A as input

--- imap_processing/tests/hi/test_hi_l1b.py
+++ imap_processing/tests/hi/test_hi_l1b.py
@@ -6,6 +6,7 @@ import numpy as np
 import pytest
 import xarray as xr
 
+from imap_processing.cdf.utils import load_cdf
 from imap_processing.hi.l1a.hi_l1a import hi_l1a
 from imap_processing.hi.l1b.hi_l1b import (
     CoincidenceBitmap,
@@ -13,7 +14,7 @@ from imap_processing.hi.l1b.hi_l1b import (
     compute_hae_coordinates,
     hi_l1b,
 )
-from imap_processing.hi.utils import HIAPID, HiConstants
+from imap_processing.hi.utils import HiConstants
 from imap_processing.spice.geometry import SpiceFrame
 
 
@@ -29,16 +30,19 @@ def test_hi_l1b_hk(hi_l0_test_data_path):
     assert l1b_dataset.attrs["Logical_source"] == "imap_hi_l1b_45sensor-hk"
 
 
-def test_hi_l1b_de(create_de_data, tmp_path):
+@pytest.mark.external_kernel()
+@pytest.mark.use_test_metakernel("imap_ena_sim_metakernel.template")
+def test_hi_l1b_de(hi_l1a_test_data_path):
     """Test coverage for imap_processing.hi.hi_l1b.hi_l1b() with
     direct events L1A as input"""
-    # TODO: once things are more stable, check in an L1A DE file as test data
+    l1a_test_file_path = (
+        hi_l1a_test_data_path / "imap_hi_l1a_45sensor-de_20250415_v000.cdf"
+    )
     # Process using test data
-    bin_data_path = create_de_data(HIAPID.H45_SCI_DE.value)
     data_version = "001"
-    processed_data = hi_l1a(packet_file_path=bin_data_path, data_version=data_version)
+    l1a_dataset = load_cdf(l1a_test_file_path)
 
-    l1b_dataset = hi_l1b(processed_data[0], data_version=data_version)
+    l1b_dataset = hi_l1b(l1a_dataset, data_version=data_version)
     assert l1b_dataset.attrs["Logical_source"] == "imap_hi_l1b_45sensor-de"
     assert len(l1b_dataset.data_vars) == 14
 
