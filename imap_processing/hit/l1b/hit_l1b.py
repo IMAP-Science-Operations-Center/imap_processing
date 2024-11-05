68d1ea5d0e4fa851fac1e0696515d5b378150de6
Veronica Martinez <veronica.martinez@lasp.colorado.edu>
2024-11-04 18:20:30 -0700

    Simplify housekeeping dataset creation by calling function from utils directly from hit_l1b() and update unit tests accordingly. Add unit tests to check housekeeping variables and attributes

--- imap_processing/hit/l1b/hit_l1b.py
+++ imap_processing/hit/l1b/hit_l1b.py
@@ -4,13 +4,12 @@ import logging
 
 import xarray as xr
 
-from imap_processing import imap_module_directory
 from imap_processing.hit.hit_utils import (
     HitAPID,
     get_attribute_manager,
-    process_housekeeping,
+    get_datasets_by_apid,
+    process_housekeeping_data,
 )
-from imap_processing.utils import packet_file_to_datasets
 
 logger = logging.getLogger(__name__)
 
@@ -20,6 +19,8 @@ logger = logging.getLogger(__name__)
 def hit_l1b(dependencies: dict, data_version: str) -> list[xr.Dataset]:
     """
     Will process HIT data to L1B.
+
+    Processes dependencies needed to create L1B data products.
 
     Parameters
     ----------
@@ -32,64 +33,28 @@ def hit_l1b(dependencies: dict, data_version: str) -> list[xr.Dataset]:
 
     Returns
     -------
-    cdf_filepaths : xarray.Dataset
-        L1B processed data.
+    processed_data : list[xarray.Dataset]
+        List of L1B datasets.
     """
-    # Create datasets
-    datasets = []
+    # Create the attribute manager for this data level
+    attr_mgr = get_attribute_manager(data_version, "l1b")
+
+    # Create L1B datasets
+    datasets: list = []
     if "imap_hit_l0_raw" in dependencies:
+        logger.info("Creating HIT L1B housekeeping dataset")
+        # Unpack ccsds file to xarray datasets
         packet_file = dependencies["imap_hit_l0_raw"]
-        datasets = create_l1b_hk_dataset(packet_file, data_version)
-    elif "imap_hit_l1a_countrates" in dependencies:
+        datasets_by_apid = get_datasets_by_apid(packet_file, derived=True)
+        # Process housekeeping to l1b.
+        datasets.append(
+            process_housekeeping_data(
+                datasets_by_apid[HitAPID.HIT_HSKP], attr_mgr, "imap_hit_l1b_hk"
+            )
+        )
+        logger.info("HIT L1B housekeeping dataset created")
+    if "imap_hit_l1a_countrates" in dependencies:
         # TODO: process science data. placeholder for future code
         pass
 
     return datasets
-
-
-def create_l1b_hk_dataset(packet_file: str, data_version: str) -> list[xr.Dataset]:
-    """
-    Create HIT L1B housekeeping dataset.
-
-    Reads in L0 CCSDS file using packet_file_to_datasets with
-    use_derived_value=True to get housekeeping data with
-    engineering units as a Xarray Dataset. This dataset is then
-    updated to replace the leak variables with a single
-    leak_i 2D array and add the dataset attributes and
-    coordinates, and data variable dimensions according to
-    specifications in a cdf yaml file.
-
-    Parameters
-    ----------
-    packet_file : str
-        Path to the CCSDS data packet file.
-    data_version : str
-        Version of the data product being created.
-
-    Returns
-    -------
-    dataset : xarray.Dataset
-        An updated dataset ready for CDF conversion.
-    """
-    logger.info("Creating HIT L1B housekeeping dataset")
-
-    # Unpack ccsds file
-    packet_definition = (
-        imap_module_directory / "hit/packet_definitions/hit_packet_definitions.xml"
-    )
-    datasets_by_apid = packet_file_to_datasets(
-        packet_file=packet_file,
-        xtce_packet_definition=packet_definition,
-        use_derived_value=True,
-    )
-
-    # Create the attribute manager for this data level
-    attr_mgr = get_attribute_manager(data_version, "l1b")
-
-    # Process housekeeping to l1b.
-    logical_source = "imap_hit_l1b_hk"
-    dataset = process_housekeeping(
-        datasets_by_apid[HitAPID.HIT_HSKP], attr_mgr, logical_source
-    )
-    logger.info("HIT L1B housekeeping dataset created")
-    return [dataset]
--- imap_processing/tests/hit/test_hit_l1b.py
+++ imap_processing/tests/hit/test_hit_l1b.py
@@ -26,15 +26,135 @@ def dependencies(packet_filepath):
     return data_dict
 
 
-def test_create_l1b_hk_dataset(packet_filepath):
-    """Test creating housekeeping L1B dataset
+@pytest.fixture()
+def hk_dataset(dependencies):
+    """Get the housekeeping dataset"""
+    datasets = hit_l1b.hit_l1b(dependencies, "001")
+    for dataset in datasets:
+        if dataset.attrs["Logical_source"] == "imap_hit_l1b_hk":
+            return dataset
 
-    Creates a xarray dataset for housekeeping data
-    """
-    datasets = hit_l1b.create_l1b_hk_dataset(packet_filepath, "001")
-    assert len(datasets) == 1
-    assert isinstance(datasets[0], xr.Dataset)
-    assert datasets[0].attrs["Logical_source"] == "imap_hit_l1b_hk"
+
+def test_hit_l1b_hk_dataset_variables(hk_dataset):
+    """Test the variables in the housekeeping dataset"""
+    # Define the keys that should have dropped from the housekeeping dataset
+    dropped_keys = {
+        "pkt_apid",
+        "sc_tick",
+        "version",
+        "type",
+        "sec_hdr_flg",
+        "seq_flgs",
+        "src_seq_ctr",
+        "pkt_len",
+        "hskp_spare1",
+        "hskp_spare2",
+        "hskp_spare3",
+        "hskp_spare4",
+        "hskp_spare5",
+    }
+    # Define the keys that should be present in the housekeeping dataset
+    valid_keys = {
+        "heater_on",
+        "fsw_version_b",
+        "ebox_m12va",
+        "phasic_stat",
+        "ebox_3d4vd",
+        "ebox_p2d0vd",
+        "temp1",
+        "last_bad_seq_num",
+        "ebox_m5d7va",
+        "ebox_p12va",
+        "table_status",
+        "enable_50khz",
+        "mram_disabled",
+        "temp3",
+        "preamp_l1a",
+        "l2ab_bias",
+        "l34b_bias",
+        "fsw_version_c",
+        "num_evnt_last_hk",
+        "dac1_enable",
+        "preamp_l234b",
+        "analog_temp",
+        "fee_running",
+        "fsw_version_a",
+        "num_errors",
+        "test_pulser_on",
+        "dac0_enable",
+        "preamp_l1b",
+        "l1ab_bias",
+        "l34a_bias",
+        "leak_i",
+        "last_good_cmd",
+        "lvps_temp",
+        "idpu_temp",
+        "temp2",
+        "preamp_l234a",
+        "last_good_seq_num",
+        "num_good_cmds",
+        "heater_control",
+        "hvps_temp",
+        "ebox_p5d7va",
+        "spin_period_long",
+        "enable_hvps",
+        "temp0",
+        "spin_period_short",
+        "dyn_thresh_lvl",
+        "num_bad_cmds",
+        "adc_mode",
+        "ebox_5d1vd",
+        "active_heater",
+        "last_error_num",
+        "last_bad_cmd",
+        "ref_p5v",
+        "code_checksum",
+        "mode",
+    }
+    # Check that the dataset has the correct variables
+    assert valid_keys == set(hk_dataset.data_vars.keys())
+    assert set(dropped_keys).isdisjoint(set(hk_dataset.data_vars.keys()))
+
+
+def test_hit_l1b_hk_dataset_attributes(hk_dataset):
+    """Test the attributes, dims, and coords in the housekeeping dataset"""
+    # Define the housekeeping dataset attributes
+    dataset_attrs = {
+        "Data_level": "1B",
+        "Data_type": "L1B_HK>Level-1B Housekeeping",
+        "Data_version": "001",
+        "Descriptor": "HIT>IMAP High-energy Ion Telescope",
+        "Discipline": "Solar Physics>Heliospheric Physics",
+        "File_naming_convention": "source_descriptor_datatype_yyyyMMdd_vNNN",
+        "HTTP_LINK": "https://imap.princeton.edu/",
+        "Instrument_type": "Particles (space)",
+        "LINK_TITLE": "IMAP The Interstellar Mapping and Acceleration Probe",
+        "Logical_file_id": None,
+        "Logical_source": "imap_hit_l1b_hk",
+        "Logical_source_description": "IMAP Mission HIT Instrument Level-1B "
+        "Housekeeping Data.",
+        "Mission_group": "IMAP",
+        "PI_affiliation": "Princeton University",
+        "PI_name": "Prof. David J. McComas",
+        "Project": "STP>Solar Terrestrial Probes",
+        "Source_name": "IMAP>Interstellar Mapping and Acceleration Probe",
+        "TEXT": "The High-energy Ion Telescope (HIT) measures the elemental "
+        "composition, energy spectra, angle distributions, and arrival "
+        "times of high-energy ions. HIT delivers full-sky coverage from "
+        "a wide instrument field-of-view (FOV) to enable a high resolution "
+        "of ion measurements, such as observing shock-accelerated ions, "
+        "determining the origin of the solar energetic particles (SEPs) "
+        "spectra, and resolving particle transport in the heliosphere. "
+        "See https://imap.princeton.edu/instruments/hit for more details.\n",
+    }
+
+    # Define the coordinates and dimensions. Both have equivalent values
+    dataset_coords_dims = {"epoch", "adc_channels", "adc_channels_label"}
+
+    # Check that the dataset has the correct attributes, coordinates, and dimensions
+    assert hk_dataset.attrs == dataset_attrs
+    assert hk_dataset.coords.keys() == dataset_coords_dims
+    assert hk_dataset.sizes.keys() == dataset_coords_dims
 
 
 def test_hit_l1b(dependencies):
@@ -49,6 +169,7 @@ def test_hit_l1b(dependencies):
     """
     # TODO: update assertions after science data processing is completed
     datasets = hit_l1b.hit_l1b(dependencies, "001")
+
     assert len(datasets) == 1
     assert isinstance(datasets[0], xr.Dataset)
     assert datasets[0].attrs["Logical_source"] == "imap_hit_l1b_hk"
