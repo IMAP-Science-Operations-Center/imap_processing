81bdef2e328908a62dbf0c26189bf25202585e87
Maxine Hartnett <maxine.hartnett@lasp.colorado.edu>
2024-11-20 15:53:31 -0700

    Fixing docs

--- imap_processing/glows/l2/glows_l2_data.py
+++ imap_processing/glows/l2/glows_l2_data.py
@@ -12,58 +12,58 @@ class HistogramL2:
 
     This class collects multiple HistogramL1B classes into one L2 per observational day.
 
-    flight_software_version
-    number_of_good_l1b_inputs: int
+    flight_software_version : str
+    number_of_good_l1b_inputs : int
         number of good-time Level-1B times used for generation of Level-2 data
-    total_l1b_inputs: int
+    total_l1b_inputs : int
       number of all Level-1B times for observational day
-    identifier
+    identifier : int
         unique Level-2 histogram identifier
-    start_time
+    start_time : numpy.double
         UTC start time of a given observational day
-    end_time
+    end_time : numpy.double
         UTC end time of a given observational day
-    daily_lightcurve
+    daily_lightcurve : numpy.ndarray
         arrays for observational-day-accumulated lightcurve
-    filter_temperature_average
+    filter_temperature_average : numpy.ndarray
         observational-day-averaged filter temperature [Celsius deg]
-    filter_temperature_variance
+    filter_temperature_variance : numpy.ndarray
         standard deviation for filter temperature [Celsius deg]
-    hv_voltage_average
+    hv_voltage_average : numpy.ndarray
         observational-day-averaged channeltron voltage [volt]
-    hv_voltage_variance
+    hv_voltage_variance : numpy.ndarray
         standard deviation for channeltron voltage [volt]
-    spin_period_average
+    spin_period_average : numpy.ndarray
         observational-day-averaged spin period [s] (onboard value)
-    spin_period_variance
+    spin_period_variance : numpy.ndarray
         a standard deviation for spin period [s]
-    pulse_length_average
+    pulse_length_average : numpy.ndarray
         observational-day-averaged pulse length [μs]
-    pulse_length_variance
+    pulse_length_variance : numpy.ndarray
         standard deviation for pulse length [μs]
-    spin_period_ground_average
+    spin_period_ground_average : numpy.ndarray
         observational-day-averaged spin period [s] (ground value)
-    spin_period_ground_variance
+    spin_period_ground_variance : numpy.ndarray
         a standard deviation for spin period [s]
-    position_angle_offset_average
+    position_angle_offset_average : numpy.ndarray
         observational-day-averaged GLOWS angular offset [deg]
-    position_angle_offset_variance
+    position_angle_offset_variance : numpy.ndarray
         standard deviation for GLOWS angular offset [seg]
-    spin_axis_orientation_variance
+    spin_axis_orientation_variance : numpy.ndarray
         standard deviation for spin-axis longitude and latitude [deg]
-    spacecraft_location_variance
+    spacecraft_location_variance : numpy.ndarray
         standard deviation for ecliptic coordinates [km] of IMAP
-    spacecraft_velocity_variance
+    spacecraft_velocity_variance : numpy.ndarray
         standard deviation for IMAP velocity components [km/s]
-    spin_axis_orientation_average
+    spin_axis_orientation_average : numpy.ndarray
         observational-day-averaged spin-axis ecliptic longitude and latitude [deg]
-    spacecraft_location_average
+    spacecraft_location_average : numpy.ndarray
         observational-day-averaged Cartesian ecliptic coordinates ⟨X⟩, ⟨Y ⟩, ⟨Z⟩ [km]
         of IMAP
-    spacecraft_velocity_average
+    spacecraft_velocity_average : numpy.ndarray
         observational-day-averaged values ⟨VX ⟩, ⟨VY ⟩, ⟨VZ ⟩ of IMAP velocity
         components [km/s] (Cartesian ecliptic frame)
-    bad_time_flag_occurrences
+    bad_time_flag_occurrences : numpy.ndarray
         numbers of occurrences of blocks for each bad-time flag during observational day
     """
 
