d168a8a68db51684e4a002995038ca3bd4afdec4
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2025-01-17 12:43:09 -0700

    response to pr

--- imap_processing/quality_flags.py
+++ imap_processing/quality_flags.py
@@ -37,6 +37,16 @@ class ENAFlags(FlagNameMixin):
     BADSPIN = 2**2  # bit 2, Bad spin
 
 
+class ImapAttitudeUltraFlags(FlagNameMixin):
+    """IMAP Ultra flags."""
+
+    NONE = CommonFlags.NONE
+    INF = CommonFlags.INF  # bit 0
+    NEG = CommonFlags.NEG  # bit 1
+    BADSPIN = ENAFlags.BADSPIN  # bit 2
+    FLAG1 = 2**3  # bit 3
+
+
 class ImapHkUltraFlags(FlagNameMixin):
     """IMAP Ultra HK flags."""
 
