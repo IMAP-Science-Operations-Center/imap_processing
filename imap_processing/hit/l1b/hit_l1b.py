65165457fb228e74e33bba76bab18cf6101ee245
Veronica Martinez <veronica.martinez@lasp.colorado.edu>
2025-01-22 15:59:15 -0700

    Add function to process standard rates dataset

--- imap_processing/hit/l1b/hit_l1b.py
+++ imap_processing/hit/l1b/hit_l1b.py
@@ -4,6 +4,7 @@ import logging
 
 import xarray as xr
 
+from imap_processing.cdf.imap_cdf_manager import ImapCdfAttributes
 from imap_processing.hit.hit_utils import (
     HitAPID,
     get_attribute_manager,
@@ -34,7 +35,7 @@ def hit_l1b(dependencies: dict, data_version: str) -> list[xr.Dataset]:
     Returns
     -------
     processed_data : list[xarray.Dataset]
-        List of L1B datasets.
+        List of L1B datasets. Total of four datasets.
     """
     # Create the attribute manager for this data level
     attr_mgr = get_attribute_manager(data_version, "l1b")
@@ -52,8 +53,84 @@ def hit_l1b(dependencies: dict, data_version: str) -> list[xr.Dataset]:
             )
         )
         logger.info("HIT L1B housekeeping dataset created")
-    if "imap_hit_l1a_countrates" in dependencies:
-        # TODO: process science data. placeholder for future code
+    if "imap_hit_l1a_count-rates" in dependencies:
+        # Process science data to L1B
+        l1a_counts_dataset = dependencies["imap_hit_l1a_count-rates"]
+        # Process standard rates
+        datasets.append(process_standard_rates_data(l1a_counts_dataset, attr_mgr))
+        # TODO: Process summed rates
+        # TODO: Process sectored rates
         pass
 
     return datasets
+
+
+def process_standard_rates_data(
+    raw_counts_dataset: xr.Dataset, attr_mgr: ImapCdfAttributes
+) -> xr.Dataset:
+    """
+    Will process L1B standard rates data from raw L1A counts data.
+
+    Parameters
+    ----------
+    raw_counts_dataset : xr.Dataset
+        The L1A counts dataset.
+    attr_mgr : AttributeManager
+        The attribute manager for the data level.
+
+    Returns
+    -------
+    xr.Dataset
+        The processed L1B standard rates dataset.
+    """
+    # Create a new dataset to store the L1B standard rates
+    l1b_standard_rates_dataset = xr.Dataset()
+
+    # Add required coordinates from the raw_counts_dataset
+    coords = [
+        "epoch",
+        "gain",
+        "sngrates_index",
+        "coinrates_index",
+        "pbufrates_index",
+        "l2fgrates_index",
+        "l2bgrates_index",
+        "l3fgrates_index",
+        "l3bgrates_index",
+        "penfgrates_index",
+        "penbgrates_index",
+        "ialirtrates_index",
+    ]
+    l1b_standard_rates_dataset = l1b_standard_rates_dataset.assign_coords(
+        {coord: raw_counts_dataset.coords[coord] for coord in coords}
+    )
+
+    # Define list of fields from the raw_counts_dataset to calculate standard rates
+    standard_rate_fields = [
+        "sngrates",
+        "coinrates",
+        "pbufrates",
+        "l2fgrates",
+        "l2bgrates",
+        "l3fgrates",
+        "l3bgrates",
+        "penfgrates",
+        "penbgrates",
+        "ialirtrates",
+        "l4fgrates",
+        "l4bgrates",
+    ]
+
+    # Calculate livetime from the livetime counter
+    livetime = raw_counts_dataset["livetime"] / 270
+
+    # Calculate standard rates by dividing the raw counts by livetime for
+    # data variables with names that contain a substring from a defined
+    # list of field names.
+    for var in raw_counts_dataset.data_vars:
+        if var != "livetime" and any(
+            base_var in var for base_var in standard_rate_fields
+        ):
+            l1b_standard_rates_dataset[var] = raw_counts_dataset[var] / livetime
+    raw_counts_dataset.attrs.update(attr_mgr.get_attrs("imap_hit_l1b_standard-rates"))
+    return l1b_standard_rates_dataset
