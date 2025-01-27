c6517025db006fd8cbff790f38a038f41ecc6457
Maxine Hartnett <maxine.hartnett@lasp.colorado.edu>
2025-01-27 09:16:55 -0700

    fixing mypy

--- imap_processing/glows/l1b/glows_l1b_data.py
+++ imap_processing/glows/l1b/glows_l1b_data.py
@@ -613,7 +613,7 @@ class HistogramL1B:
         return tuple(getattr(self, out.name) for out in dataclasses.fields(self))
 
     @staticmethod
-    def deserialize_flags(raw: int) -> np.ndarray:
+    def deserialize_flags(raw: int) -> np.ndarray[int]:
         """
         Deserialize the flags into a list.
 
@@ -629,6 +629,8 @@ class HistogramL1B:
         """
         # there are only 10 flags in the on-board flag array, additional flags are added
         # later.
-        flags: np.ndarray = np.array([bool((raw >> i) & 1) for i in range(10)])
+        flags: np.ndarray[bool] = np.array(
+            [bool((raw >> i) & 1) for i in range(10)], dtype=bool
+        )
 
         return flags
