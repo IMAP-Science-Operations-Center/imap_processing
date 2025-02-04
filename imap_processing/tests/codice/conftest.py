0dd0aff64ec59bf919547a73c2462bb17997abe4
Matthew Bourque <matthew.bourque@lasp.colorado.edu>
2025-02-04 12:45:51 -0700

    Added code to download CoDICE L0 test file from AWS via API instead of keeping it within the repo

--- imap_processing/tests/codice/conftest.py
+++ imap_processing/tests/codice/conftest.py
@@ -1,3 +1,5 @@
+import requests
+
 from imap_processing import imap_module_directory
 
 TEST_DATA_PATH = imap_module_directory / "tests" / "codice" / "data"
@@ -41,3 +43,14 @@ VALIDATION_DATA = [
     TEST_DATA_PATH / "validation" / "imap_codice_l1a_lo-pha_20241110193700_v0.0.0.cdf",
     TEST_DATA_PATH / "validation" / "imap_codice_l1a_hi-pha_20241110193700_v0.0.0.cdf",
 ]  # fmt: skip
+
+# Download CoDICE L0 test data and write it to the appropriate directory
+response = requests.get(
+    "https://api.dev.imap-mission.com/download/test_data/imap_codice_l0_raw_20241110_v001.pkts"
+)
+if response.status_code == 200:
+    with open(TEST_L0_FILE, "wb") as file:
+        file.write(response.content)
+    print(f"Downloaded file: {TEST_L0_FILE}")
+else:
+    print(f"Failed to download file: {response.status_code}")
