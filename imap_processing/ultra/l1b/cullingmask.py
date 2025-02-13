edbbc66b1dcdd8a079163a4a370f6be55e8d58a8
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2025-02-12 17:07:27 -0700

    added culling mask

--- imap_processing/ultra/l1b/cullingmask.py
+++ imap_processing/ultra/l1b/cullingmask.py
@@ -2,7 +2,7 @@
 
 import xarray as xr
 
-from imap_processing.ultra.utils.ultra_l1_utils import create_dataset
+from imap_processing.quality_flags import ImapAttitudeUltraFlags, ImapRatesUltraFlags
 
 
 def calculate_cullingmask(
@@ -25,14 +25,32 @@ def calculate_cullingmask(
     cullingmask_dataset : xarray.Dataset
         Dataset containing the data.
     """
-    cullingmask_dict = {}
-    cullingmask_dict["spin_number"] = extendedspin_dataset["spin_number"]
-    cullingmask_dict["energy_bin_geometric_mean"] = extendedspin_dataset[
-        "energy_bin_geometric_mean"
-    ]
+    # If the spin rate was too high or low then the spin should be thrown out.
+    rate_mask = (
+        extendedspin_dataset["quality_attitude"] & ImapAttitudeUltraFlags.SPINRATE.value
+    ) == 0
+    good_spins = extendedspin_dataset["spin_number"].values[rate_mask]
+    good_attitude_dataset = extendedspin_dataset.sel(spin_number=good_spins)
 
-    # TODO: add more data to cullingmask_dict.
+    # Counts in every energy bin should be equal to zero to throw out the spin.
+    zero_counts = (
+        (
+            good_attitude_dataset["quality_ena_rates"]
+            & ImapRatesUltraFlags.ZEROCOUNTS.value
+        )
+        != 0
+    ).all(dim="energy_bin_geometric_mean")
+    zero_counts_mask = ~zero_counts
+    non_zero_dataset = good_attitude_dataset.sel(
+        spin_number=good_attitude_dataset["spin_number"][zero_counts_mask]
+    )
 
-    cullingmask_dataset = create_dataset(cullingmask_dict, name, "l1b", data_version)
+    # If the rates at any energy level are too high then throw out the entire spin.
+    high_rates_mask = (
+        non_zero_dataset["quality_ena_rates"] & ImapRatesUltraFlags.HIGHRATES.value == 0
+    ).all(dim="energy_bin_geometric_mean")
+    cullingmask_dataset = non_zero_dataset.sel(
+        spin_number=non_zero_dataset["spin_number"][high_rates_mask]
+    )
 
     return cullingmask_dataset
