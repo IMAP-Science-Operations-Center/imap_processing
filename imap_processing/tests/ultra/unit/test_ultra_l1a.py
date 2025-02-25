68d7e8edac0d127826a6b4a0c9961ea5ffceb5d7
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2025-02-24 17:14:42 -0700

    added a unit test and removed the drop duplicate section

--- imap_processing/tests/ultra/unit/test_ultra_l1a.py
+++ imap_processing/tests/ultra/unit/test_ultra_l1a.py
@@ -258,10 +258,7 @@ def test_cdf_rates(ccsds_path_theta_0, decom_test_data):
     test_data = ultra_l1a(
         ccsds_path_theta_0, data_version="001", apid=ULTRA_RATES.apid[0]
     )
-    # TODO: Dropping duplicates to ignore ISTP for now. Need to update test data
-    # or wait for an update to cdflib
-    test_data[0] = test_data[0].sortby("epoch").groupby("epoch").first()
-    test_data_path = write_cdf(test_data[0])
+    test_data_path = write_cdf(test_data[0], istp=False)
 
     assert test_data_path.exists()
     assert test_data_path.name == "imap_ultra_l1a_45sensor-rates_20240207_v001.cdf"
@@ -324,10 +321,7 @@ def test_cdf_events(ccsds_path_theta_0, decom_ultra_aux, decom_test_data):
     test_data = ultra_l1a(
         ccsds_path_theta_0, data_version="001", apid=ULTRA_EVENTS.apid[0]
     )
-    # TODO: Dropping duplicates to ignore ISTP for now. Need to update test data
-    # or wait for an update to cdflib
-    test_data[0] = test_data[0].sortby("epoch").groupby("epoch").first()
-    test_data_path = write_cdf(test_data[0])
+    test_data_path = write_cdf(test_data[0], istp=False)
 
     assert test_data_path.exists()
     assert test_data_path.name == "imap_ultra_l1a_45sensor-de_20240207_v001.cdf"
@@ -352,10 +346,10 @@ def test_get_event_time():
     counters_for_met = []
     for i in range(len(decom_events["EVENTID"])):
         event_id = decom_events["EVENTID"][i]
-        met_extracted = event_id >> np.uint64(32)
+        met_extracted = event_id >> np.int64(32)
         assert met_extracted == np.uint64(
             decom_ultra_dict[ULTRA_EVENTS.apid[0]]["SHCOARSE"][i]
         )
-        counters_for_met.append(event_id & np.uint64(0xFFFFFFFF))
+        counters_for_met.append(event_id & np.int64(0xFFFFFFFF))
 
     assert counters_for_met == [0, 0, 0, 1]
