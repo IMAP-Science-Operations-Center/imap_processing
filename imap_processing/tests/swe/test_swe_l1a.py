d0396e1af9b31a6f3a7fb8a41486fc1e210dade8
Tenzin Choedon <36522642+tech3371@users.noreply.github.com>
2025-03-05 16:53:16 -0700

    SWE: Add CDF attrs for non-science apid (#1430)

--- imap_processing/tests/swe/test_swe_l1a.py
+++ imap_processing/tests/swe/test_swe_l1a.py
@@ -10,3 +10,21 @@ def test_cdf_creation():
     cem_raw_cdf_filepath = write_cdf(processed_data[0])
 
     assert cem_raw_cdf_filepath.name == "imap_swe_l1a_sci_20240510_v001.cdf"
+
+
+def test_cdf_creation_hk():
+    test_data_path = "tests/swe/l0_data/2024051010_SWE_HK_packet.bin"
+    processed_data = swe_l1a(imap_module_directory / test_data_path, "001")
+
+    hk_cdf_filepath = write_cdf(processed_data[0])
+
+    assert hk_cdf_filepath.name == "imap_swe_l1a_hk_20240510_v001.cdf"
+
+
+def test_cdf_creation_cem_raw():
+    test_data_path = "tests/swe/l0_data/2024051011_SWE_CEM_RAW_packet.bin"
+    processed_data = swe_l1a(imap_module_directory / test_data_path, "001")
+
+    cem_raw_cdf_filepath = write_cdf(processed_data[0])
+
+    assert cem_raw_cdf_filepath.name == "imap_swe_l1a_cem-raw_20240510_v001.cdf"
