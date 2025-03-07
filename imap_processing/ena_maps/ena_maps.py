7c1e579c5fdbb81d18449673246d9dae4f0bc419
Nat Kerman <nkerman@users.noreply.github.com>
2025-03-07 11:56:53 -0700

    Docs changes in response to PR comments

--- imap_processing/ena_maps/ena_maps.py
+++ imap_processing/ena_maps/ena_maps.py
@@ -503,8 +503,39 @@ class RectangularSkyMap(AbstractSkyMap):
     """
     Map which tiles the sky with a 2D rectangular grid of azimuth/elevation pixels.
 
-    NOTE: Internally, the map is stored as a 1D array of pixels,
-    with the final (-1) axis as the spatial axis.
+    ^  |15,  75|45,  75|75,  75|105,  75|...|255,  75|285,  75|315,  75|345,  75|
+    |  |15,  45|45,  45|75,  45|105,  45|...|255,  45|285,  45|315,  45|345,  45|
+    |  |15,  15|45,  15|75,  15|105,  15|...|255,  15|285,  15|315,  15|345,  15|
+    |  |15, -15|45, -15|75, -15|105, -15|...|255, -15|285, -15|315, -15|345, -15|
+    |  |15, -45|45, -45|75, -45|105, -45|...|255, -45|285, -45|315, -45|345, -45|
+    |  |15, -75|45, -75|75, -75|105, -75|...|255, -75|285, -75|315, -75|345, -75|
+    |
+    ---------------------------------------------------------------> Azimuth (degrees)
+    Elevation (degrees)
+
+    Fig. 1: Example of a rectangular grid of pixels in azimuth and elevation coordinates
+    in degrees, with a spacing of 30 degrees. There will be 12 azimuth bins and 6
+    elevation bins in this example, resulting in 72 pixels in the map.
+
+    NOTE: Internally, the map is stored as a 1D array of pixels, and all data arrays
+    are stored with the final (-1) axis as the only spatial axis, representing the
+    pixel index in the 1D array.
+    A multidimentional value (e.g. counts, with energy levels at each pixel)
+    will be stored as a 2D array with the first axis as the energy dimension and the
+    second axis as the pixel index.
+
+    ^  |5|11|17|23|29|35|41|47|53|59|65|71|
+    |  |4|10|16|22|28|34|40|46|52|58|64|70|
+    |  |3|9 |15|21|27|33|39|45|51|57|63|69|
+    |  |2|8 |14|20|26|32|38|44|50|56|62|68|
+    |  |1|7 |13|19|25|31|37|43|49|55|61|67|
+    |  |0|6 |12|18|24|30|36|42|48|54|60|66|
+    ---------------------------------------> Azimuth
+    Elevation
+
+    Fig. 2: The 1D indices of the pixels in Fig. 1.
+    Note that the indices are raveled from the 2D grid of (az, el) such that as one
+    increases in pixel index, elevation increments first, then azimuth.
 
     Parameters
     ----------
@@ -521,23 +552,28 @@ class RectangularSkyMap(AbstractSkyMap):
     ):
         # Define the core properties of the map:
         self.tiling_type = SkyTilingType.RECTANGULAR  # Type of tiling of the sky
-        self.spacing_deg = spacing_deg
+
+        # The reference Spice frame of the map, in which angles are defined
         self.spice_reference_frame = spice_frame
+
+        # Angular spacing of the map grid (degrees) defines the number, size of pixels.
+        self.spacing_deg = spacing_deg
         self.sky_grid = spatial_utils.AzElSkyGrid(
             spacing_deg=self.spacing_deg,
         )
+        # Unwrap the az, el grids to 1D array of points tiling the sky
+        az_points = self.sky_grid.az_grid.ravel()
+        el_points = self.sky_grid.el_grid.ravel()
 
-        # Solid angles of each pixel in the map grid in units of steradians
+        # Stack so axis 0 is different pixels, and axis 1 is (az, el) of the pixel
+        self.az_el_points = np.column_stack((az_points, el_points))
+        self.num_points = self.az_el_points.shape[0]
+
+        # Calculate solid angles of each pixel in the map grid in units of steradians
         self.solid_angle_grid = spatial_utils.build_solid_angle_map(
             spacing_deg=self.spacing_deg,
         )
-
-        # Unwrap the az, el, solid angle grids to series of points tiling the sky
-        az_points = self.sky_grid.az_grid.ravel()
-        el_points = self.sky_grid.el_grid.ravel()
-        self.az_el_points = np.column_stack((az_points, el_points))
         self.solid_angle_points = self.solid_angle_grid.ravel()
-        self.num_points = self.az_el_points.shape[0]
 
         # Initialize empty data dictionary to store map data
         self.data_dict: dict[str, NDArray] = {}
@@ -560,7 +596,7 @@ class RectangularSkyMap(AbstractSkyMap):
 
 class HealpixSkyMap(AbstractSkyMap):
     """
-    Map which tiles the sky with a Healpix tessellation.
+    Map which tiles the sky with a Healpix tessellation of equal-area pixels.
 
     Parameters
     ----------
@@ -578,19 +614,34 @@ class HealpixSkyMap(AbstractSkyMap):
         # Define the core properties of the map:
         self.tiling_type = SkyTilingType.HEALPIX
         self.spice_reference_frame = spice_frame
+
+        # Tile the sky with a Healpix tessellation. Defined by nside, nested parameters.
         self.nside = nside
+        self.nested = nested
+
+        # Calculate how many pixels cover the sky and the approximate resolution (rad)
         self.num_points = hp.nside2npix(nside)
+        self.approx_resolution = hp.nside2resol(nside, arcmin=False)
+
+        # The centers of each pixel in the Healpix tessellation in azimuth (az) and
+        # elevation (el) coordinates (radians) within the map's Spice frame.
         pixel_az, pixel_el = np.deg2rad(
             hp.pix2ang(
                 nside=nside, ipix=np.arange(self.num_points), nest=nested, lonlat=True
             )
         )
+        # Stack so axis 0 is different pixels, and axis 1 is (az, el) of the pixel
         self.az_el_points = np.column_stack((pixel_az, pixel_el))
+
+        # Tracks Per-Pixel Solid Angle in steradians.
         self.solid_angle = hp.nside2pixarea(nside, degrees=False)
+
+        # Solid angle is equal at all pixels, but define
+        # solid_angle_points to be consistent with RectangularSkyMap
         self.solid_angle_points = np.full(self.num_points, self.solid_angle)
-        self.approx_resolution = hp.nside2resol(nside, arcmin=False)
+
+        # Initialize the data dictionary to store map data projected from pointing sets
         self.data_dict: dict[str, NDArray] = {}
-        self.nested = nested
 
     def __repr__(self) -> str:
         """
