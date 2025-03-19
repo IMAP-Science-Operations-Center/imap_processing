0fc01a29688c04a4fe3c6c9ddf65348dba8609fa
Tim Plummer <Timothy.Plummer@lasp.colorado.edu>
2025-03-19 16:08:52 -0600

    Update pointing table to include subsecond fields

--- imap_processing/tests/conftest.py
+++ imap_processing/tests/conftest.py
@@ -590,8 +590,12 @@ def generate_repoint_data(
         repoint_end_met = repoint_start_times + 15 * 60
     repoint_df = pd.DataFrame.from_dict(
         {
-            "repoint_start_time": repoint_start_times,
-            "repoint_end_time": np.array(repoint_end_met),
+            "repoint_start_sec": repoint_start_times.astype(int),
+            "repoint_start_subsec": ((repoint_start_times % 1.0) * 1e3).astype(int),
+            "repoint_start_met": repoint_start_times,
+            "repoint_end_sec": repoint_end_met.astype(int),
+            "repoint_end_subsec": ((repoint_end_met % 1.0) * 1e3).astype(int),
+            "repoint_end_met": np.array(repoint_end_met),
             "repoint_id": np.arange(repoint_start_times.size, dtype=int)
             + repoint_id_start,
         }
