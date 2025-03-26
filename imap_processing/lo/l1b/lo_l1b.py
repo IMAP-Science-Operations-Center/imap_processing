565b6ee2e3693c57431235e48c45129f650913aa
Sean Hoyt <sean.hoyt@lasp.colorado.edu>
2025-03-26 13:05:55 -0600

    docstring edit

--- imap_processing/lo/l1b/lo_l1b.py
+++ imap_processing/lo/l1b/lo_l1b.py
@@ -215,12 +215,13 @@ def set_spin_bin(l1b_de: xr.Dataset, spin_angle: np.ndarray) -> xr.Dataset:
 
 def set_spin_cycle(l1a_de: xr.Dataset, l1b_de: xr.Dataset) -> xr.Dataset:
     """
-    Set the spin cycle for each Direct Event.
+    Set the spin cycle for each direct event.
 
     spin_cycle = spin_start + 7 + (esa_step - 1) * 2
+
     where spin start is the spin number for the first spin
-    in an Aggregated Science Cycle (first spin number of an epoch)
-    and esa_step is the esa_step for a direct event
+    in an Aggregated Science Cycle (first spin number of an epoch
+    in the L1A spin dataset) and esa_step is the esa_step for a direct event
 
     Parameters
     ----------
