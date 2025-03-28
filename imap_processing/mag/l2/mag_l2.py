e8be833f65f48dfc7c697ab84ce9ad1c8600d6de
Maxine Hartnett <maxine.hartnett@lasp.colorado.edu>
2025-03-28 08:07:52 -0600

    First pass at calibration

--- imap_processing/mag/l2/mag_l2.py
+++ imap_processing/mag/l2/mag_l2.py
@@ -5,6 +5,7 @@ import xarray as xr
 
 from imap_processing.cdf.imap_cdf_manager import ImapCdfAttributes
 from imap_processing.mag.constants import DataMode
+from imap_processing.mag.l1b.mag_l1b import calibrate_vector
 from imap_processing.mag.l2.mag_l2_data import MagL2
 
 
@@ -19,6 +20,10 @@ def mag_l2(
 
     Input data can be burst or normal mode, but MUST match the file in offset_dataset.
     TODO: retrieve the file from offset_dataset in cli.py.
+    Offset dataset will be split by norm/burst but will include all other data.
+    Calibration dataset is the same for all runs.
+
+    MAGi data is not used unless we indicate it.
 
     Parameters
     ----------
@@ -39,8 +44,15 @@ def mag_l2(
     """
     # TODO we may need to combine multiple calibration datasets into one timeline.
 
+    # TODO set from offsets file
+    always_output_mago = True
+
+    vectors = apply_calibration_matrix(
+        input_data["vectors"].data, calibration_dataset, always_output_mago
+    )
+
     basic_test_data = MagL2(
-        input_data["vectors"].data[:, :3],  # level 2 vectors don't include range
+        vectors[:, :3],  # level 2 vectors don't include range
         input_data["epoch"].data,
         input_data["vectors"].data[:, 3],
         {"Data_version": data_version},
@@ -52,26 +64,39 @@ def mag_l2(
     attributes.add_instrument_global_attrs("mag")
     # temporarily point to l1c
     attributes.add_instrument_variable_attrs("mag", "l1c")
-
     return [basic_test_data.generate_dataset(attributes)]
 
 
 def apply_calibration_matrix(
-    calibration_dataset: xr.Dataset, vectors: np.ndarray
+    vectors: np.ndarray, calibration_dataset: xr.Dataset, use_mago: bool = True
 ) -> np.ndarray:
     """
     Apply the calibration file to the vectors to rotate them in space.
 
     Parameters
     ----------
-    calibration_dataset : xr.Dataset
-        Ancillary file input for calibration.
     vectors : np.ndarray
         (n, 4) array of vectors to rotate and timeshift.
+    calibration_dataset : xr.Dataset
+        Ancillary file input for calibration.
+    use_mago : bool
+        Use the MAGo calibration matrix. Default is True.
 
     Returns
     -------
     np.ndarray
         Rotated and timeshifted vectors.
     """
-    raise NotImplementedError
+    if use_mago:
+        # TODO these variable names will change but the structure is the same
+        calibration_data = calibration_dataset["MFOTOURFO"]
+    else:
+        calibration_data = calibration_dataset["MFITOURFI"]
+
+    # TODO will need to combine multiple files here
+    # TODO: Check validity of the calibration file?
+    output_vectors: np.ndarray = np.apply_along_axis(
+        calibrate_vector, 1, vectors, calibration_data
+    )
+
+    return output_vectors
--- imap_processing/tests/mag/test_mag_l2.py
+++ imap_processing/tests/mag/test_mag_l2.py
@@ -1,11 +1,60 @@
+from pathlib import Path
+
+import numpy as np
+import pytest
+import xarray as xr
+
+from imap_processing.cdf.utils import load_cdf
+from imap_processing.mag.l2.mag_l2 import apply_calibration_matrix, mag_l2
+from imap_processing.tests.mag.conftest import mag_l1a_dataset_generator
+
+
+@pytest.fixture()
+def norm_dataset():
+    dataset = mag_l1a_dataset_generator(20)
+    epoch_vals = np.arange(0, 10, 0.5) * 1e9
+    vectors_per_second_attr = "0:2,4000000000:4"
+    dataset.attrs["vectors_per_second"] = vectors_per_second_attr
+    dataset["epoch"] = epoch_vals
+    dataset.attrs["Logical_source"] = "imap_mag_l1c_norm-mago"
+    vectors = np.array([[i, i, i, 2] for i in range(1, 21)])
+    dataset["vectors"].data = vectors
+
+    return dataset
+
+
+def test_mag_l2(norm_dataset):
+    calibration_dataset = load_cdf(
+        Path(__file__).parent
+        / "validation"
+        / "calibration"
+        / "imap_mag_l1b-calibration_20240229_v001.cdf"
+    )
+    offset_dataset = xr.Dataset()
+    l2 = mag_l2(calibration_dataset, offset_dataset, norm_dataset, "v001")
+    assert "vectors" in l2[0].data_vars
+
+
 def test_failure_on_mismatch_files():
     # input_offsets =
     pass
 
 
-def test_calibration_application():
+def test_apply_calibration(norm_dataset, mag_test_l1b_calibration_data):
     # Test matrix application
-    pass
+    output = apply_calibration_matrix(
+        np.array([[1, 1, 1, 0]], dtype=np.float64), mag_test_l1b_calibration_data, True
+    )
+
+    expected_vector = np.array([2.2972, 2.2415, 2.2381, 0])
+
+    assert np.allclose(output, expected_vector, atol=1e-9)
+
+    vectors = np.array([[1, 1, 1, 0] for i in range(1, 21)], dtype=np.float64)
+    expected_output = np.array([[2.2972, 2.2415, 2.2381, 0] for i in range(1, 21)])
+    output = apply_calibration_matrix(vectors, mag_test_l1b_calibration_data, True)
+
+    assert np.allclose(output, expected_output, atol=1e-9)
 
 
 def test_offset_application():
