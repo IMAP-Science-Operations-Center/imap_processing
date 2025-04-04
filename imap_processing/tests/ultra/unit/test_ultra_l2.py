1ec0d7112370e973ef8f8b20d13100ecc48a15f6
Nat Kerman <nkerman@users.noreply.github.com>
2025-04-04 16:22:16 -0600

    Initial tests for Ultra L2

---
+++ imap_processing/tests/ultra/unit/test_ultra_l2.py
@@ -0,0 +1,278 @@
+from pathlib import Path
+from unittest.mock import patch
+
+import numpy as np
+import pytest
+from spiceypy import furnsh
+
+from imap_processing.cdf.utils import write_cdf
+from imap_processing.ena_maps import ena_maps
+from imap_processing.ena_maps.utils.coordinates import CoordNames
+from imap_processing.ena_maps.utils.map_properties import (
+    MapProperties,
+)
+from imap_processing.tests.ultra.data.mock_data import mock_l1c_pset_product_healpix
+from imap_processing.ultra.l2 import ultra_l2
+
+
+class TestUltraL2:
+    @pytest.fixture()
+    def _setup_spice_kernels(self, spice_test_data_path):
+        required_kernel_names = [
+            "imap_science_0001.tf",
+            "imap_sclk_0000.tsc",
+            "sim_1yr_imap_attitude.bc",
+            "sim_1yr_imap_pointing_frame.bc",
+        ]
+        self.required_kernels = [
+            str(spice_test_data_path / kernel) for kernel in required_kernel_names
+        ]
+        furnsh(self.required_kernels)
+
+    @pytest.fixture()
+    def _mock_single_pset(self, _setup_spice_kernels):
+        self.ultra_pset = mock_l1c_pset_product_healpix(
+            nside=128, stripe_center_lat=0, timestr="2025-05-15T12:00:00"
+        )
+
+    @pytest.fixture()
+    def _mock_multiple_psets(self, _setup_spice_kernels):
+        self.ultra_psets = [
+            mock_l1c_pset_product_healpix(
+                nside=128,
+                stripe_center_lat=mid_latitude,
+                width_scale=5,
+                counts_scaling_params=(50, 0.5),
+                peak_exposure=1000,
+                timestr=f"2025-05-{4 * i + 1:02d}T12:00:00",
+                head=("90"),
+            )
+            for i, mid_latitude in enumerate(
+                np.arange(
+                    22.5,
+                    180,
+                    45,
+                )
+            )
+        ]
+
+        self.psets_total_counts = np.sum(
+            [pset["counts"].values.sum() for pset in self.ultra_psets]
+        )
+
+    @pytest.fixture()
+    def mock_data_dict(self, _mock_multiple_psets):
+        return {pset.attrs["Logical_file_id"]: pset for pset in self.ultra_psets}
+
+    @pytest.mark.usefixtures("_mock_single_pset")
+    def test_read_into_pointing_set(
+        self,
+    ):
+        cdf_filepath = write_cdf(self.ultra_pset, istp=False)
+
+        ultra_pset_from_dataset = ultra_l2.read_into_pointing_set(self.ultra_pset)
+        ultra_pset_from_dataset_copy = ultra_l2.read_into_pointing_set(
+            self.ultra_pset, inplace=False
+        )
+
+        ultra_pset_from_str = ultra_l2.read_into_pointing_set(cdf_filepath)
+        ultra_pset_from_path = ultra_l2.read_into_pointing_set(Path(cdf_filepath))
+
+        np.testing.assert_allclose(
+            ultra_pset_from_dataset.data["counts"].values,
+            ultra_pset_from_str.data["counts"].values,
+            rtol=1e-6,
+        )
+
+        np.testing.assert_allclose(
+            ultra_pset_from_dataset.data["counts"].values,
+            ultra_pset_from_path.data["counts"].values,
+            rtol=1e-6,
+        )
+
+        # delete cdf_filepath once we're done with it
+        Path(cdf_filepath).unlink()
+
+        # The two datasets should should start as equal, but not the same object
+        # So if we modify one, the other should not change
+        np.testing.assert_allclose(
+            ultra_pset_from_dataset.data["counts"].values,
+            ultra_pset_from_dataset_copy.data["counts"].values,
+            rtol=1e-6,
+        )
+        ultra_pset_from_dataset.data["counts"].values[0] += int(1e8)
+        assert not np.allclose(
+            ultra_pset_from_dataset.data["counts"].values,
+            ultra_pset_from_dataset_copy.data["counts"].values,
+            rtol=1e-6,
+        )
+
+    @pytest.mark.parametrize(
+        ["map_frame", "rtol"],
+        [
+            # Tight tolerance when 'projecting' to the same frame
+            ("IMAP_DPS", 1e-8),
+            # Loose tolerance of  30% error vs naive flux estimate with real projection.
+            # TODO: Ideally this tolerance will tighten if we can fix the issue with
+            # the exposure time for uneven numbers of pixels from each PointingSet.
+            ("ECLIPJ2000", 3e-1),
+        ],
+    )
+    @pytest.mark.usefixtures("_mock_single_pset", "_setup_spice_kernels")
+    def test_generate_ultra_healpix_skymap_single_pset(self, map_frame, rtol):
+        # Avoid modifying the original pset
+        pset = self.ultra_pset.copy(deep=True)
+
+        # Set the values in the single input PSET for easy calculation
+        # of the expected flux and flux uncertainty
+        pset["counts"].values = np.full_like(pset["counts"].values, 10)
+        pset["exposure_factor"].values = np.ones_like(pset["exposure_factor"].values)
+        pset["background_rates"].values = np.ones_like(pset["background_rates"].values)
+        pset["sensitivity"].values = np.ones_like(pset["sensitivity"].values)
+        pset["energy_bin_delta"].values = np.ones_like(pset["energy_bin_delta"].values)
+
+        # Create the Healpix skymap in the desired frame.
+        hp_skymap = ultra_l2.generate_ultra_healpix_skymap(
+            ultra_l1c_psets=[
+                pset,
+            ],
+            output_map_properties=MapProperties(
+                {
+                    "sky_tiling_type": "HEALPIX",
+                    "spice_reference_frame": map_frame,
+                    "projection_method_and_values": {
+                        "PUSH": [
+                            "counts",
+                            "exposure_factor",
+                            "sensitivity",
+                            "background_rates",
+                        ],
+                    },
+                    "nside": 32,
+                    "nested": False,
+                }
+            ),
+        )
+
+        assert hp_skymap.nside == 32
+        assert hp_skymap.nested is False
+
+        # Check that required variables are present, and dropped variables are not
+        expected_vars = [
+            "flux",
+            "flux_uncertainty",
+            "exposure_factor",
+            "observation_time",
+        ]
+        for var in expected_vars:
+            assert var in hp_skymap.data_1d.data_vars
+        unexpected_vars = ultra_l2.VARIABLES_TO_DROP_AFTER_FLUX_CALCULATION
+        for var in unexpected_vars:
+            assert var not in hp_skymap.data_1d.data_vars
+
+        # The ratio of the solid angle of a map's pixel to the solid angle of a
+        # pointing set's pixel. Counts will scale up by this ratio.
+        solid_angle_ratio_map_to_pset = (
+            hp_skymap.solid_angle / ena_maps.UltraPointingSet(pset).solid_angle
+        )
+
+        # Estimate the expected flux and flux uncertainty
+        expected_flux = (
+            (10 * solid_angle_ratio_map_to_pset / 1) - 1 * solid_angle_ratio_map_to_pset
+        ) / (1 * hp_skymap.solid_angle * 1)
+        expected_flux_unc = ((10 * solid_angle_ratio_map_to_pset) ** 0.5 / 1) / (
+            1 * hp_skymap.solid_angle * 1
+        )
+
+        np.testing.assert_allclose(
+            hp_skymap.data_1d["flux"].values,
+            expected_flux,
+            rtol=rtol,
+        )
+        np.testing.assert_allclose(
+            hp_skymap.data_1d["flux_uncertainty"].values,
+            expected_flux_unc,
+            rtol=rtol,
+        )
+
+    @pytest.mark.usefixtures("_mock_multiple_psets", "_setup_spice_kernels")
+    def test_generate_ultra_healpix_skymap_multiple_psets(
+        self,
+    ):
+        with patch(
+            "imap_processing.ultra.l2.ultra_l2.VARIABLES_TO_DROP_AFTER_FLUX_CALCULATION",
+            [],
+        ):
+            hp_skymap = ultra_l2.generate_ultra_healpix_skymap(
+                ultra_l1c_psets=self.ultra_psets,
+                output_map_properties=MapProperties(
+                    {
+                        "sky_tiling_type": "RECTANGULAR",
+                        "spice_reference_frame": "ECLIPJ2000",
+                        "projection_method_and_values": {
+                            "PUSH": [
+                                "counts",
+                                "exposure_factor",
+                                "sensitivity",
+                                "background_rates",
+                            ],
+                        },
+                        "spacing_deg": 2.0,
+                    }
+                ),
+            )
+
+        assert hp_skymap.nside == ultra_l2.DEFAULT_L2_HEALPIX_NSIDE
+        assert hp_skymap.nested == ultra_l2.DEFAULT_L2_HEALPIX_NESTED
+
+        # The total counts in the skymap should be equal to the sum of the counts
+        # in the individual psets
+        np.testing.assert_allclose(
+            hp_skymap.data_1d["counts"].sum(),
+            self.psets_total_counts,
+        )
+
+        # The map should contain the following variables,
+        # because we did not drop any variables
+        expected_vars = (
+            ultra_l2.REQUIRED_L1C_VARIABLES
+            + ultra_l2.VARIABLES_TO_DROP_AFTER_FLUX_CALCULATION
+            + ["flux", "flux_uncertainty"]
+        )
+        for var in expected_vars:
+            assert var in hp_skymap.data_1d.data_vars
+
+        # Check the dims of the key variables
+        counts_dims = (
+            CoordNames.TIME.value,
+            CoordNames.ENERGY.value,
+            CoordNames.GENERIC_PIXEL.value,
+        )
+        assert hp_skymap.data_1d["counts"].dims == counts_dims
+        assert hp_skymap.data_1d["flux"].dims == counts_dims
+        assert hp_skymap.data_1d["flux_uncertainty"].dims == counts_dims
+        assert hp_skymap.data_1d["exposure_factor"].dims == counts_dims[-1:]
+
+    @pytest.mark.usefixtures("_setup_spice_kernels")
+    def test_ultra_l2_output_unbinned_healpix(self, mock_data_dict):
+        props = MapProperties(
+            {
+                "sky_tiling_type": "HEALPIX",
+                "spice_reference_frame": "ECLIPJ2000",
+                "projection_method_and_values": {
+                    "PUSH": ["counts", "exposure_factor", "sensitivity"],
+                },
+                "nside": 16,
+                "nested": True,
+            }
+        )
+        [
+            map_dataset,
+        ] = ultra_l2.ultra_l2(
+            data_dict=mock_data_dict,
+            data_version="001",
+            output_map_properties=props,
+        )
+
+        assert map_dataset.attrs["HEALPix_nside"] == props.nside
+        assert map_dataset.attrs["HEALPix_nest"] == props.nested
