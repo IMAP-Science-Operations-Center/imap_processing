0c74ad51b9e1294347d8f827222d0dab70e96d20
Jonathan Niehof <Jonathan.Niehof@unh.edu>
2025-04-22 14:33:39 -0400

    swe_l2: make ruff happy

--- imap_processing/swe/l2/swe_l2.py
+++ imap_processing/swe/l2/swe_l2.py
@@ -101,7 +101,7 @@ def calculate_phase_space_density(l1b_dataset: xr.Dataset) -> xr.Dataset:
     particle_energy_data = particle_energy_data.reshape(
         -1, swe_constants.N_ESA_STEPS, swe_constants.N_ANGLE_SECTORS
     )
-    #assert not np.diff(particle_energy_data, axis=-1).any()
+    # assert not np.diff(particle_energy_data, axis=-1).any()
 
     # Calculate phase space density using formula:
     #   2 * (C/tau) / (G * 1.237e31 * eV^2)
