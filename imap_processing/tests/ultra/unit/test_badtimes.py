684cfe9965bb21b092ba66a2cdd4a9d021ade5d6
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2025-04-22 13:34:35 -0600

    fix tests

--- imap_processing/tests/ultra/unit/test_badtimes.py
+++ imap_processing/tests/ultra/unit/test_badtimes.py
@@ -32,6 +32,7 @@ def test_calculate_badtimes():
 
     ds = xr.Dataset(
         {
+            "epoch": np.array([0, 1, 2, 3], dtype="datetime64[ns]"),
             "quality_attitude": (("spin_number",), quality_attitude),
             "quality_ena_rates": (
                 ("energy_bin_geometric_mean", "spin_number"),
