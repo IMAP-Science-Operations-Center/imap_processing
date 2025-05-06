aadc196e317ddf93e3d8e2b31cf9f798d130aa67
Matthew Bourque <matthew.bourque@lasp.colorado.edu>
2025-05-06 15:11:42 -0600

    Now using data product config dict like in L1a; added more logic to process more datasets

--- imap_processing/codice/codice_l1b.py
+++ imap_processing/codice/codice_l1b.py
@@ -9,9 +9,16 @@ from imap_processing.codice.codice_l1b import process_codice_l1b
 dataset = process_codice_l1b(l1a_filenanme)
 """
 
+# TODO: Some things to figure out with Joey:
+#       - Do any of the support variables need to be converted to rates?
+#       - How to convert hi-priority data product?
+#       - Do hi-omni and hi-sectored energy bins need to be converted?
+
 import logging
+import sys
 from pathlib import Path
 
+import numpy as np
 import xarray as xr
 
 from imap_processing.cdf.imap_cdf_manager import ImapCdfAttributes
@@ -20,6 +27,65 @@ from imap_processing.codice import constants
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
+
+def convert_to_rates(
+    dataset: xr.Dataset, descriptor: str, variable_name: str
+) -> np.ndarray:
+    """
+    Apply a conversion from counts to rates.
+
+    The formula for conversion from counts to rates is specific to each data
+    product, but is largely grouped by CoDICE-Lo and CoDICE-Hi products.
+
+    Parameters
+    ----------
+    dataset : xr.Dataset
+        The L1b dataset containing the data to convert.
+    descriptor : str
+        The descriptor of the data product of interest.
+    variable_name : str
+        The variable name to apply the conversion to.
+
+    Returns
+    -------
+    rates_data : np.ndarray
+        The converted data array.
+    """
+    acq_times = dataset.epoch.data
+    acq_times = 1
+
+    if descriptor in [
+        "lo-counters-aggregated",
+        "lo-counters-singles",
+        "lo-nsw-angular",
+        "lo-sw-angular",
+        "lo-nsw-priority",
+        "lo-sw-priority",
+        "lo-nsw-species",
+        "lo-sw-species",
+    ]:
+        rates_data = dataset[variable_name].data / (
+            acq_times
+            * 10e-6
+            * constants.L1B_DATA_PRODUCT_CONFIGURATIONS[descriptor]["num_spin_sectors"]
+        )
+    elif descriptor in [
+        "hi-counters-aggregated",
+        "hi-counters-singles",
+        "hi-omni",
+        "hi-priority",
+        "hi-sectored",
+    ]:
+        rates_data = dataset[variable_name].data / (
+            constants.L1B_DATA_PRODUCT_CONFIGURATIONS[descriptor]["num_spin_sectors"]
+            * constants.L1B_DATA_PRODUCT_CONFIGURATIONS[descriptor]["num_spins"]
+            * acq_times
+        )
+    elif descriptor == "hskp":
+        rates_data = dataset[variable_name].data / acq_times
+
+    return rates_data
 
 
 def process_codice_l1b(file_path: Path) -> xr.Dataset:
@@ -47,6 +113,11 @@ def process_codice_l1b(file_path: Path) -> xr.Dataset:
     descriptor = dataset_name.removeprefix("imap_codice_l1b_")
     apid = constants.CODICEAPID_MAPPING[descriptor]
 
+    # Direct event data products do not have a level L1B
+    if descriptor in ["lo-pha", "hi-pha"]:
+        logger.warning("Encountered direct event data product. Skipping processing")
+        sys.exit()
+
     # Get the L1b CDF attributes
     cdf_attrs = ImapCdfAttributes()
     cdf_attrs.add_instrument_global_attrs("codice")
@@ -63,7 +134,12 @@ def process_codice_l1b(file_path: Path) -> xr.Dataset:
     if descriptor == "hskp":
         data_variables = []
         support_variables = ["cmdexe", "cmdrjct"]
-        variables_to_convert = support_variables
+    elif descriptor == "hi-sectored":
+        data_variables = ["h", "he3he4", "cno", "fe"]
+        support_variables = []
+    elif descriptor == "hi-omni":
+        data_variables = ["h", "he3", "he4", "c", "o", "ne_mg_si", "fe", "uh"]
+        support_variables = []
     else:
         data_variables = getattr(
             constants, f"{descriptor.upper().replace('-', '_')}_VARIABLE_NAMES"
@@ -71,13 +147,12 @@ def process_codice_l1b(file_path: Path) -> xr.Dataset:
         support_variables = constants.DATA_PRODUCT_CONFIGURATIONS[apid][
             "support_variables"
         ]
-        variables_to_convert = data_variables + support_variables
+    variables_to_convert = data_variables + support_variables
 
     for variable_name in variables_to_convert:
-        # Apply conversion of data from counts to rates
-        # TODO: Properly implement conversion factors on a per-data-product basis
-        #       For now, just divide by 100 to get float values
-        l1b_dataset[variable_name].data = l1b_dataset[variable_name].data / 100
+        l1b_dataset[variable_name].data = convert_to_rates(
+            l1b_dataset, descriptor, variable_name
+        )
 
         # Set the variable attributes
         if variable_name in data_variables:
--- imap_processing/codice/constants.py
+++ imap_processing/codice/constants.py
@@ -406,6 +406,56 @@ DATA_PRODUCT_CONFIGURATIONS: dict[CODICEAPID | int, dict] = {
     },
 }
 
+# Various configurations to support L1b processing of individual data products
+# Much of these are described in the algorithm document in chapter 11 ("Data
+# Level 1B")
+L1B_DATA_PRODUCT_CONFIGURATIONS: dict[str, dict] = {
+    "hi-counters-aggregated": {
+        "num_spin_sectors": 24,
+        "num_spins": 16,
+    },
+    "hi-counters-singles": {
+        "num_spin_sectors": 24,
+        "num_spins": 16,
+    },
+    "hi-omni": {
+        "num_spin_sectors": 24,
+        "num_spins": 4,
+    },
+    "hi-priority": {  # TODO: Ask Joey to define these
+        "num_spin_sectors": 1,
+        "num_spins": 1,
+    },
+    "hi-sectored": {
+        "num_spin_sectors": 2,
+        "num_spins": 16,
+    },
+    "lo-counters-aggregated": {
+        "num_spin_sectors": 2,
+    },
+    "lo-counters-singles": {
+        "num_spin_sectors": 2,
+    },
+    "lo-nsw-angular": {
+        "num_spin_sectors": 1,
+    },
+    "lo-sw-angular": {
+        "num_spin_sectors": 1,
+    },
+    "lo-nsw-priority": {
+        "num_spin_sectors": 1,
+    },
+    "lo-sw-priority": {
+        "num_spin_sectors": 1,
+    },
+    "lo-nsw-species": {
+        "num_spin_sectors": 12,
+    },
+    "lo-sw-species": {
+        "num_spin_sectors": 12,
+    },
+}
+
 # Dictionary to define the bit structure of CoDICE-Lo direct events
 LO_DE_BIT_STRUCTURE = {
     "APDGain": 1,
