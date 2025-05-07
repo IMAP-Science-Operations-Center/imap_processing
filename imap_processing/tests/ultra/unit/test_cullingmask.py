c525356c82c76b71c9b9b144efdd1cc8513c43a5
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2025-05-07 15:09:36 -0600

    cullingmask update

--- imap_processing/tests/ultra/unit/test_cullingmask.py
+++ imap_processing/tests/ultra/unit/test_cullingmask.py
@@ -87,3 +87,50 @@ def test_calculate_cullingmask_rates():
 
     expected_spins = np.array([0, 1])
     np.testing.assert_array_equal(result_ds["spin_number"].values, expected_spins)
+
+
+def test_calculate_cullingmask_empty():
+    """Test calculate_cullingmask when all spins are culled (empty case)."""
+
+    spin_numbers = np.array([0, 1, 2])
+    energy_bins = np.array([10, 20, 30])
+    spin_start_time = np.array([0, 1, 2])
+
+    quality_attitude = np.full(
+        spin_numbers.shape, ImapAttitudeUltraFlags.NONE.value, dtype=np.uint16
+    )
+
+    quality_ena_rates = np.full(
+        (len(energy_bins), len(spin_numbers)),
+        ImapRatesUltraFlags.ZEROCOUNTS.value | ImapRatesUltraFlags.HIGHRATES.value,
+        dtype=np.uint16,
+    )
+
+    ds = xr.Dataset(
+        {
+            "epoch": np.array([0, 1, 2], dtype="datetime64[ns]"),
+            "quality_attitude": (("spin_number",), quality_attitude),
+            "quality_ena_rates": (
+                ("energy_bin_geometric_mean", "spin_number"),
+                quality_ena_rates,
+            ),
+            "spin_start_time": (("spin_number",), spin_start_time),
+        },
+        coords={
+            "spin_number": spin_numbers,
+            "energy_bin_geometric_mean": energy_bins,
+        },
+    )
+
+    cullingmask_ds = calculate_cullingmask(
+        ds,
+        name="imap_ultra_l1b_45sensor-cullingmask",
+    )
+
+    assert cullingmask_ds["spin_number"].values[0] == 4294967295
+    assert cullingmask_ds["spin_start_time"].values[0] == -1.0e31
+    assert cullingmask_ds["spin_period"].values[0] == -1.0e31
+    assert cullingmask_ds["spin_rate"].values[0] == -1.0e31
+    assert cullingmask_ds["quality_attitude"].values[0] == 65535
+    assert np.all(cullingmask_ds["ena_rates"].values == -1.0e31)
+    assert np.all(cullingmask_ds["quality_ena_rates"].values == 65535)
