9ee23929018bde29659652b3cd4484fb317fafcb
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2025-05-09 15:20:01 -0600

    works but terrible

--- imap_processing/ialirt/l0/ialirt_spice.py
+++ imap_processing/ialirt/l0/ialirt_spice.py
@@ -101,6 +101,9 @@ def compute_sc_to_inertial_rotation_matrix_from_z(
         # Columns = spacecraft axes in inertial frame:
         # SPICE: Zsc → X, Ysc → Y, Xsc → Z
         R = np.stack([x_rot, y_rot, z], axis=1)
+        # Orthonormalize
+        u, _, vh = np.linalg.svd(R)
+        R = u @ vh
         R_all.append(R)
 
     return np.stack(R_all)
@@ -132,10 +135,11 @@ def transform_instrument_vectors_to_inertial(
     # SC → inertial
     R_sc_to_inertial = spice.pxform("IMAP_SPACECRAFT", "ECLIPJ2000", et[0])
 
-    R_sc_to_inertial = compute_sc_to_inertial_rotation_matrix_from_z(
+    R_sc_to_inertial_test = compute_sc_to_inertial_rotation_matrix_from_z(
         z_axis,
         spin_phase,
     )
+    R_sc_to_inertial = R_sc_to_inertial_test[0]
 
     # Final transform matrix: (N, 3, 3)
     rot_total = np.array([
