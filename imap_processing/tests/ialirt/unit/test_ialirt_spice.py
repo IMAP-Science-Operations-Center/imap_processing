0dc0687f95ca76db03e2fb7fb34067bb85a285b7
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2025-05-19 18:24:33 -0600

    pr response

--- imap_processing/tests/ialirt/unit/test_ialirt_spice.py
+++ imap_processing/tests/ialirt/unit/test_ialirt_spice.py
@@ -166,13 +166,12 @@ def test_transform_instrument_vectors_to_inertial(
 
     # Pick midpoint of first coverage interval
     et_start = ck_cover[0]
-    et = et_start + 10
 
     # Assume IMAP_MAG +X is boresight
     instrument_vector = np.array([[10.0, 2.0, 3.0]])
 
     # Get RA/Dec of angular momentum vector (Z-axis) from SPICE
-    rot_sc_to_j2000 = spiceypy.pxform("IMAP_SPACECRAFT", "ECLIPJ2000", et)
+    rot_sc_to_j2000 = spiceypy.pxform("IMAP_SPACECRAFT", "ECLIPJ2000", et_start + 10)
     sc_z_inertial = rot_sc_to_j2000[:, 2]  # SC +Z axis (angular momentum)
     # Convert inertial Z into RA/Dec (radians)
     _, ra, dec = spiceypy.recrad(sc_z_inertial.copy())
@@ -188,21 +187,33 @@ def test_transform_instrument_vectors_to_inertial(
         atol=1e-9,
     )
 
-    spin_phase = np.array([120.0])
-
-    v_manual = transform_instrument_vectors_to_inertial(
+    v_manual_0 = transform_instrument_vectors_to_inertial(
         instrument_vector,
-        spin_phase,
+        np.array([120.0]),
+        np.array([np.degrees(ra)]),
+        np.array([np.degrees(dec)]),
+    )
+    v_manual_1 = transform_instrument_vectors_to_inertial(
+        instrument_vector,
+        np.array([240.0]),
         np.array([np.degrees(ra)]),
         np.array([np.degrees(dec)]),
     )
 
-    rot_inst_to_inertial = spiceypy.pxform("IMAP_MAG", "ECLIPJ2000", et)
-    v_spice = spiceypy.mxv(rot_inst_to_inertial, instrument_vector[0])
+    rot_inst_to_inertial_0 = spiceypy.pxform("IMAP_MAG", "ECLIPJ2000", et_start + 10)
+    rot_inst_to_inertial_1 = spiceypy.pxform("IMAP_MAG", "ECLIPJ2000", et_start + 20)
+
+    v_spice_0 = spiceypy.mxv(rot_inst_to_inertial_0, instrument_vector[0])
+    v_spice_1 = spiceypy.mxv(rot_inst_to_inertial_1, instrument_vector[0])
 
     np.testing.assert_allclose(
-        v_manual[0],
-        v_spice,
+        v_manual_0[0],
+        v_spice_0,
+        atol=1e-9,
+    )
+    np.testing.assert_allclose(
+        v_manual_1[0],
+        v_spice_1,
         atol=1e-9,
     )
 
