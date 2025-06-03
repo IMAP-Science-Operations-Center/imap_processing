5ad80a8689fd317dd28a0412643268f0e692059b
Matthew Bourque <matthew.bourque@lasp.colorado.edu>
2025-06-03 13:55:20 -0600

    Debugging Actions build errors

--- imap_processing/tests/codice/conftest.py
+++ imap_processing/tests/codice/conftest.py
@@ -2,7 +2,9 @@ from imap_processing import imap_module_directory
 
 TEST_L0_DATA_PATH = imap_module_directory / "tests" / "codice" / "data"
 TEST_L0_FILE = TEST_L0_DATA_PATH / "imap_codice_l0_raw_20241110_v001.pkts"
-TEST_L1A_DATA_PATH = imap_module_directory / "imap" / "codice" / "l1a" / "2024" / "11"
+TEST_L1A_DATA_PATH = (
+    imap_module_directory / "tests" / "imap" / "codice" / "l1a" / "2024" / "11"
+)
 
 
 TEST_L1A_FILES = [
