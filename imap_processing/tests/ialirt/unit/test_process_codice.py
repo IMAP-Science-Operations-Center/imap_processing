6c10cc810486594f0708eb11a1cb06795b0e453c
Matthew Bourque <matthew.bourque@lasp.colorado.edu>
2025-06-17 12:26:30 -0600

    Changes to I-ALiRT processing in prepration to I-ALiRT SIT

--- imap_processing/tests/ialirt/unit/test_process_codice.py
+++ imap_processing/tests/ialirt/unit/test_process_codice.py
@@ -6,11 +6,10 @@ See tests.codice.test_codice_l1a for more unit tests related to this code.
 from pathlib import Path
 
 import pytest
-import xarray as xr
 
 from imap_processing import imap_module_directory
-from imap_processing.codice.codice_l0 import decom_packets
 from imap_processing.ialirt.l0.process_codice import process_codice
+from imap_processing.utils import packet_file_to_datasets
 
 pytestmark = pytest.mark.external_test_data
 
@@ -20,26 +19,30 @@ def l0_test_file():
     return Path(
         imap_module_directory
         / "tests"
-        / "codice"
+        / "ialirt"
         / "data"
-        / "imap_codice_l0_raw_20241110_v001.pkts"
+        / "l0"
+        / "iois_1_packets_2025_181_15_42_30.bin"
     )
 
 
 @pytest.fixture(scope="session")
 def test_datasets(l0_test_file):
-    return decom_packets(l0_test_file)
+    xtce_packet_definition = Path(
+        imap_module_directory / "ialirt" / "packet_definitions" / "ialirt.xml"
+    )
+
+    datasets = packet_file_to_datasets(l0_test_file, xtce_packet_definition)
+
+    return datasets
 
 
 @pytest.fixture
-def codice_test_data(apid, test_datasets):
-    return test_datasets[apid]
+def codice_test_data(test_datasets):
+    return test_datasets[478]
 
 
-@pytest.mark.parametrize(
-    "apid", [1152, 1168]
-)  # APIDs correspond to COD_LO_IAL and CO_HI_IAL
-def test_process_codice(apid, codice_test_data, caplog):
+def test_process_codice(codice_test_data, caplog):
     """Ensure that the ``process_codice`` function creates a dataset
 
     Here we just need to make sure the function is returning the expected data.
@@ -48,6 +51,9 @@ def test_process_codice(apid, codice_test_data, caplog):
     """
 
     with caplog.at_level("WARNING"):
-        dataset = process_codice(codice_test_data)
+        cod_lo_data, cod_hi_data = process_codice(codice_test_data)
 
-    assert isinstance(dataset, xr.Dataset)
+    assert isinstance(cod_lo_data, list)
+    assert all(isinstance(item, dict) for item in cod_lo_data)
+    assert isinstance(cod_hi_data, list)
+    assert all(isinstance(item, dict) for item in cod_hi_data)
