547c41e56467269ff1b09aa6a0dc3c80a8b499bd
Matthew Bourque <matthew.bourque@lasp.colorado.edu>
2025-06-23 09:57:29 -0600

    squash

--- imap_processing/ialirt/l0/process_codice.py
+++ imap_processing/ialirt/l0/process_codice.py
@@ -72,7 +72,7 @@ def process_codice(
         epoch_data = {
             "apid": int(dataset.pkt_apid[epoch].data),
             "met": met,
-            "utc": utc,
+            "met_to_utc": utc,
             "ttj2000ns": ttj2000ns,
         }
 
