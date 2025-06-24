6bf238b29fdeea53ea2b02316d0ceeb7ff91d9d8
Tim Plummer <Timothy.Plummer@lasp.colorado.edu>
2025-06-24 15:13:20 -0600

    Add reference to algorithm document in comment

--- imap_processing/hi/utils.py
+++ imap_processing/hi/utils.py
@@ -64,7 +64,8 @@ class HiConstants:
     TOF3_TICK_DUR = 0.5  # 0.5 ns
 
     # These values are stored in the TOF telemetry when the TOF timer
-    # does not have valid data.
+    # does not have valid data. See IMAP-Hi Algorithm Document Section
+    # 2.2.5 Annotated Direct Events
     TOF1_BAD_VALUES = (511,)
     TOF2_BAD_VALUES = (511,)
     TOF3_BAD_VALUES = (1023,)
