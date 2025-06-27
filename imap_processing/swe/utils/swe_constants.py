8602dd3fc3473fa062a92e158c742d5ab9c47b9c
Tenzin Choedon <tenzin.choedon@lasp.colorado.edu>
2025-06-27 10:10:43 -0600

    Fixing Constants from validation. Revisit this.

--- imap_processing/swe/utils/swe_constants.py
+++ imap_processing/swe/utils/swe_constants.py
@@ -12,7 +12,7 @@ N_QUARTER_CYCLE_STEPS = 180
 MICROSECONDS_IN_SECOND = 1e6
 
 # TODO: add these to instrument status summary
-ENERGY_CONVERSION_FACTOR = 4.75
+ENERGY_CONVERSION_FACTOR = 4.76
 # 7 CEMs geometric factors in cm^2 sr eV/eV units.
 GEOMETRIC_FACTORS = np.array(
     [
@@ -26,7 +26,7 @@ GEOMETRIC_FACTORS = np.array(
     ]
 )
 
-ELECTRON_MASS = 9.10938356e-31  # kg
+ELECTRON_MASS = 9.10938e-31  # kg
 
 # See doc string of calculate_phase_space_density() for more details.
 VELOCITY_CONVERSION_FACTOR = 1.237e31
