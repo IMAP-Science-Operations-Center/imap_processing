d00a32f0844d627da575611c1782c90dd38dd9bb
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2025-06-27 17:16:27 -0600

    fix tests

--- imap_processing/ultra/l1a/ultra_l1a.py
+++ imap_processing/ultra/l1a/ultra_l1a.py
@@ -8,7 +8,6 @@ import xarray as xr
 from imap_processing import imap_module_directory
 from imap_processing.cdf.imap_cdf_manager import ImapCdfAttributes
 from imap_processing.ultra.l0.decom_ultra import (
-    process_ultra_energy_events,
     process_ultra_energy_rates,
     process_ultra_events,
     process_ultra_rates,
@@ -29,7 +28,7 @@ from imap_processing.utils import packet_file_to_datasets
 logger = logging.getLogger(__name__)
 
 
-def ultra_l1a(packet_file: str, apid_input: Optional[int] = None) -> list[xr.Dataset]:
+def ultra_l1a(packet_file: str, apid_input: Optional[int] = None) -> list[xr.Dataset]:  # noqa: PLR0912
     """
     Will process ULTRA L0 data into L1A CDF files at output_filepath.
 
