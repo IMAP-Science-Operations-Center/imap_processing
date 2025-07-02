31a090c800bd289d5c7496780ace1fbcd8e8d555
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2025-07-02 08:50:25 -0600

    get rid of ruff error

--- imap_processing/ultra/l1a/ultra_l1a.py
+++ imap_processing/ultra/l1a/ultra_l1a.py
@@ -34,7 +34,9 @@ from imap_processing.utils import packet_file_to_datasets
 logger = logging.getLogger(__name__)
 
 
-def ultra_l1a(packet_file: str, apid_input: Optional[int] = None) -> list[xr.Dataset]:  # noqa: PLR0912
+def ultra_l1a(  # noqa: PLR0912
+    packet_file: str, apid_input: Optional[int] = None
+) -> list[xr.Dataset]:
     """
     Will process ULTRA L0 data into L1A CDF files at output_filepath.
 
