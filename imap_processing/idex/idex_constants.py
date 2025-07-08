bb6694c62283b1b28b8c59927281017e8aafde07
Luisa <luisa.coakley@lasp.colorado.edu>
2025-07-08 14:25:19 -0600

    remove healpix map constants

--- imap_processing/idex/idex_constants.py
+++ imap_processing/idex/idex_constants.py
@@ -82,10 +82,6 @@ SPICE_ARRAYS = [
     "spin_phase",
 ]
 
-# Default IDEX Healpix parameters
-# Used in IDEX l2c processing
-IDEX_HEALPIX_NSIDE = 10
-IDEX_HEALPIX_NESTED = False
 # Default IDEX Rectangular parameters
 # Used in IDEX l2c processing
 IDEX_SPACING_DEG = 6
