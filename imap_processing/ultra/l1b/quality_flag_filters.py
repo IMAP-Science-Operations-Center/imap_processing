42bd24ddad5671e35ceb39c89fdb4abd38261a75
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2025-07-11 16:53:51 -0600

    fix tests

--- imap_processing/ultra/l1b/quality_flag_filters.py
+++ imap_processing/ultra/l1b/quality_flag_filters.py
@@ -1,8 +1,18 @@
 """Contains list of QFs to use for filtering."""
 
-from imap_processing.quality_flags import ImapAttitudeUltraFlags, ImapRatesUltraFlags
+from imap_processing.quality_flags import (
+    FlagNameMixin,
+    ImapAttitudeUltraFlags,
+    ImapRatesUltraFlags,
+)
 
-QUALITY_FLAG_FILTERS = {
-    "quality_attitude": [ImapAttitudeUltraFlags.SPINRATE],
-    "quality_ena_rates": [ImapRatesUltraFlags.HIGHRATES],
+QUALITY_FLAG_FILTERS: dict[str, list[FlagNameMixin]] = {
+    "quality_attitude": [
+        ImapAttitudeUltraFlags.SPINRATE,
+        ImapAttitudeUltraFlags.AUXMISMATCH,
+    ],
+    "quality_ena_rates": [
+        ImapRatesUltraFlags.HIGHRATES,
+        ImapRatesUltraFlags.LASTSPIN,
+    ],
 }
