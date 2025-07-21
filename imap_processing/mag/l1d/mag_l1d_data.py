39fa9685acec29003c1aea752148ae3a8bdd4b9f
Maxine Hartnett <maxine.hartnett@lasp.colorado.edu>
2025-07-21 17:04:43 -0600

    precommit fix

--- imap_processing/mag/l1d/mag_l1d_data.py
+++ imap_processing/mag/l1d/mag_l1d_data.py
@@ -332,7 +332,7 @@ class MagL1d(MagL2L1dBase):  # type: ignore[misc]
             )
 
         # TODO: get the spin numbers which correspond to the epoch values for output
-        sc_spin_phase = spin.get_spacecraft_spin_phase(self.epoch)  # type: ignore[index]
+        sc_spin_phase = spin.get_spacecraft_spin_phase(self.epoch)  # type: ignore
         # mark vectors as nan where they are nan in sc_spin_phase
         vectors = self.vectors.copy().astype(np.float64)
 
