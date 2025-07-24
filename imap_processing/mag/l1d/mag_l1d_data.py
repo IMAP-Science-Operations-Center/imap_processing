Fix spin tests and calculation

imap_processing/mag/l1d/mag_l1d_data.py
@@ -410,11 +410,9 @@ class MagL1d(MagL2L1dBase):  # type: ignore[misc]
         # this is when the spin crosses zero
         spin_starts = np.where(np.diff(sc_spin_phase) < 0)[0] + 1
 
-        # if the value switches from nan to a number, that is also a spin start (for an
-        # invalid spin)
-        nan_to_number = (
-            np.where(np.isnan(sc_spin_phase[:-1]) & ~np.isnan(sc_spin_phase[1:]))[0] + 1
-        )
+        # if the value switches from nan to a number, or from a number to nan, that
+        # is also a spin start
+        nan_to_number = np.where(np.diff(np.isnan(sc_spin_phase)) != 0)[0] + 1
 
         # find the places spins start while skipping over invalid or missing data
         # (marked as nan by get_spacecraft_spin_phase)
@@ -425,23 +423,29 @@ class MagL1d(MagL2L1dBase):  # type: ignore[misc]
         spin_data = spin.get_spin_data()
         # Use the median spin period as the expected value
         expected_spin = np.median(spin_data["spin_period_sec"]) * 1e9
-        # find nan gaps that contain more than one spin
-        for nan_gap_index in range(len(nan_to_number)):
-            existing_index = nan_gap_index
-            start_gap = nan_to_number[nan_gap_index]
-            # find the number immediately after start_gap in spin_starts
-            next_spin_start_indices = np.where(spin_starts > start_gap)[0]
-            if len(next_spin_start_indices) > 0:
-                next_spin_start = next_spin_start_indices[0]
-                number_of_spins = (
-                    self.epoch[next_spin_start] - self.epoch[start_gap]
-                ) // expected_spin
-                while number_of_spins > 1:
-                    estimated_start = self.epoch[next_spin_start] + expected_spin
+
+        paired_nans = nan_to_number.reshape(-1, 2)
+
+        for start_of_gap, end_of_gap in paired_nans:
+            # in nan_to_number, we have the start and end for every nan gap
+            # if this gap spans more than 1 spin period, we need to insert
+            # additional spin_starts into spin_starts.
+
+            gap_start_time = self.epoch[start_of_gap]
+            gap_end_time = self.epoch[end_of_gap]
+
+            # Calculate the number of spins in this gap
+            number_of_spins = int((gap_end_time - gap_start_time) // expected_spin)
+            if number_of_spins > 1:
+                # Insert new spin starts into spin_starts
+                for i in range(1, number_of_spins):
+                    estimated_start = gap_start_time + i * expected_spin
                     new_spin_index = (np.abs(self.epoch - estimated_start)).argmin()
-                    np.insert(spin_starts, existing_index, new_spin_index)
-                    existing_index += 1
-                    number_of_spins -= 1
+
+                    spin_starts = np.append(spin_starts, new_spin_index)
+                    print(f"Inserting new spin start {estimated_start}")
+
+        spin_starts = np.sort(spin_starts)
 
         chunk_start = 0
         offset_epochs = []

imap_processing/tests/mag/test_mag_l1d.py
@@ -120,6 +120,7 @@ def test_offset_vector():
 def test_calculate_spin_offsets(
     mag_l1d_test_class, fake_mag_spin_data, furnish_kernels
 ):
+    # Expected to fail, need to merge spice fix
     x_vectors = np.arange(1, 156)
     y_vectors = np.arange(156, 1, -1)
     mag_l1d_test_class.vectors[:, 0] = x_vectors
@@ -139,30 +140,16 @@ def test_calculate_spin_offsets(
     with furnish_kernels(kernels):
         offsets = mag_l1d_test_class.calculate_spin_offsets()
 
-    expected_epochs = [15, 45, 90, 150]
+    expected_epochs = [15, 45, 75, 105, 135]
     assert np.array_equal(offsets["epoch"].data, expected_epochs)
 
-    # pull out the valid full spins from the test data (last few are fudging to get a
-    # chunk from 150-155)
-    valid_spins = [
-        [15, 30],
-        [30, 45],
-        [45, 60],
-        [75, 90],
-        [90, 105],
-        [135, 150],
-        [150, 155],
-        [155, 155],
-    ]
-
+    indices_to_average = [[15, 45], [45, 75], [75, 105], [105, 135], [135, 155]]
     expected_x_avg = []
     expected_y_avg = []
-    for index in range(0, len(valid_spins), 2):
-        x_spin = x_vectors[valid_spins[index][0] : valid_spins[index + 1][1]]
 
-        expected_x_avg.append(np.nanmean(x_spin))
-        y_spin = y_vectors[valid_spins[index][0] : valid_spins[index + 1][1]]
-        expected_y_avg.append(np.nanmean(y_spin))
+    for start_index, end_index in indices_to_average:
+        expected_x_avg.append(np.nanmean(x_vectors[start_index:end_index]))
+        expected_y_avg.append(np.nanmean(y_vectors[start_index:end_index]))
 
     np.testing.assert_allclose(offsets["x_offset"].data, expected_x_avg)
     np.testing.assert_allclose(offsets["y_offset"].data, expected_y_avg)