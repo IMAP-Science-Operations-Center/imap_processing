e394084ee0e7c2fbbb1654cb2c91e88f9e3fa98d
Maxine Hartnett <maxine.hartnett@lasp.colorado.edu>
2025-08-04 11:27:28 -0600

    fix Ultra remote tests

--- imap_processing/tests/external_test_data_config.py
+++ imap_processing/tests/external_test_data_config.py
@@ -101,6 +101,13 @@ EXTERNAL_TEST_DATA = [
     ("imap_ultra_l1b-90sensor-spbtphcorr_20250101_v000.csv", "ultra/data/l1/"),
     ("ultra45_raw_sc_ultrarawimg_withFSWccs_FM45_40P_Phi28p5_BeamCal_LinearScan_"
      "phi2850_theta-000_20240207T102740_revised20250724.csv", "ultra/data/l1/"),
+    ("ultra_90_dps_exposure.csv", "ultra/data/l1/"),
+    ("Ultra_90_DPS_efficiencies_all.csv", "ultra/data/l1/"),
+    ("Ultra_efficiencies_45_combined_logistic_interpolation.csv", "ultra/data/l1/"),
+    ("ultra_90_dps_gf.csv", "ultra/data/l1/"),
+    ("ultra_90_dps_exposure.csv", "ultra/test_data/l1/"),
+    ("Ultra_90_DPS_efficiencies_all.csv", "ultra/test_data/l1/"),
+    ("ultra_90_dps_gf.csv", "ultra/test_data/l1/"),
 
     # MAG
     ("mag-l1b-l1c-t013-magi-burst-in.csv",
