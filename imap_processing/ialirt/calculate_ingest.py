c9b933ad38fec5f89d1c50324550578db7f6a38d
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2025-08-07 10:30:59 -0600

    first pass

--- imap_processing/ialirt/calculate_ingest.py
+++ imap_processing/ialirt/calculate_ingest.py
@@ -2,7 +2,7 @@
 
 import logging
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from imap_processing.ialirt.constants import STATIONS
 from imap_processing.ialirt.process_ephemeris import calculate_azimuth_and_elevation
@@ -11,13 +11,10 @@ from imap_processing.spice.time import et_to_utc, str_to_et
 # Logger setup
 logger = logging.getLogger(__name__)
 
-# TODO: get a list of all potential DSN stations.
-ALL_STATIONS = [*STATIONS.keys(), "DSS-24", "DSS-25", "DSS-26",
-                "DSS-34", "DSS-35", "DSS-36", "DSS-53", "DSS-54",
-                "DSS-55", "DSS-56", "DSS-74", "DSS-75"]
+ALL_STATIONS = [*STATIONS.keys()]
 
 
-def find_tcp_connections(filename, lines):
+def find_tcp_connections(filename, lines, partner):
     """
     Find connection time ranges for Kiel ground station from log lines.
 
@@ -28,17 +25,17 @@ def find_tcp_connections(filename, lines):
     connection_ranges = []
     current_start = None
 
-    # Extract start of hour from filename: "flight_iois_1.log.2025-212T16_55_27.531613"
-    timestamp_str = filename.split(".")[2]  # "2025-212T16_55_27"
-    timestamp_str = timestamp_str.replace("_", ":")  # "2025-212T16:55:27"
+    timestamp_str = filename.split(".")[2]
+    timestamp_str = timestamp_str.replace("_", ":")
     base_time = datetime.strptime(timestamp_str, "%Y-%jT%H:%M:%S")
     start_of_hour = base_time.replace(minute=0, second=0, microsecond=0)
+    end_of_hour = start_of_hour + timedelta(hours=1)
 
     for line in lines:
-        if "Kiel antenna partner connection is up." in line:
+        if f"{partner} antenna partner connection is up." in line:
             timestamp = line.split(" ")[0]
             current_start = datetime.strptime(timestamp, "%Y/%j-%H:%M:%S.%f")
-        elif "Kiel antenna partner connection is down!" in line:
+        elif f"{partner} antenna partner connection is down!" in line:
             timestamp = line.split(" ")[0]
             end_time = datetime.strptime(timestamp, "%Y/%j-%H:%M:%S.%f")
             if current_start is None:
@@ -46,6 +43,9 @@ def find_tcp_connections(filename, lines):
             else:
                 connection_ranges.append((current_start, end_time))
                 current_start = None
+
+    if current_start is not None:
+        connection_ranges.append((current_start, end_of_hour))
 
     return connection_ranges
 
