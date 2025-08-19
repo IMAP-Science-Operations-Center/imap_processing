091e715bc90df95d02fc9dfacc5f2f05d8ef3632
Tenzin Choedon <36522642+tech3371@users.noreply.github.com>
2025-08-19 17:56:34 +0000

    format-fixes

--- imap_processing/glows/l1b/glows_l1b.py
+++ imap_processing/glows/l1b/glows_l1b.py
@@ -230,7 +230,7 @@ def process_histogram(
         tuple
             Tuple of processed L1B data arrays from HistogramL1B.output_data().
         """
-        return HistogramL1B(
+        return HistogramL1B(  # type: ignore[call-arg]
             *args, ancillary_exclusions, ancillary_parameters
         ).output_data()
 
