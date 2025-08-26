261cfd335a1f7ef957c47e3116003e3b966fab81
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2025-08-26 14:13:24 -0600

    update import

--- imap_processing/ultra/l1c/spacecraft_pset.py
+++ imap_processing/ultra/l1c/spacecraft_pset.py
@@ -7,11 +7,11 @@ import pandas as pd
 import xarray as xr
 
 from imap_processing.cdf.utils import parse_filename_like
+from imap_processing.ultra.l1b.ultra_l1b_culling import get_de_rejection_mask
 from imap_processing.ultra.l1c.l1c_lookup_utils import (
     calculate_pixels_within_scattering_threshold,
     get_spacecraft_pointing_lookup_tables,
-from imap_processing.ultra.l1b.ultra_l1b_culling import get_de_rejection_mask
-
+)
 from imap_processing.ultra.l1c.ultra_l1c_pset_bins import (
     build_energy_bins,
     get_efficiencies_and_geometric_function,
