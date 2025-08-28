d049e1ef92fee95c9b01d23cabdb46d58bbc0cd2
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2025-08-28 17:00:03 -0600

    fix tests

--- imap_processing/tests/ultra/unit/test_spacecraft_pset.py
+++ imap_processing/tests/ultra/unit/test_spacecraft_pset.py
@@ -158,6 +158,9 @@ def test_calculate_spacecraft_pset_with_cdf(
         de_dict["quality_scattering"] = np.zeros(len(sc_dps_velocity), dtype=np.uint16)
         de_dict["quality_outliers"] = np.zeros(len(sc_dps_velocity), dtype=np.uint16)
         de_dict["species"] = np.ones(len(sc_dps_velocity), dtype=np.uint8)
+        de_dict["event_times"] = 817561854.185627 + (
+            df_subset["tdb"].values - df_subset["tdb"].values[0]
+        )
 
         name = "imap_ultra_l1b_45sensor-de"
         dataset = create_dataset(de_dict, name, "l1b")
