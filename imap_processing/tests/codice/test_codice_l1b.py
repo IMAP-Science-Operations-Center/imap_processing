d470f8390bd2dedbccdc250273eee1c15b1dc80c
Tenzin Choedon <tenzin.choedon@lasp.colorado.edu>
2025-09-17 01:13:07 -0600

    CoDICE: L1B SW angular counts

--- imap_processing/tests/codice/test_codice_l1b.py
+++ imap_processing/tests/codice/test_codice_l1b.py
@@ -46,3 +46,38 @@ def test_l1b_lo_sw_species():
     # Write to CDF
     cdf_file = write_cdf(processed_data)
     assert cdf_file.name == "imap_codice_l1b_lo-sw-species_20250814_v999.cdf"
+
+
+def test_l1b_lo_sw_angular():
+    l1a_test_file = (
+        imap_module_directory
+        / "tests"
+        / "codice"
+        / "data"
+        / "l1a_validation"
+        / "imap_codice_l1a_lo-sw-angular_20250814211100_v0.0.3.cdf"
+    )
+
+    l1b_val_data = (
+        imap_module_directory
+        / "tests"
+        / "codice"
+        / "data"
+        / "l1b_validation"
+        / "imap_codice_l1b_lo-sw-angular_20250814211100_v0.0.3.cdf"
+    )
+    l1b_val_data = load_cdf(l1b_val_data)
+    processed_data = process_codice_l1b(l1a_test_file)
+
+    for variable in l1b_val_data.data_vars:
+        assert processed_data[variable].shape == l1b_val_data[variable].shape
+        np.testing.assert_allclose(
+            processed_data[variable].values,
+            l1b_val_data[variable].values,
+            rtol=1e-5,
+            err_msg=f"Mismatch in variable '{variable}'",
+        )
+
+    # Write to CDF
+    cdf_file = write_cdf(processed_data)
+    assert cdf_file.name == "imap_codice_l1b_lo-sw-angular_20250814_v999.cdf"
