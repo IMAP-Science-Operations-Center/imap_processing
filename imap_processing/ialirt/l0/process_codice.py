9817a1cb021d2b2fae7df47655b87632157b6ab7
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2025-10-10 14:36:32 -0600

    formatting

--- imap_processing/ialirt/l0/process_codice.py
+++ imap_processing/ialirt/l0/process_codice.py
@@ -99,7 +99,7 @@ def process_codice(
         cod_hi_data_stream = concatenate_bytes(grouped_cod_hi_data, group, "lo")
 
         # Decompress binary stream
-        decompressed_data = decompress._apply_loggy_a(bytes(cod_hi_data_stream))  # noqa
+        decompressed_data = decompress._apply_lossy_a(bytes(cod_hi_data_stream))  # noqa
 
     # For I-ALiRT SIT, the test data being used has all zeros and thus no
     # groups can be found, thus there is no data to process
