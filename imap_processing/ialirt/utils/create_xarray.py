d01fbe61a08a7e26961d1fcdc67f71f7fa925079
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2025-10-15 16:03:44 -0600

    prevent cdf archive creation from breaking

--- imap_processing/ialirt/utils/create_xarray.py
+++ imap_processing/ialirt/utils/create_xarray.py
@@ -155,6 +155,7 @@ def create_xarray_from_records(records: list[dict]) -> xr.Dataset:  # noqa: PLR0
                 "sc_velocity_GSM",
                 "sc_velocity_GSE",
                 "mag_hk_status",
+                "spice_kernels",
             ]:
                 continue
             elif key in ["mag_B_GSE", "mag_B_GSM", "mag_B_RTN"]:
