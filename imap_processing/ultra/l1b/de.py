5db3588abf95bcc06169f6a93d9b88b3dcba820c
Luisa <luisa.coakley@lasp.colorado.edu>
2025-10-23 16:37:28 -0600

    fix vaild events filtering

--- imap_processing/ultra/l1b/de.py
+++ imap_processing/ultra/l1b/de.py
@@ -318,18 +318,18 @@ def calculate_de(
     ultra_frame = getattr(SpiceFrame, f"IMAP_ULTRA_{sensor}")
 
     # Account for counts=0 (event times have FILL value)
-    valid_events = event_times != FILLVAL_FLOAT32
+    valid_events = (event_times != FILLVAL_FLOAT32).copy()
     # TODO - find a better solution than filtering out data from repointings?
     if repoint_id is not None:
         in_pointing = calculate_events_in_pointing(
-            repoint_id, event_times, valid_events
+            repoint_id, event_times[valid_events]
         )
         # Update quality flags for valid events that are not in the pointing
-        quality_flags[valid_events & ~in_pointing] |= (
+        quality_flags[valid_events][~in_pointing] |= (
             ImapAttitudeUltraFlags.DURINGREPOINT.value
         )
         # Update valid_events to only include times within a pointing
-        valid_events &= in_pointing
+        valid_events[valid_events] &= in_pointing
 
     if np.any(valid_events):
         (
@@ -395,7 +395,6 @@ def calculate_de(
 def calculate_events_in_pointing(
     repoint_id: int,
     event_times: np.ndarray,
-    valid_events: np.ndarray,
 ) -> np.ndarray:
     """
     Calculate boolean array of events within a pointing.
@@ -406,8 +405,6 @@ def calculate_events_in_pointing(
         The repointing ID.
     event_times : np.ndarray
         Array of event times in ET.
-    valid_events : np.ndarray
-        Boolean array indicating valid events.
 
     Returns
     -------
@@ -424,12 +421,9 @@ def calculate_events_in_pointing(
     pointing_start_met = repoint_row["repoint_end_met"].values[0]
     pointing_end_met = next_repoint_row["repoint_start_met"].values[0]
 
-    # Create a boolean array for events within the pointing
-    in_pointing = np.zeros(len(event_times), dtype=bool)
-
     # Check which events are within the pointing
-    in_pointing[valid_events] = (
-        et_to_met(event_times[valid_events]) >= pointing_start_met
-    ) & (et_to_met(event_times[valid_events]) <= pointing_end_met)
+    in_pointing = (et_to_met(event_times) >= pointing_start_met) & (
+        et_to_met(event_times) <= pointing_end_met
+    )
 
     return in_pointing
