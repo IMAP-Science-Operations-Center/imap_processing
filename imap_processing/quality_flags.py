04a9417a372500686e0973f98186049e0e537135
Luisa <luisa.coakley@lasp.colorado.edu>
2025-11-03 15:00:13 -0700

    fix big flag comment

--- imap_processing/quality_flags.py
+++ imap_processing/quality_flags.py
@@ -43,7 +43,7 @@ class ImapDEOutliersUltraFlags(FlagNameMixin):
     NONE = CommonFlags.NONE
     FOV = 2**0  # bit 0
     PHCORR = 2**1  # bit 1
-    COINPH = 2**2  # bit 4 # Event validity
+    COINPH = 2**2  # bit 2 # Event validity
     INVALID_ENERGY = 2**3  # bit 3
 
 
