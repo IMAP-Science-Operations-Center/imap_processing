6cd810d483ac95e52b475bd6dc29263cb2d9049f
Luisa <luisa.coakley@lasp.colorado.edu>
2025-11-05 14:12:59 -0700

    remove spin edge conversion

--- imap_processing/ultra/l1b/ultra_l1b_culling.py
+++ imap_processing/ultra/l1b/ultra_l1b_culling.py
@@ -72,8 +72,7 @@ def get_energy_histogram(
     spin_df = get_spin_data()
 
     unique_spin_number = np.unique(spin_number)
-    spin_edges = unique_spin_number.astype(np.uint32)
-    spin_edges = np.append(spin_edges, spin_edges.max() + 1)
+    spin_edges = np.append(unique_spin_number, unique_spin_number.max() + 1)
 
     # Counts per spin at each energy bin.
     hist, _ = np.histogramdd(
