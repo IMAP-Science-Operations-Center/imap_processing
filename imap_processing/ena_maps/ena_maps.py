c11c5f198c725d961db8e2be2823789925c65118
Tim Plummer <Timothy.Plummer@lasp.colorado.edu>
2025-11-04 17:08:08 -0700

    Try again to fix github tests

--- imap_processing/ena_maps/ena_maps.py
+++ imap_processing/ena_maps/ena_maps.py
@@ -675,8 +675,9 @@ class HiPointingSet(LoHiBasePointingSet):
         self.data = self.data.rename(rename_dict)
 
         # Add obs_date variable to be used in determining a map mean obs_date
+        mid_time = self.data["epoch"].values[0] + self.data["epoch_delta"].values[0] / 2
         self.data["obs_date"] = xr.full_like(
-            self.data["exposure_factor"], self.data["epoch"].values[0]
+            self.data["exposure_factor"], float(mid_time)
         )
 
         # Update az_el_points using the base class method
