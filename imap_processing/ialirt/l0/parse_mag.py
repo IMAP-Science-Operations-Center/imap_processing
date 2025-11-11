fff4b0ec433f6a19de68948feb679a847921eadf
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2025-11-11 12:15:03 -0700

    single packet error

--- imap_processing/ialirt/l0/parse_mag.py
+++ imap_processing/ialirt/l0/parse_mag.py
@@ -212,12 +212,13 @@ def get_time(
 
     primary_time = TimeTuple(int(pri_coarsetm.item()), int(pri_fintm.item()))
     secondary_time = TimeTuple(int(sec_coarsetm.item()), int(sec_fintm.item()))
-
-    time_data_primary_ttj2000ns = primary_time.to_j2000ns()
+    time_data_pri_met = primary_time.to_seconds()
+    time_data_primary_ttj2000ns = met_to_ttj2000ns(time_data_pri_met)
     time_data["primary_epoch"] = shift_time(
         time_data_primary_ttj2000ns, time_shift_mago
     )
-    time_data_secondary_ttj2000ns = secondary_time.to_j2000ns()
+    time_data_sec_met = secondary_time.to_seconds()
+    time_data_secondary_ttj2000ns = met_to_ttj2000ns(time_data_sec_met)
     time_data["secondary_epoch"] = shift_time(
         time_data_secondary_ttj2000ns, time_shift_magi
     )
@@ -691,10 +692,14 @@ def process_packet(
     )
 
     spherical = cartesian_to_spherical(gsm_vector)
+    if spherical.ndim == 1:
+        spherical = spherical[np.newaxis, :]
     phi_gsm = spherical[:, 1]
     theta_gsm = spherical[:, 2]
 
     spherical = cartesian_to_spherical(gse_vector)
+    if spherical.ndim == 1:
+        spherical = spherical[np.newaxis, :]
     phi_gse = spherical[:, 1]
     theta_gse = spherical[:, 2]
 
