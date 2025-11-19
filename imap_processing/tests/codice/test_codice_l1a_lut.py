e22a8517afe48ce42b6d22fe5ca9118ad1daef76
Tenzin Choedon <tenzin.choedon@lasp.colorado.edu>
2025-11-19 12:59:48 -0700

    other tests fixes because of new LUT update

--- imap_processing/tests/codice/test_codice_l1a_lut.py
+++ imap_processing/tests/codice/test_codice_l1a_lut.py
@@ -7,6 +7,7 @@ from imap_processing import imap_module_directory
 from imap_processing.codice.utils import (
     calculate_acq_time_per_step,
     get_collapse_pattern_shape,
+    get_counters_aggregated_pattern,
 )
 
 pytestmark = pytest.mark.external_test_data
@@ -36,7 +37,7 @@ def test_codice_non_zero_patterns():
     # expected non-zero row indices for each collapse_lo matrix
     expected_lo_non_zero_rows = {
         "0": [1, 2, 3, 23, 24],
-        # "1" is tested separately below
+        # "1" aggregation is tested separately below
         "2": list(range(1, 25)),
         "3": [1, 2, 3, 23, 24, 28, 31],
         "4": list(range(4, 23)),
@@ -46,13 +47,8 @@ def test_codice_non_zero_patterns():
         "8": list(range(4, 23)),
     }
     for key in collapse_lo.keys():
-        # instrument counts stores data as each variable in a separate key
-        if key == "1" and "variables" in collapse_lo[key].keys():
-            for variable_name in collapse_lo[key]["variables"]:
-                arr = np.array(collapse_lo[key]["variables"][variable_name])
-                assert arr.shape == (12,)
+        if key == "1":
             continue
-
         # check matrix shape is uniform across all keys
         arr = np.array(collapse_lo[f"{key}"]["matrix"])
         assert arr.shape == (32, 12)
@@ -62,12 +58,20 @@ def test_codice_non_zero_patterns():
         if key in expected_lo_non_zero_rows:
             assert non_zero_rows == expected_lo_non_zero_rows[key]
 
+    # Test Lo aggregation separately as its structure is different
+    # instrument counts stores data as each variable in a separate key
+    key = "1"
+    for variable_name in collapse_lo[key]["variables"]:
+        arr = np.array(collapse_lo[key]["variables"][variable_name])
+        assert arr.shape == (12,)
+
     hi_collapse = sci_lut[table_id]["collapse_hi"]
 
     # expected non-zero row indices for each collapse_hi matrix
     # actual non-zero rows observed in the JSON collapse_hi matrices
     expected_hi_non_zero_rows = {
-        "0": [0, 1, 2, 4, 5, 6, 9, 12, 13, 14, 15],
+        # Tested Hi aggregated separately below
+        # "0": [0, 1, 2, 4, 5, 6, 9, 12, 13, 14, 15],
         "1": [0, 1, 3, 4, 5, 7, 8, 9, 11, 12, 13, 15],
         "2": [0, 1, 3, 4, 5, 7, 8, 9, 11, 12, 13, 15],
         "4": [0, 1, 3, 4, 5, 7, 8, 9, 11, 12, 13, 15],
@@ -76,11 +80,19 @@ def test_codice_non_zero_patterns():
         "10": [0, 1, 3, 4, 5, 7, 8, 9, 11, 12, 13, 15],
     }
     for key in hi_collapse.keys():
+        if key == "0":
+            continue
         arr = np.array(hi_collapse[f"{key}"]["matrix"])
         assert arr.shape == (16, 24)
         non_zero_rows = np.where(arr.any(axis=1))[0].tolist()
         if key in expected_hi_non_zero_rows:
             assert non_zero_rows == expected_hi_non_zero_rows[key]
+
+    # Test Hi aggregated separately as its structure is different
+    key = "0"
+    for variable_name in hi_collapse[key]["variables"]:
+        arr = np.array(hi_collapse[key]["variables"][variable_name])
+        assert arr.shape == (24,)
 
 
 def test_get_collapse_pattern_shape():
@@ -112,13 +124,15 @@ def test_get_collapse_pattern_shape():
     assert aggre_counts == (1,)
 
     # Hi aggregated counts
-    collapsed_row_example = get_collapse_pattern_shape(
+    collapsed_row_example = get_counters_aggregated_pattern(
         sci_lut_data,
         sensor_id=1,
         collapse_table_id=0,
     )
-    assert collapsed_row_example == (1, 11)
-
+    for variable in collapsed_row_example.keys():
+        # All Hi aggregated variables should have
+        # collapsed to one column cell.
+        assert collapsed_row_example[variable] == 1
     # LoSW priority
     row_collapsed_example = get_collapse_pattern_shape(
         sci_lut_data, sensor_id=0, collapse_table_id=4
