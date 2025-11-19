5b423f51aa34b1e2012d0a69295b79da78c726a8
Tenzin Choedon <tenzin.choedon@lasp.colorado.edu>
2025-11-19 16:05:40 -0700

    doc build fixes

--- imap_processing/codice/utils.py
+++ imap_processing/codice/utils.py
@@ -166,8 +166,8 @@ def get_collapse_pattern_shape(
 
     Returns
     -------
-    tuple[spin_sector, inst_azimuth]
-        The reduced shape describing the collapsed pattern. Examples:
+    tuple[int, int]
+        (<spin_sector, inst_azimuth>) describing the collapsed pattern. Examples:
         ``(1,)`` for a fully collapsed 1-D pattern or ``(N, M)`` for a
         reduced 2-D pattern.
     """
