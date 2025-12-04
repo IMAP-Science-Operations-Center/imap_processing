41920df61e9189a09ed9dd7c92f9d26f61caee57
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2025-12-04 13:31:27 -0700

    add lut for efficiency

--- imap_processing/tests/external_test_data_config.py
+++ imap_processing/tests/external_test_data_config.py
@@ -82,6 +82,7 @@ EXTERNAL_TEST_DATA = [
     ("imap_codice_l2-hi-sectored-efficiency_20251008_v001.csv", "codice/data/l2_lut/"),
     ("imap_codice_l2-lo-gfactor_20251008_v001.csv", "codice/data/l2_lut/"),
     ("imap_codice_l2-lo-efficiency_20251008_v001.csv", "codice/data/l2_lut/"),
+    ("imap_codice_l2-lo-ialirt-efficiency_20251008_v001.csv", "codice/data/l2_lut/"),
 
     # L2 Validation data
     (f"imap_codice_l2_hi-omni_{VALIDATION_FILE_DATE}_{VALIDATION_FILE_VERSION}.cdf", "codice/data/l2_validation/"),
