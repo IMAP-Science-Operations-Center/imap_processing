5c7504acb559af95edad8d0bce376e0b5e73c0fc
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2025-12-08 12:21:01 -0700

    fix documentation issue

--- imap_processing/codice/codice_l2.py
+++ imap_processing/codice/codice_l2.py
@@ -55,7 +55,7 @@ def get_geometric_factor_lut(
     ----------
     dependencies : ProcessingInputCollection
         The collection of processing input files.
-    path : Path (Optional)
+    path : pathlib.Path, optional
         Optional path used for I-ALiRT.
 
     Returns
@@ -100,7 +100,7 @@ def get_efficiency_lut(
     ----------
     dependencies : ProcessingInputCollection
         The collection of processing input files.
-    path : Path (Optional)
+    path : pathlib.Path, optional
         Optional path used for I-ALiRT.
 
     Returns
