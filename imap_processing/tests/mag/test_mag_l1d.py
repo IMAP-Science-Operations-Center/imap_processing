18301b53863e87242a6d4c3bf8f04deff0b9652c
Mhairi Finlayson <m.finlayson@imperial.ac.uk>
2025-12-10 11:48:30 +0000

    Update test to use new spice_frame formulation, and reflect that MAGO and MAGI might share a spice frame

--- imap_processing/tests/mag/test_mag_l1d.py
+++ imap_processing/tests/mag/test_mag_l1d.py
@@ -569,7 +569,6 @@ def test_enhanced_gradiometry_with_quality_flags_detailed():
     )
     assert np.array_equal(grad_ds["quality_flags"].data, expected_flags)
 
-
 def test_rotate_frames(mag_l1d_test_class):
     # Reset to initial MAGO frame for this test
     mag_l1d_test_class.frame = ValidFrames.MAGO
@@ -581,10 +580,8 @@ def test_rotate_frames(mag_l1d_test_class):
     def mock_frame_transform(
         epoch_et, vectors, from_frame, to_frame, allow_spice_noframeconnect
     ):
-        if from_frame == ValidFrames.MAGO.value:
+        if from_frame == ValidFrames.MAGO.spice_frame or from_frame == ValidFrames.MAGI.spice_frame:
             return vectors + 100
-        elif from_frame == ValidFrames.MAGI.value:
-            return vectors + 200
         else:
             return vectors + 300
 
@@ -600,20 +597,22 @@ def test_rotate_frames(mag_l1d_test_class):
 
         # First call should be for MAGO vectors
         first_call_args = mock_transform_l1d.call_args_list[0]
-        assert first_call_args[1]["from_frame"] == ValidFrames.MAGO.value
-        assert first_call_args[1]["to_frame"] == ValidFrames.SRF.value
+        assert first_call_args[1]["from_frame"] == ValidFrames.MAGO.spice_frame
+        assert first_call_args[1]["to_frame"] == ValidFrames.SRF.spice_frame
 
         # Second call should be for MAGI vectors
         second_call_args = mock_transform_l1d.call_args_list[1]
-        assert second_call_args[1]["from_frame"] == ValidFrames.MAGI.value
-        assert second_call_args[1]["to_frame"] == ValidFrames.SRF.value
+        assert second_call_args[1]["from_frame"] == ValidFrames.MAGI.spice_frame
+        assert second_call_args[1]["to_frame"] == ValidFrames.SRF.spice_frame
 
-        # Check that MAGO vectors were transformed from MAGO frame (+100)
+        # MAGO frame and MAGi frame not necessarily different (and are now the same)
+
+        # Check that MAGO vectors were transformed (+100)
         expected_mago_vectors = initial_vectors + 100
         np.testing.assert_array_equal(mag_l1d_test_class.vectors, expected_mago_vectors)
 
-        # Check that MAGI vectors were transformed from MAGI frame (+200)
-        expected_magi_vectors = initial_magi_vectors + 200
+        # Check that MAGI vectors were transformed (+100)
+        expected_magi_vectors = initial_magi_vectors + 100
         np.testing.assert_array_equal(
             mag_l1d_test_class.magi_vectors, expected_magi_vectors
         )
