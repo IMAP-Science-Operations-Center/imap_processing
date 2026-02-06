b65927a05255a9de8ba3784fe997d388ef26a028
Tim Plummer <Timothy.Plummer@lasp.colorado.edu>
2026-02-06 09:59:03 -0700

    Remove one more reference to second packet

--- imap_processing/hi/hi_l1c.py
+++ imap_processing/hi/hi_l1c.py
@@ -605,7 +605,7 @@ def find_last_de_packet_data(l1b_dataset: xr.Dataset) -> xr.Dataset:
     # Get the indices of the packet before each ESA change.
     esa_step = epoch_dataset["esa_step"].values
     esa_energy_step = epoch_dataset["esa_energy_step"].values
-    # A change in esa_step should indicate the location of the second packet in
+    # A change in esa_step should indicate the location of the last packet in
     # each pair of DE packets at an esa_energy_step. In practice, during some
     # calibration activities, it was observed that the esa_energy_step can change
     # when the esa_step did not. So, we look for either to change and use the
