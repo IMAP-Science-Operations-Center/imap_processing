86a499a594756e486aa159dc571698151dc8b3fa
Luisa <luisa.coakley@lasp.colorado.edu>
2026-03-02 11:30:13 -0700

    remove duplicate code

--- imap_processing/ultra/l1b/extendedspin.py
+++ imap_processing/ultra/l1b/extendedspin.py
@@ -151,19 +151,6 @@ def calculate_extendedspin(
     # is flagged will have the bitflag of all the energy flags combined.
     voltage_qf = voltage_qf * np.bitwise_or.reduce(energy_bin_flags)
     # Expand binned quality flags to individual spins.
-    # high energy and statistical outlier flags are energy dependent
-    # Collapse them into a single flag array
-    high_energy_qf = np.bitwise_or.reduce(
-        high_energy_qf * energy_bin_flags[:, np.newaxis], axis=0
-    )
-    stat_outliers_qf = np.bitwise_or.reduce(
-        stat_outliers_qf * energy_bin_flags[:, np.newaxis], axis=0
-    )
-    # Low voltage flag is shape (n_spin_bins,) but we want to convert from a boolean
-    # to a bitwise flag to be consistent with the other flags, where each spin that
-    # is flagged will have the bitflag of all the energy flags combined.
-    voltage_qf = voltage_qf * np.bitwise_or.reduce(energy_bin_flags)
-    # Expand binned quality flags to individual spins.
     high_energy_qf = expand_bin_flags_to_spins(len(spin), high_energy_qf, spin_bin_size)
     voltage_qf = expand_bin_flags_to_spins(len(spin), voltage_qf, spin_bin_size)
     stat_outliers_qf = expand_bin_flags_to_spins(
