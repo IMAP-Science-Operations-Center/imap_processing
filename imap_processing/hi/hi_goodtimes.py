38c537179ef22b53bf648e5f6194eed537eb8b59
Tim Plummer <timothy.plummer@lasp.colorado.edu>
2026-04-03 13:57:46 -0600

    Apply suggestion from @subagonsouth

--- imap_processing/hi/hi_goodtimes.py
+++ imap_processing/hi/hi_goodtimes.py
@@ -604,6 +604,8 @@ class GoodtimesAccessor:
             cull_next = grouped[i + 1][1]
 
             # Compare shapes first (different lengths = different pattern)
+            # In a nominal Pointing, the final ESA sweep will get cut short by the repoint
+            # maneuver. This is the typical case where the shape would be different.
             if cull_curr.shape != cull_next.shape:
                 pattern_changes.append(True)
             else:
