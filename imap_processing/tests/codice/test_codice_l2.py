cb4c9e4e75c3a5dcce0fd9bb445079d1c1937371
David Turner <davidt0x@gmail.com>
2026-05-26 00:24:48 -0400

    fix(codice): (5.1) clarify lo-sw species sunward wording
    SPDF comment (5.1):
    Could you clarify what "sw" in the dataset name (imap_codice_l2_lo-sw-species) means? From Logical_source_description="IMAP Mission CoDICE Lo Level-2 Sunward Species Intensity Data.", it's seems "sw" means "Sunward", and not Solar Wind (what some may assume). And then I see in many variable descriptions (CATDES) such statement as "sunward solar-wind", e.g. for "cplus5" variable, CATDESC="Differential intensity for sunward solar-wind C5+." Could you double check if this "sunward solar-wind" correct, and what sw means?

--- imap_processing/tests/codice/test_codice_l2.py
+++ imap_processing/tests/codice/test_codice_l2.py
@@ -473,13 +473,16 @@ def test_codice_l2_sw_species_intensity(mock_get_file_paths, codice_lut_path):
     with cdflib.CDF(cdf_path) as cdf_file:
         hplus_attrs = cdf_file.varattsget("hplus")
         assert (
-            hplus_attrs["CATDESC"] == "Differential intensity for sunward solar-wind H+"
+            hplus_attrs["CATDESC"]
+            == "Differential intensity from sunward-looking detectors measuring "
+            "solar-wind H+"
         )
         assert hplus_attrs["FIELDNAM"] == "Sunward Differential Intensity - H+"
         unc_hplus_attrs = cdf_file.varattsget("unc_hplus")
         assert (
             unc_hplus_attrs["CATDESC"]
-            == "Uncertainty in differential intensity for sunward solar-wind H+"
+            == "Uncertainty in differential intensity from sunward-looking "
+            "detectors measuring solar-wind H+"
         )
         assert unc_hplus_attrs["FIELDNAM"] == "Sunward Uncertainty - H+"
         for var in ["nso_esa_step", "nso_spin_sector"]:
