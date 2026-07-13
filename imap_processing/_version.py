efbce408c418ad328ad86d9a2189278858186dce
Tenzin Choedon <36522642+tech3371@users.noreply.github.com>
2026-07-13 12:20:20 -0600

    updating to imap-data-access == v0.42.0 (#3329)

--- imap_processing/_version.py
+++ imap_processing/_version.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "1.0.34.post5.dev0+abedb33d"
-__version_tuple__ = (1, 0, 34, "post5", "dev0", "abedb33d")
+__version__ = "0.0.0"
+__version_tuple__ = (0, 0, 0)
