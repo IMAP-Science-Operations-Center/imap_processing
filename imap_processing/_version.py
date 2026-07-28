5bd190e283fdf9bc4223915448de1e67251c8026
Vineet Bansal <vineetbansal@protonmail.com>
2026-07-28 17:27:02 -0400

    pre-commit fix

--- imap_processing/_version.py
+++ imap_processing/_version.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "0.0.0"
-__version_tuple__ = (0, 0, 0)
+__version__ = "1.0.35.post11.dev0+ed24bc01"
+__version_tuple__ = (1, 0, 35, "post11", "dev0", "ed24bc01")
