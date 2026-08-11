17c7090cf73a2b66ce98e703e0e2683d2a4ef214
Luisa Coakley <luisa.coakley@lasp.colorado.edu>
2026-08-11 12:33:27 -0600

    update lo sw species depend 1

--- imap_processing/codice/codice_l1b.py
+++ imap_processing/codice/codice_l1b.py
@@ -22,7 +22,9 @@ from imap_processing.codice import constants
 logger = logging.getLogger(__name__)
 
 
-def convert_to_rates(dataset: xr.Dataset, descriptor: str) -> np.ndarray:
+def convert_to_rates(
+    dataset: xr.Dataset, descriptor: str, cdf_attrs: ImapCdfAttributes
+) -> np.ndarray:
     """
     Apply a conversion from counts to rates.
 
@@ -35,6 +37,8 @@ def convert_to_rates(dataset: xr.Dataset, descriptor: str) -> np.ndarray:
         The L1b dataset containing the data to convert.
     descriptor : str
         The descriptor of the data product of interest.
+    cdf_attrs : ImapCdfAttributes
+        The CDF attributes manager, with L1b variable attributes loaded.
 
     Returns
     -------
@@ -49,19 +53,27 @@ def convert_to_rates(dataset: xr.Dataset, descriptor: str) -> np.ndarray:
     )
     if descriptor.startswith("lo-"):
         # Calculate energy_per_charge using voltage_table and k_factor
-        energy_attrs = dataset["voltage_table"].attrs | {
-            "UNITS": "keV/e",
-            "LABLAXIS": "E/q",
-            "CATDESC": "Energy per charge",
-            "FIELDNAM": "Energy per charge",
-        }
         # 1e3 is to convert eV to keV
+        energy_per_charge = (
+            dataset["voltage_table"].values * dataset["k_factor"].values * 1e-3
+        )
         dataset["energy_per_charge"] = xr.DataArray(
-            dataset["voltage_table"].values * dataset["k_factor"].values * 1e-3,
+            energy_per_charge,
             dims=[
                 "esa_step",
             ],
-            attrs=energy_attrs,
+            attrs=cdf_attrs.get_variable_attributes(
+                "energy_per_charge", check_schema=False
+            ),
+        )
+        dataset["energy_per_charge_label"] = xr.DataArray(
+            np.array([f"{value:.3f}" for value in energy_per_charge]),
+            dims=[
+                "esa_step",
+            ],
+            attrs=cdf_attrs.get_variable_attributes(
+                "energy_per_charge_label", check_schema=False
+            ),
         )
 
     if descriptor in [
@@ -180,6 +192,7 @@ def process_codice_l1b(file_path: Path) -> xr.Dataset:
     # Get the L1b CDF attributes
     cdf_attrs = ImapCdfAttributes()
     cdf_attrs.add_instrument_global_attrs("codice")
+    cdf_attrs.add_instrument_variable_attrs("codice", "l1b")
 
     # Use the L1a data product as a starting point for L1b
     l1b_dataset = l1a_dataset.copy(deep=True)
@@ -190,4 +203,5 @@ def process_codice_l1b(file_path: Path) -> xr.Dataset:
     return convert_to_rates(
         l1b_dataset,
         descriptor,
+        cdf_attrs,
     )
