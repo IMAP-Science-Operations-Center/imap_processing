95519f22bce082a1dbf6a2ebb3b4f74c29ec31a8
Leo Werneck <wernecklr@gmail.com>
2026-08-11 17:19:07 -0400

    Added Python 3.13 and 3.14 support

--- imap_processing/_version.py
+++ imap_processing/_version.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "1.0.36.post2.dev0+6f06f337"
-__version_tuple__ = (1, 0, 36, "post2", "dev0", "6f06f337")
+__version__ = "1.0.36.post5.dev0+4650530a"
+__version_tuple__ = (1, 0, 36, "post5", "dev0", "4650530a")
