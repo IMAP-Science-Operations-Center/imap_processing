204e3c914567bd3d7d5bc9792c3161034c9bc5c6
alex <aldo9253@colorado.edu>
2026-08-19 12:25:33 -0600

    fixed expected nan condition in l2a for ion grid values

--- imap_processing/tests/idex/test_idex_l2a.py
+++ imap_processing/tests/idex/test_idex_l2a.py
@@ -210,8 +210,6 @@ def test_l2a_logical_source_and_cdf(l2a_dataset: xr.Dataset, l1b_dataset: xr.Dat
 
     # TODO: remove this NAN block when fitting logic is applied
     expected_nan_vars = [
-        "ion_grid_dust_mass_estimate",
-        "ion_grid_velocity_estimate",
         "tof_peak_area_under_fit",
         "tof_peak_chi_square",
         "tof_peak_fit_parameters",
