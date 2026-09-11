25d46c84d2ba6733cda9f79d4d6db89d4f36f240
Luisa Coakley <luisa.coakley@lasp.colorado.edu>
2026-09-11 14:11:33 -0600

    update spin phase and fix binning bug

--- imap_processing/idex/idex_l1b.py
+++ imap_processing/idex/idex_l1b.py
@@ -40,7 +40,10 @@ from imap_processing.spice.geometry import (
     instrument_pointing,
     solar_longitude,
 )
-from imap_processing.spice.spin import get_spacecraft_spin_phase, get_spin_angle
+from imap_processing.spice.spin import (
+    get_instrument_spin_phase,
+    get_spin_angle,
+)
 from imap_processing.spice.time import et_to_met, ttj2000ns_to_et
 from imap_processing.utils import convert_raw_to_eu
 
@@ -590,8 +593,10 @@ def get_spice_data(
     et = ttj2000ns_to_et(l1a_dataset["epoch"].data)
     # Get (Mission Elapsed Time)
     met = et_to_met(et)
-    # Get spacecraft spin phase in degrees
-    spin_phase = get_spacecraft_spin_phase(query_met_times=met)
+    # Get instrument spin phase in degrees
+    spin_phase = get_instrument_spin_phase(
+        query_met_times=met, instrument=SpiceFrame.IMAP_IDEX
+    )
     imap_spin_phase = get_spin_angle(spin_phase, degrees=True)
     # Get the position and velocity of IMAP in ecliptic frame
     ephemeris = imap_state(et, observer=SpiceBody.SUN)
